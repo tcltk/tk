f53052eddcb9760338772ef822599c3a8052b68f
escoffon <escoffon>
1998-07-01 18:32:09 +0000

    Initial revision

---
+++ win/mkd.bat
@@ -0,0 +1,20 @@
+@echo off
+rem SCCS: %Z% $Id: mkd.bat,v 1.1 1998/07/01 18:32:09 escoffon Exp $ 
+
+if exist %1 goto end
+
+if %OS% == Windows_NT goto winnt
+
+echo Add support for Win 95 please
+goto end
+
+goto success
+
+:winnt
+md %1
+if errorlevel 1 goto end
+
+:success
+echo created directory %1
+
+:end
---
+++ win/rmd.bat
@@ -0,0 +1,20 @@
+@echo off
+rem SCCS: %Z% $Id: rmd.bat,v 1.1 1998/07/01 18:32:09 escoffon Exp $ 
+
+if not exist %1 goto end
+
+if %OS% == Windows_NT goto winnt
+
+echo Add support for Win 95 please
+goto end
+
+goto success
+
+:winnt
+rmdir %1 /s /q
+if errorlevel 1 goto end
+
+:success
+echo deleted directory %1
+
+:end
