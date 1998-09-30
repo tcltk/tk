5e674ce6194b24cecf0ad4ef5aaaf5c790451138
escoffon <escoffon>
1998-09-30 20:19:00 +0000

    Added support for Win95.

--- win/rmd.bat
+++ win/rmd.bat
@@ -1,13 +1,18 @@
 @echo off
-rem RCS: @(#) $Id: rmd.bat,v 1.2 1998/09/14 18:23:59 stanton Exp $
+rem RCS: @(#) $Id: rmd.bat,v 1.3 1998/09/30 20:19:59 escoffon Exp $
 
-if not exist %1 goto end
+if not exist %1\tag.txt goto end
 
-if %OS% == Windows_NT goto winnt
+echo Removing directory %1
 
-echo Add support for Win 95 please
-goto end
+if "%OS%" == "Windows_NT" goto winnt
 
+cd %1
+if errorlevel 1 goto end
+del *.*
+cd ..
+rmdir %1
+if errorlevel 1 goto end
 goto success
 
 :winnt
