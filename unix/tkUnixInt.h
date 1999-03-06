f7eb1a03a334f0ea61efcdb409a31ac9ed02b2c6
redman <redman>
1999-03-06 00:06:22 +0000

    Modifications for Tk stubs implementation

--- unix/tkUnixInt.h
+++ unix/tkUnixInt.h
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkUnixInt.h,v 1.2 1998/09/14 18:23:57 stanton Exp $
+ * RCS: @(#) $Id: tkUnixInt.h,v 1.2.4.1 1999/03/06 00:06:22 redman Exp $
  */
 
 #ifndef _TKUNIXINT
@@ -21,12 +21,6 @@
  * than the ones they're defined in.
  */
 
-EXTERN void		TkCreateXEventSource _ANSI_ARGS_((void));
-EXTERN TkWindow *	TkpGetContainer _ANSI_ARGS_((TkWindow *embeddedPtr));
-EXTERN TkWindow *	TkpGetWrapperWindow _ANSI_ARGS_((TkWindow *winPtr));
-EXTERN Window		TkUnixContainerId _ANSI_ARGS_((TkWindow *winPtr));
-EXTERN int		TkUnixDoOneXEvent _ANSI_ARGS_((Tcl_Time *timePtr));
-EXTERN void		TkUnixSetMenubar _ANSI_ARGS_((Tk_Window tkwin,
-				Tk_Window menubar));
+#include "tkIntPlatDecls.h"
 
 #endif /* _TKUNIXINT */
