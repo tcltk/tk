72483241b7348e466329b0c91f7b8aa2ad6f3dec
hobbs <hobbs>
2000-02-08 10:00:44 +0000

    * generic/tk.h (TK_RELEASE_SERIAL): Moved to 8.3.0 patchlevel

--- generic/tk.h
+++ generic/tk.h
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tk.h,v 1.36 2000/01/14 03:25:38 hobbs Exp $
+ * RCS: @(#) $Id: tk.h,v 1.37 2000/02/08 10:00:44 hobbs Exp $
  */
 
 #ifndef _TK
@@ -25,7 +25,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    
+
 /*
  * When version numbers change here, you must also go into the following files
  * and update the version numbers:
@@ -46,11 +46,11 @@ extern "C" {
 
 #define TK_MAJOR_VERSION   8
 #define TK_MINOR_VERSION   3
-#define TK_RELEASE_LEVEL   TCL_BETA_RELEASE
-#define TK_RELEASE_SERIAL  2
+#define TK_RELEASE_LEVEL   TCL_FINAL_RELEASE
+#define TK_RELEASE_SERIAL  0
 
 #define TK_VERSION	"8.3"
-#define TK_PATCH_LEVEL	"8.3b2"
+#define TK_PATCH_LEVEL	"8.3.0"
 
 /*
  * The following definitions set up the proper options for Macintosh
