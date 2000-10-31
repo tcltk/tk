032d4fd9828b704021d738faadf62f421c1c0778
hobbs <hobbs>
2000-10-31 00:52:11 +0000

    * tests/tk.test: 	* doc/tk.n: updated to reflect default on status of useinputmethods. 	* library/tk.tcl: tk useinputmethods is set to 1 by default.  This 	enables Kanji and dead-char input by default.  Intro'd in 	1999-12-16 with default off to avoid some problems with older X 	servers that would slow down widget creation over time.

--- library/tk.tcl
+++ library/tk.tcl
@@ -3,7 +3,7 @@
 # Initialization script normally executed in the interpreter for each
 # Tk-based application.  Arranges class bindings for widgets.
 #
-# RCS: @(#) $Id: tk.tcl,v 1.25 2000/09/30 18:00:12 drh Exp $
+# RCS: @(#) $Id: tk.tcl,v 1.26 2000/10/31 00:52:11 hobbs Exp $
 #
 # Copyright (c) 1992-1994 The Regents of the University of California.
 # Copyright (c) 1994-1996 Sun Microsystems, Inc.
@@ -32,6 +32,10 @@ if {[info exists auto_path] && [string compare {} $tk_library] && \
 # Turn off strict Motif look and feel as a default.
 
 set tk_strictMotif 0
+
+# Turn on useinputmethods (X Input Methods) by default.
+
+tk useinputmethods 1
 
 # Create a ::tk namespace
 
