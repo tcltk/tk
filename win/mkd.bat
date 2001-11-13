7389fa0668243c3e20c20dae89750aa9eaeeb997
davygrvy <davygrvy>
2001-11-13 02:46:23 +0000

    * win/mkd.bat: 	* win/rmd.bat:  Removed -kb CVS attribute and added changes 	from Llyod Lim for better stability.  [Patch #456761]
    	* win/rules.vc(new):
    	* win/buildall.vc.bat(new):
    	* win/makefile.vc:  large rewrite following Tcl's makefile.vc as
    	a guide and Patch #456761.  Appears BugFree(tm).

--- win/mkd.bat
+++ win/mkd.bat
@@ -1,22 +1,13 @@
 @echo off
-rem RCS: @(#) $Id: mkd.bat,v 1.4 2001/09/08 22:34:42 mdejong Exp $
+rem RCS: @(#) $Id: mkd.bat,v 1.5 2001/11/13 02:46:23 davygrvy Exp $
 
-if exist %1\tag.txt goto end
-
-if "%OS%" == "Windows_NT" goto winnt
+if exist %1\nul goto end
 
 md %1
 if errorlevel 1 goto end
 
-goto success
-
-:winnt
-md %1
-if errorlevel 1 goto end
-
-:success
-echo TAG >%1\tag.txt
-echo created directory %1
+echo Created directory %1
 
 :end
+
 
