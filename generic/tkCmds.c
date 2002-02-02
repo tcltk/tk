4c4cfd8a45c05788c35b6eaabdcd9b03def5e621
hobbs <hobbs>
2002-02-02 00:16:05 +0000

    * generic/tkCmds.c (Tk_TkObjCmd): don't use 'bool' as an arg as it 	conflicts with the C99 spec. [Bug #511956] (ingham)

--- generic/tkCmds.c
+++ generic/tkCmds.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkCmds.c,v 1.21 2002/01/17 05:13:11 dgp Exp $
+ * RCS: @(#) $Id: tkCmds.c,v 1.22 2002/02/02 00:16:05 hobbs Exp $
  */
 
 #include "tkPort.h"
@@ -708,13 +708,13 @@ Tk_TkObjCmd(clientData, interp, objc, objv)
 		 * That will indicate to the user that input methods
 		 * are just not available.
 		 */
-		int bool;
-		if (Tcl_GetBooleanFromObj(interp, objv[2+skip], &bool)
+		int boolVal;
+		if (Tcl_GetBooleanFromObj(interp, objv[2+skip], &boolVal)
 			!= TCL_OK) {
 		    return TCL_ERROR;
 		}
 #ifdef TK_USE_INPUT_METHODS
-		dispPtr->useInputMethods = bool;
+		dispPtr->useInputMethods = boolVal;
 #endif /* TK_USE_INPUT_METHODS */
 	    } else if ((objc - skip) != 2) {
 		Tcl_WrongNumArgs(interp, 2, objv,
