1602350f3b047bb7da28140fdd89db2329a6dd0d
davygrvy <davygrvy>
2002-03-29 03:55:41 +0000

    updated to match Tcl

--- win/buildall.vc.bat
+++ win/buildall.vc.bat
@@ -1,13 +1,36 @@
 @echo off
-if "%MSVCDir%" == "" call c:\dev\devstudio60\vc98\bin\vcvars32.bat
 
-set INSTALLDIR=C:\tclTestArea
+::  This is an example batchfile for building everything. Please
+::  edit this (or make your own) for your needs and wants using
+::  the instructions for calling makefile.vc found in makefile.vc
+::
+::  RCS: @(#) $Id: buildall.vc.bat,v 1.3 2002/03/29 03:55:41 davygrvy Exp $
+
+echo Sit back and have a cup of coffee while this grinds through ;)
+echo You asked for *everything*, remember?
+echo.
+
+if "%MSVCDir%" == "" call C:\dev\devstudio60\vc98\bin\vcvars32.bat
+set INSTALLDIR=C:\progra~1\tcl
 set TCLDIR=..\..\tcl_head
 
-nmake -nologo -f makefile.vc release
+nmake -nologo -f makefile.vc release winhelp OPTS=none
+if errorlevel 1 goto error
 nmake -nologo -f makefile.vc release OPTS=static,linkexten
-nmake -nologo -f makefile.vc core    OPTS=static,msvcrt
-nmake -nologo -f makefile.vc release OPTS=static,threads,linkexten
-nmake -nologo -f makefile.vc core    OPTS=static,msvcrt,threads
+if errorlevel 1 goto error
+nmake -nologo -f makefile.vc core OPTS=static,msvcrt
+if errorlevel 1 goto error
+nmake -nologo -f makefile.vc core OPTS=static,threads
+if errorlevel 1 goto error
+nmake -nologo -f makefile.vc core OPTS=static,msvcrt,threads
+if errorlevel 1 goto error
 nmake -nologo -f makefile.vc release OPTS=threads
+if errorlevel 1 goto error
+goto end
+
+:error
+echo *** BOOM! ***
+
+:end
+echo done!
 pause
