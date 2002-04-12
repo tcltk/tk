8c7f629e8d7cc90276c74a9ac78047003e83f991
hobbs <hobbs>
2002-04-12 07:18:49 +0000

    * win/tkWinInt.h: declaration for TkWinGetUnicodeEncoding 	* win/tkWinDialog.c (GetFileNameW): use TkWinGetUnicodeEncoding 	* win/tkWinInit.c (TkpDisplayWarning): use TkWinGetUnicodeEncoding 	* win/tkWinFont.c: use TkWinGetUnicodeEncoding instead of static 	unicodeEncoding.

--- win/tkWinDialog.c
+++ win/tkWinDialog.c
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkWinDialog.c,v 1.25 2002/02/08 02:57:11 dgp Exp $
+ * RCS: @(#) $Id: tkWinDialog.c,v 1.26 2002/04/12 07:18:49 hobbs Exp $
  *
  */
 
@@ -495,7 +495,6 @@ GetFileNameW(clientData, interp, objc, objv, open)
     int open;			/* 1 to call GetOpenFileName(), 0 to 
 				 * call GetSaveFileName(). */
 {
-    Tcl_Encoding unicodeEncoding = Tcl_GetEncoding(NULL, "unicode");
     OPENFILENAMEW ofn;
     WCHAR file[TK_MULTI_MAX_PATH];
     int result, winCode, oldMode, i, multi = 0;
@@ -504,6 +503,7 @@ GetFileNameW(clientData, interp, objc, objv, open)
     HWND hWnd;
     Tcl_DString utfFilterString, utfDirString;
     Tcl_DString extString, filterString, dirString, titleString;
+    Tcl_Encoding unicodeEncoding = TkWinGetUnicodeEncoding();
     ThreadSpecificData *tsdPtr = (ThreadSpecificData *) 
             Tcl_GetThreadData(&dataKey, sizeof(ThreadSpecificData));
     static CONST char *saveOptionStrings[] = {
@@ -2424,13 +2424,13 @@ Tk_MessageBoxObjCmd(clientData, interp, objc, objv)
     int objc;			/* Number of arguments. */
     Tcl_Obj *CONST objv[];	/* Argument objects. */
 {
-    Tcl_Encoding unicodeEncoding = Tcl_GetEncoding(NULL, "unicode");
     Tk_Window tkwin, parent;
     HWND hWnd;
     char *message, *title;
     int defaultBtn, icon, type;
     int i, oldMode, flags, winCode;
     Tcl_DString messageString, titleString;
+    Tcl_Encoding unicodeEncoding = TkWinGetUnicodeEncoding();
     static CONST char *optionStrings[] = {
 	"-default",	"-icon",	"-message",	"-parent",
 	"-title",	"-type",	NULL
