8807fd2349deb5a6863ef43786f782d7aec5875a
hobbs <hobbs>
2002-06-15 02:15:39 +0000

    corrected cast warning

--- generic/tkCmds.c
+++ generic/tkCmds.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkCmds.c,v 1.26 2002/06/15 02:08:12 hobbs Exp $
+ * RCS: @(#) $Id: tkCmds.c,v 1.27 2002/06/15 02:15:39 hobbs Exp $
  */
 
 #include "tkPort.h"
@@ -726,7 +726,7 @@ Tk_TkObjCmd(clientData, interp, objc, objv)
 		return TCL_ERROR;
 	    }
 	    Tcl_SetBooleanObj(Tcl_GetObjResult(interp),
-		    (dispPtr->flags & TK_DISPLAY_USE_IM));
+		    (int) (dispPtr->flags & TK_DISPLAY_USE_IM));
 	    break;
 	}
     }
