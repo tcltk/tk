2fa4c35f0a045d304b0eb448ff5a2505530af10d
davygrvy <davygrvy>
2002-06-26 22:53:13 +0000

    incorrect <eol> fixed.

--- generic/tkStyle.c
+++ generic/tkStyle.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkStyle.c,v 1.1 2002/06/18 23:51:46 dkf Exp $
+ * RCS: @(#) $Id: tkStyle.c,v 1.2 2002/06/26 22:53:13 davygrvy Exp $
  */
 
 #include "tkInt.h"
