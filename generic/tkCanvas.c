75f478183adb7df3293a47f206b923fb76533cd3
hobbs <hobbs>
2002-07-24 19:40:27 +0000

    * tests/canvas.test: added canvas-14.[1-6] 	* generic/tkCanvas.c (CanvasWidgetCmd): corrected handling of 	'scan' subcommand args.  Also removed early return cases to goto 	done instead where the canvasPtr would be Tcl_Release'd.  This may 	solve other problems with unreleased canvasPtr's. [Bug #581560]

--- generic/tkCanvas.c
+++ generic/tkCanvas.c
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkCanvas.c,v 1.14.2.2 2001/04/06 01:10:17 hobbs Exp $
+ * RCS: @(#) $Id: tkCanvas.c,v 1.14.2.3 2002/07/24 19:40:27 hobbs Exp $
  */
 
 /* #define USE_OLD_TAG_SEARCH 1 */
@@ -1390,7 +1390,8 @@ CanvasWidgetCmd(clientData, interp, argc, argv)
       case CANV_ITEMCGET: {
 	if (argc != 4) {
 	    Tcl_WrongNumArgs(interp, 2, argv, "tagOrId option");
-	    return TCL_ERROR;
+	    result = TCL_ERROR;
+	    goto done;
 	}
 #ifdef USE_OLD_TAG_SEARCH
 	itemPtr = StartTagSearch(canvasPtr, argv[2], &search);
@@ -1625,26 +1626,22 @@ CanvasWidgetCmd(clientData, interp, argc, argv)
 	    "mark", "dragto", NULL
 	};
 
-	if (Tcl_GetIndexFromObj(interp, argv[2], optionStrings, "scan option", 0,
-		&index) != TCL_OK) {
-	    return TCL_ERROR;
-	}
-
-	if ((argc != 5) && (argc != 5+index)) {
+	if (argc < 5) {
+	    Tcl_WrongNumArgs(interp, 2, argv, "mark|dragto x y ?dragGain?");
+	    result = TCL_ERROR;
+	} else if (Tcl_GetIndexFromObj(interp, argv[2], optionStrings,
+		"scan option", 0, &index) != TCL_OK) {
+	    result = TCL_ERROR;
+	} else if ((argc != 5) && (argc != 5+index)) {
 	    Tcl_WrongNumArgs(interp, 3, argv, index?"x y ?gain?":"x y");
 	    result = TCL_ERROR;
-	    goto done;
-	}
-	if ((Tcl_GetIntFromObj(interp, argv[3], &x) != TCL_OK)
+	} else if ((Tcl_GetIntFromObj(interp, argv[3], &x) != TCL_OK)
 		|| (Tcl_GetIntFromObj(interp, argv[4], &y) != TCL_OK)){
 	    result = TCL_ERROR;
-	    goto done;
-	}
-	if ((argc == 6) && (Tcl_GetIntFromObj(interp, argv[5], &gain) != TCL_OK)) {
+	} else if ((argc == 6) &&
+		(Tcl_GetIntFromObj(interp, argv[5], &gain) != TCL_OK)) {
 	    result = TCL_ERROR;
-            goto done;
-	}
-	if (!index) {
+	} else if (!index) {
 	    canvasPtr->scanX = x;
 	    canvasPtr->scanXOrigin = canvasPtr->xOrigin;
 	    canvasPtr->scanY = y;
@@ -1719,7 +1716,8 @@ CanvasWidgetCmd(clientData, interp, argc, argv)
 	}
 	if (Tcl_GetIndexFromObj(interp, argv[2], optionStrings, "select option", 0,
 		&optionindex) != TCL_OK) {
-	    return TCL_ERROR;
+	    result = TCL_ERROR;
+	    goto done;
 	}
 	switch ((enum options) optionindex) {
 	  case CANV_ADJUST: {
