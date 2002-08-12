defbacd4a1c0aa643606b8793c2a73b087661f39
donal.k.fellows@manchester.ac.uk <dkf>
2002-08-12 13:38:47 +0000

    Tweaked resizing behaviour of the image-viewer demo.

--- library/demos/image2.tcl
+++ library/demos/image2.tcl
@@ -3,7 +3,7 @@
 # This demonstration script creates a simple collection of widgets
 # that allow you to select and view images in a Tk label.
 #
-# RCS: @(#) $Id: image2.tcl,v 1.5 2001/11/15 14:13:13 dkf Exp $
+# RCS: @(#) $Id: image2.tcl,v 1.6 2002/08/12 13:38:48 dkf Exp $
 
 if {![info exists widgetDemo]} {
     error "This script should be run from the \"widget\" demo."
@@ -83,8 +83,8 @@ entry $w.dir.e -width 30 -textvariable dirName
 button $w.dir.b -pady 0 -padx 2m -text "Select Dir." \
 	-command "selectAndLoadDir $w"
 bind $w.dir.e <Return> "loadDir $w"
-pack $w.dir.e -side left -fill y -padx 2m -pady 2m
-pack $w.dir.b -side left -fill y -padx {0 2m} -pady 2m
+pack $w.dir.e -side left -fill both -padx 2m     -pady 2m -expand true
+pack $w.dir.b -side left -fill y    -padx {0 2m} -pady 2m
 labelframe $w.f -text "File:" -padx 2m -pady 2m
 
 listbox $w.f.list -width 20 -height 10 -yscrollcommand "$w.f.scroll set"
@@ -99,6 +99,6 @@ labelframe $w.image -text "Image:"
 label $w.image.image -image image2a
 pack $w.image.image -padx 2m -pady 2m
 
-grid $w.dir -        -sticky w -padx 1m -pady 1m -in $w.mid
+grid $w.dir -        -sticky ew -padx 1m -pady 1m -in $w.mid
 grid $w.f   $w.image -sticky nw -padx 1m -pady 1m -in $w.mid
 grid columnconfigure $w.mid 1 -weight 1
