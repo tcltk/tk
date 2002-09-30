ba2790ec24676e226f972cb566f38959ed0f53ef
hobbs <hobbs>
2002-09-30 18:55:56 +0000

    * tests/panedwindow.test: added panedwindow-28.[12] 	* generic/tkPanedWindow.c (PanedWindowCmdDeletedProc): 	(Tk_PanedWindowObjCmd): when destroying the panedwindow, destroy 	the proxy before the parent to avoid dependency conflicts. 	[Bug #616589]

--- generic/tkPanedWindow.c
+++ generic/tkPanedWindow.c
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkPanedWindow.c,v 1.8 2002/08/08 01:43:31 hobbs Exp $
+ * RCS: @(#) $Id: tkPanedWindow.c,v 1.9 2002/09/30 18:55:57 hobbs Exp $
  */
 
 #include "tkPort.h"
@@ -418,8 +418,8 @@ Tk_PanedWindowObjCmd(clientData, interp, objc, objv)
 	    (ClientData) pwPtr);
     
     if (ConfigurePanedWindow(interp, pwPtr, objc - 2, objv + 2) != TCL_OK) {
-	Tk_DestroyWindow(pwPtr->tkwin);
 	Tk_DestroyWindow(pwPtr->proxywin);
+	Tk_DestroyWindow(pwPtr->tkwin);
 	ckfree((char *) pwPtr);
 	return TCL_ERROR;
     }
@@ -1309,8 +1309,8 @@ PanedWindowCmdDeletedProc(clientData)
      */
 
     if (!(pwPtr->flags & WIDGET_DELETED)) {
-	Tk_DestroyWindow(pwPtr->tkwin);
 	Tk_DestroyWindow(pwPtr->proxywin);
+	Tk_DestroyWindow(pwPtr->tkwin);
     }
 }
 
