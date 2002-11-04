99902f0281c6c4dd7836d03cfc1303cad0c41f49
davygrvy <davygrvy>
2002-11-04 07:49:43 +0000

    * win/winMain.c (Tcl_AppInit): Calls Registry_Init() and 	Dde_Init() when STATIC_BUILD and TCL_USE_STATIC_PACKAGES macros 	are set.
    	* win/makefile.vc:  linkexten option now sets the
    	TCL_USE_STATIC_PACKAGES	macro which also adds the registry and
    	dde static lib files to the link of the shell. [Patch 479697]
    
    	* win/rules.vc: Matches the one from Tcl.
    
    	* win/buildall.vc.bat: More useful commentary.

--- win/buildall.vc.bat
+++ win/buildall.vc.bat
@@ -4,33 +4,60 @@
 ::  edit this (or make your own) for your needs and wants using
 ::  the instructions for calling makefile.vc found in makefile.vc
 ::
-::  RCS: @(#) $Id: buildall.vc.bat,v 1.3 2002/03/29 03:55:41 davygrvy Exp $
+::  RCS: @(#) $Id: buildall.vc.bat,v 1.4 2002/11/04 07:49:43 davygrvy Exp $
 
 echo Sit back and have a cup of coffee while this grinds through ;)
 echo You asked for *everything*, remember?
 echo.
 
-if "%MSVCDir%" == "" call C:\dev\devstudio60\vc98\bin\vcvars32.bat
-set INSTALLDIR=C:\progra~1\tcl
-set TCLDIR=..\..\tcl_head
+title Building Tk, please wait...
 
+if "%MSVCDir%" == "" call c:\dev\devstudio60\vc98\bin\vcvars32.bat
+::if "%MSVCDir%" == "" call "C:\Program Files\Microsoft Developer Studio\vc98\bin\vcvars32.bat"
+
+set INSTALLDIR=C:\Program Files\Tcl
+
+:: Where is the Tcl source directory?
+set TCLDIR=..\..\tcl_head_stock
+::set TCLDIR=..\..\tcl8.4
+
+:: Build the normal stuff along with the help file.
+::
 nmake -nologo -f makefile.vc release winhelp OPTS=none
 if errorlevel 1 goto error
-nmake -nologo -f makefile.vc release OPTS=static,linkexten
+
+:: Build the static core, dlls and shell.
+::
+nmake -nologo -f makefile.vc release OPTS=static
 if errorlevel 1 goto error
+
+:: Build the special static libraries that use the dynamic runtime.
+::
 nmake -nologo -f makefile.vc core OPTS=static,msvcrt
 if errorlevel 1 goto error
-nmake -nologo -f makefile.vc core OPTS=static,threads
-if errorlevel 1 goto error
-nmake -nologo -f makefile.vc core OPTS=static,msvcrt,threads
-if errorlevel 1 goto error
+
+:: Build the core and shell for thread support.
+::
 nmake -nologo -f makefile.vc release OPTS=threads
 if errorlevel 1 goto error
+
+:: Build a static, thread support core library (no shell).
+::
+nmake -nologo -f makefile.vc core OPTS=static,threads
+if errorlevel 1 goto error
+
+:: Build the special static libraries the use the dynamic runtime,
+:: but now with thread support.
+::
+nmake -nologo -f makefile.vc core OPTS=static,msvcrt,threads
+if errorlevel 1 goto error
+
 goto end
 
 :error
 echo *** BOOM! ***
 
 :end
-echo done!
+title Building Tk, please wait...DONE!
+echo DONE!
 pause
