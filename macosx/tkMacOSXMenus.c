18fe9e1e4965d84b2be4c68b4f54abd793852ee3
wolfsuit <wolfsuit>
2003-03-08 02:15:45 +0000

    Fix a bug with MacOS X Tk's handling of the help menu.  The code expected the MacOS 9 behavior, which was to insert a separator item after the system help menus, but this doesn't happen on X...

--- macosx/tkMacOSXMenus.c
+++ macosx/tkMacOSXMenus.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMacOSXMenus.c,v 1.2 2002/08/31 06:12:30 das Exp $
+ * RCS: @(#) $Id: tkMacOSXMenus.c,v 1.3 2003/03/08 02:15:46 wolfsuit Exp $
  */
 
 #include "tk.h"
@@ -209,7 +209,6 @@ TkMacOSXInitMenus(
     }
     
     DrawMenuBar();
-    TkMacOSXSetHelpMenuItemCount();
     return;
 }
 
