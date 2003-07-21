cfed25705be3362ddf0b3efea16c0fcc42317975
das <das>
2003-07-21 12:39:50 +0000

    * macosx/buildTkConfig.tcl: fix to TK_BUILD_* ref generation 	broken by changes to tcl buildsystem.

--- macosx/buildTkConfig.tcl
+++ macosx/buildTkConfig.tcl
@@ -3,7 +3,7 @@
 # Synthesize tkConfig.sh for Mac OS X based
 # on tclConfig.sh and tkConfig.sh.in
 #
-# RCS: @(#) $Id: buildTkConfig.tcl,v 1.2 2003/03/21 01:43:58 das Exp $
+# RCS: @(#) $Id: buildTkConfig.tcl,v 1.3 2003/07/21 12:39:50 das Exp $
 #
 # the next line restarts using tclsh \
 exec tclsh8.4 "$0" "$@"
@@ -27,8 +27,8 @@ proc main {tclConfigFile tkConfigFileIn tkConfigFile} {
 		regsub -line -- "^TK_$var=.*\$" $tkConfig "TK_$var=$val" tkConfig
 	}
 	regsub -line -all -- {@[^@]+@} $tkConfig {} tkConfig
-	regsub -line -all -- {/[^/]+\.build/Tk.build} $tkConfig {} tkConfig
-	regsub -line {^(TK_DEFS=')} $tkConfig {\1 -DMAC_OSX_TK } tkConfig
+	regsub -line -all -- {(/tk)/(?:Development|Deployment)} $tkConfig {\1} tkConfig
+	regsub -line {^(TK_DEFS=')} $tkConfig {\1 -DMAC_OSX_TK} tkConfig
 	
 	set out [open $tkConfigFile w]
 	puts $out $tkConfig
