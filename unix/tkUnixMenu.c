d29e8db2badac2db52e78d1726b1cd2f105e64e6
hobbs <hobbs>
2004-05-03 23:23:14 +0000

    * win/tkWinMenu.c, unix/tkUnixMenu.c (DrawMenuEntryLabel): place 	images of compound menu entries in indicator space if not a radio 	of checkbutton. [Bug #756952] (eserte)

--- unix/tkUnixMenu.c
+++ unix/tkUnixMenu.c
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkUnixMenu.c,v 1.9 2003/08/14 12:30:11 dkf Exp $
+ * RCS: @(#) $Id: tkUnixMenu.c,v 1.10 2004/05/03 23:23:14 hobbs Exp $
  */
 
 #include "tkPort.h"
@@ -763,10 +763,23 @@ DrawMenuEntryLabel(menuPtr, mePtr, d, gc, tkfont, fmPtr, x, y, width, height)
 		break;
 	    }
 	    case COMPOUND_LEFT: {
+		/*
+		 * Position image in the indicator space to the left of the
+		 * entries, unless this entry is a radio|check button because
+		 * then the indicator space will be used.
+		 */
 		textXOffset = imageWidth + 2;
 		textYOffset = 0;
 		imageXOffset = 0;
 		imageYOffset = 0;
+		if ((mePtr->type != CHECK_BUTTON_ENTRY) 
+			&& (mePtr->type != RADIO_BUTTON_ENTRY)) {
+		    textXOffset -= indicatorSpace;
+		    if (textXOffset < 0) {
+			textXOffset = 0;
+		    }
+		    imageXOffset = -indicatorSpace;
+		}
 		break;
 	    }
 	    case COMPOUND_RIGHT: {
