31e58abb93b8beb200285944ab8c56929d4e6383
donal.k.fellows@manchester.ac.uk <dkf>
2004-05-24 21:21:50 +0000

    The TIP specified a -detail option, not a -finemessage option.

--- macosx/tkMacOSXDialog.c
+++ macosx/tkMacOSXDialog.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMacOSXDialog.c,v 1.9 2004/05/24 18:28:40 wolfsuit Exp $
+ * RCS: @(#) $Id: tkMacOSXDialog.c,v 1.10 2004/05/24 21:21:50 dkf Exp $
  */
 #include <Carbon/Carbon.h>
 
@@ -1316,9 +1316,9 @@ Tk_MessageBoxObjCmd(
     int                     result = TCL_OK;
     
     static CONST char *movableAlertStrings[] = {
-	"-default", "-finemessage", "-icon", 
+	"-default", "-detail", "-icon", 
 	"-message", "-parent", 
-	"-title", "-type", 	
+	"-title", "-type",
 	(char *)NULL
     };
     static CONST char *movableTypeStrings[] = {
@@ -1337,7 +1337,7 @@ Tk_MessageBoxObjCmd(
 	(char *)NULL
     };
     enum movableAlertOptions {
-	ALERT_DEFAULT, ALERT_FINEMESSAGE, ALERT_ICON,
+	ALERT_DEFAULT, ALERT_DETAIL, ALERT_ICON,
 	ALERT_MESSAGE, ALERT_PARENT,
 	ALERT_TITLE, ALERT_TYPE
     };
@@ -1422,7 +1422,7 @@ Tk_MessageBoxObjCmd(
 	    indexDefaultOption = i;
 	    break;
 	    
-            case ALERT_FINEMESSAGE:
+            case ALERT_DETAIL:
 	    str = Tcl_GetStringFromObj( objv[i + 1], NULL );
 	    finemessageTextCF = CFStringCreateWithCString( NULL, str, kCFStringEncodingUTF8 );
 	    break;
