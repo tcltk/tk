f61d16313fcd6f8759fa7628b9b7fbff8978f1e9
hobbs <hobbs>
2004-08-20 00:40:31 +0000

    * win/tkWinDialog.c (Tk_MessageBoxObjCmd): Inherit the icon from 	* win/tkWinInt.h:                          the -parent window for 	* win/tkWinWm.c (TkWinGetIcon):            the MessageBox.

--- win/tkWinInt.h
+++ win/tkWinInt.h
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkWinInt.h,v 1.14.2.1 2004/05/03 22:40:58 hobbs Exp $
+ * RCS: @(#) $Id: tkWinInt.h,v 1.14.2.2 2004/08/20 00:40:32 hobbs Exp $
  */
 
 #ifndef _TKWININT
@@ -164,6 +164,11 @@ EXTERN LRESULT CALLBACK	TkWinChildProc _ANSI_ARGS_((HWND hwnd, UINT message,
  * tkWinX.c:GenerateXEvent and tkWinClipboard.c:UpdateClipboard
  */
 EXTERN void	TkWinUpdatingClipboard(int mode);
+
+/*
+ * Used by tkWinDialog.c to associate the right icon with tk_messageBox
+ */
+EXTERN HICON	TkWinGetIcon(Tk_Window tkw, DWORD iconsize);
 
 /*
  * The following structure keeps track of whether we are using the 
