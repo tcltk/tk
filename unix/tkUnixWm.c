2117a40e6c7b84bf8486f91680a6c4e6e04165fd
dgp <dgp@users.sourceforge.net>
2004-10-27 17:34:36 +0000

    backport compiler warning silencers

--- unix/tkUnixWm.c
+++ unix/tkUnixWm.c
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkUnixWm.c,v 1.36.2.3 2004/10/05 22:27:27 hobbs Exp $
+ * RCS: @(#) $Id: tkUnixWm.c,v 1.36.2.4 2004/10/27 17:34:36 dgp Exp $
  */
 
 #include "tkPort.h"
@@ -479,7 +479,7 @@ void TkWmCleanup(dispPtr)
 	    ckfree(wmPtr->iconName);
 	}
 	if (wmPtr->iconDataPtr != NULL) {
-	    ckfree(wmPtr->iconDataPtr);
+	    ckfree((char *)wmPtr->iconDataPtr);
 	}
 	if (wmPtr->leaderName != NULL) {
 	    ckfree(wmPtr->leaderName);
@@ -506,7 +506,7 @@ void TkWmCleanup(dispPtr)
 	ckfree((char *) wmPtr);
     }
     if (dispPtr->iconDataPtr != NULL) {
-	ckfree(dispPtr->iconDataPtr);
+	ckfree((char *)dispPtr->iconDataPtr);
 	dispPtr->iconDataPtr = NULL;
     }
 }
@@ -813,7 +813,7 @@ TkWmDeadWindow(winPtr)
 	ckfree(wmPtr->iconName);
     }
     if (wmPtr->iconDataPtr != NULL) {
-	ckfree(wmPtr->iconDataPtr);
+	ckfree((char *)wmPtr->iconDataPtr);
     }
     if (wmPtr->hints.flags & IconPixmapHint) {
 	Tk_FreeBitmap(winPtr->display, wmPtr->hints.icon_pixmap);
@@ -2198,7 +2198,7 @@ WmIconphotoCmd(tkwin, winPtr, interp, objc, objv)
 	}
     }
     if (wmPtr->iconDataPtr != NULL) {
-	ckfree(wmPtr->iconDataPtr);
+	ckfree((char *)wmPtr->iconDataPtr);
 	wmPtr->iconDataPtr = NULL;
     }
     if (isDefault) {
