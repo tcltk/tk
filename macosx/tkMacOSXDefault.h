cb9e8849404eb0054d87ee2a6e8ed8665401b5c7
das <das>
2004-11-11 01:24:29 +0000

    * generic/tkMain.c: 	* macosx/tkMacOSXAppInit.c (removed): 	* macosx/Wish.pbproj/project.pbxproj: 	* macosx/tkMacOSXInit.c: 	* macosx/tkMacOSXInt.h: changes to make TkAqua dynamically loadable, 	enabling [package require Tk] from tclsh. Startup code from 	tkMacOSXAppInit.c moved into tkMacOSXInit.c, added code that 	notifies the window server that an unbundled executable is a full 	GUI application after loading Tk. [Patch 1035348]
    	* doc/wm.n: documented [wm attributes] on Mac OS X. [Bug 606665]
    	* macosx/tkMacOSXWm.c: implemented TIP 222 [wm attributes -alpha] on
    	Mac OS X. [Patch 892194]
    	WmIconbitmapCmd: adopted FSRef changes from [wm atttrs -titlepath].
    
    	* macosx/tkMacOSXSubwindows.c: synced spacing/formatting with
    	core-8-4-branch.
    
    	* generic/tkRectOval.c:
    	* macosx/README:
    	* macosx/tkMacOSXDefault.h:
    	* macosx/tkMacOSXDraw.c:
    	* macosx/tkMacOSXInit.c:
    	* macosx/tkMacOSXInt.h:
    	* macosx/tkMacOSXMenu.c:
    	* macosx/tkMacOSXWm.c: forward port from core-8-4-branch of Jim's
    	and my changes for CG drawing and [wm attributes] (corresponds to
    	8.4 changes dating from 09-18, 07-27, 07-24).
    
    	* macosx/tkMacOSXMouseEvent.c: endianness fixes.
    
    	* macosx/Wish.pbproj/project.pbxproj: corrected path to html help
    	inside framework.
    
    	* macosx/Makefile: prevent parallel make from building several
    	targets at the same time.

--- macosx/tkMacOSXDefault.h
+++ macosx/tkMacOSXDefault.h
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMacOSXDefault.h,v 1.5 2004/10/24 17:22:55 dkf Exp $
+ * RCS: @(#) $Id: tkMacOSXDefault.h,v 1.6 2004/11/11 01:24:32 das Exp $
  */
 
 #ifndef _TKMACDEFAULT
@@ -295,7 +295,12 @@
 #define DEF_MENU_SELECT_COLOR		"SystemMenuActive"
 #define DEF_MENU_SELECT_MONO		BLACK
 #define DEF_MENU_TAKE_FOCUS		"0"
-#define DEF_MENU_TEAROFF		"1"
+
+/*
+ * FIXME: Turn the default back to 1 when we make tearoff menus work again.
+ */
+ 
+#define DEF_MENU_TEAROFF		"0"
 #define DEF_MENU_TEAROFF_CMD		(char *) NULL
 #define DEF_MENU_TITLE			""
 #define DEF_MENU_TYPE			"normal"
