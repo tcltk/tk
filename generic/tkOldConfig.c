47589646e9c9ee099d9e28ebbf7608af12cac516
donal.k.fellows@manchester.ac.uk <dkf>
2005-08-12 15:24:07 +0000

    typo fixes

--- generic/tkOldConfig.c
+++ generic/tkOldConfig.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkOldConfig.c,v 1.13 2005/08/12 15:21:41 dkf Exp $
+ * RCS: @(#) $Id: tkOldConfig.c,v 1.14 2005/08/12 15:24:07 dkf Exp $
  */
 
 #include "tkPort.h"
@@ -132,7 +132,7 @@ Tk_ConfigureWidget(interp, tkwin, specs, argc, argv, widgRec, flags)
 	 */
 
 	for (specPtr=specs ; specPtr->type != TK_CONFIG_END ; specPtr++) {
-	    entryCount += sizeof(Tk_ConfigSpec);
+	    entrySpace += sizeof(Tk_ConfigSpec);
 	}
 
 	/*
@@ -165,7 +165,7 @@ Tk_ConfigureWidget(interp, tkwin, specs, argc, argv, widgRec, flags)
 	    specPtr->specFlags &= ~TK_CONFIG_OPTION_SPECIFIED;
 	}
     } else {
-	cachedSpecPtr = (Tk_ConfigSpec *) Tk_GetHashValue(entryPtr);
+	cachedSpecPtr = (Tk_ConfigSpec *) Tcl_GetHashValue(entryPtr);
     }
 
     /*
@@ -681,7 +681,7 @@ Tk_ConfigureInfo(interp, tkwin, specs, widgRec, argvName, flags)
     specCacheTablePtr = (Tcl_HashTable *)
 	    Tcl_GetAssocData(interp, "tkConfigSpec.threadTable", NULL);
     entryPtr = Tcl_FindHashEntry(specCacheTablePtr, (char *) specs);
-    cachedSpecPtr = (Tk_ConfigSpec *) Tk_GetHashValue(entryPtr);
+    cachedSpecPtr = (Tk_ConfigSpec *) Tcl_GetHashValue(entryPtr);
 
     /*
      * If information is only wanted for a single configuration spec, then
@@ -996,7 +996,7 @@ Tk_ConfigureValue(interp, tkwin, specs, widgRec, argvName, flags)
     specCacheTablePtr = (Tcl_HashTable *)
 	    Tcl_GetAssocData(interp, "tkConfigSpec.threadTable", NULL);
     entryPtr = Tcl_FindHashEntry(specCacheTablePtr, (char *) specs);
-    cachedSpecPtr = (Tk_ConfigSpec *) Tk_GetHashValue(entryPtr);
+    cachedSpecPtr = (Tk_ConfigSpec *) Tcl_GetHashValue(entryPtr);
 
     specPtr = FindConfigSpec(interp, cachedSpecPtr, argvName, needFlags,
 	    hateFlags);
