beedaed2c06f509b1650a06b47fce13d6b1eae0a
das <das>
2005-11-27 06:53:13 +0000

    * macosx/tkMacOSXMenu.c: 	* macosx/tkMacOSXMenus.c: 	* macosx/tkMacOSXMenubutton.c: define OSX 10.3 or later only constants 	if necessary to allow compilation on OSX 10.2

--- macosx/tkMacOSXMenu.c
+++ macosx/tkMacOSXMenu.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMacOSXMenu.c,v 1.23 2005/11/27 06:47:25 das Exp $
+ * RCS: @(#) $Id: tkMacOSXMenu.c,v 1.24 2005/11/27 06:53:13 das Exp $
  */
 #include "tkMacOSXInt.h"
 #include "tkMenubutton.h"
@@ -18,18 +18,18 @@
 #include "tkMacOSXInt.h"
 #undef Status
 
-#if !defined(MAC_OS_X_VERSION_10_3) || \
-        (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_3)
-    /* Define constants only available on Mac OS X 10.3 or later */
-    #define kMenuAttrDoNotUseUserCommandKeys (1 << 7)
-#endif
-
 #define USE_TK_MDEF
 //#define USE_ATSU
 
 #include <Carbon/Carbon.h>
 #include "tkMacOSXDebug.h"
 #include <CoreFoundation/CFString.h>
+
+#if !defined(MAC_OS_X_VERSION_10_3) || \
+        (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_3)
+    /* Define constants only available on Mac OS X 10.3 or later */
+    #define kMenuAttrDoNotUseUserCommandKeys (1 << 7)
+#endif
 
 typedef struct MacMenu {
     MenuRef menuHdl;		/* The Menu Manager data structure. */
--- macosx/tkMacOSXMenus.c
+++ macosx/tkMacOSXMenus.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMacOSXMenus.c,v 1.8 2005/11/27 02:36:15 das Exp $
+ * RCS: @(#) $Id: tkMacOSXMenus.c,v 1.9 2005/11/27 06:53:13 das Exp $
  */
 
 #include "tk.h"
@@ -23,6 +23,12 @@
  */
 #undef Status
 #include <Carbon/Carbon.h>
+
+#if !defined(MAC_OS_X_VERSION_10_3) || \
+        (MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_3)
+    /* Define constants only available on Mac OS X 10.3 or later */
+    #define kMenuAttrDoNotUseUserCommandKeys (1 << 7)
+#endif
 
 #define kAppleMenu              256
 #define kAppleAboutItem         1
