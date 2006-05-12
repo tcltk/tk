0efe3aa4cd5dc9cb5481aebad65d74ce1556c765
das <das>
2006-05-12 18:17:48 +0000

    * generic/tkCanvWind.c (DisplayWinItem, WinItemRequestProc): ensure 	canvas window items are unmapped when canvas is unmapped. [Bug 940117]
    	* macosx/tkMacOSXSubwindows.c (TkMacOSXUpdateClipRgn): empty clip region
    	of unmapped windows to prevent any drawing into them or into their
    	children from becoming visible. [Bug 940117]
    
    	* macosx/tkMacOSXInt.h:         revert Jim's attempt of 2005-03-14 to
    	* macosx/tkMacOSXSubwindows.c:  fix Bug 940117 as it disables Map/Unmap
    	event propagation to children.  [Bug 1480105]
    
    	* macosx/tkMacOSXDraw.c (TkPutImage): handle tkPictureIsOpen flag, fixes
    	incorrect positioning of images with complex alpha on native buttons;
    	actual alpha blending is still broken in this situation. [Bug 1155596]
    
    	* macosx/tkMacOSXEvent.c (TkMacOSXProcessCommandEvent):
    	* macosx/tkMacOSXMenus.c (TkMacOSXInitMenus): workaround carbon bug with
    	key shortcut for 'Preferences' app menu item. [Bug 1481503]
    
    	* macosx/tkMacOSXKeyEvent.c (TkMacOSXProcessKeyboardEvent): only check
    	for HICommand menu item shortcuts in the application menu.
    
    	* macosx/tkMacOSXInt.h:       initialize keyboard layout setup in
    	* macosx/tkMacOSXInit.c:      TkpInit() rather than during handling of
    	* macosx/tkMacOSXKeyEvent.c:  first key down event.
    
    	* macosx/tkMacOSXDraw.c:        add optional debug code to flash clip
    	* macosx/tkMacOSXSubwindows.c:  regions during update or draw.

--- macosx/tkMacOSXMenus.c
+++ macosx/tkMacOSXMenus.c
@@ -6,11 +6,12 @@
  *
  * Copyright (c) 1995-1996 Sun Microsystems, Inc.
  * Copyright 2001, Apple Computer, Inc.
+ * Copyright (c) 2005-2006 Daniel A. Steffen <das@users.sourceforge.net>
  *
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMacOSXMenus.c,v 1.11 2006/04/28 06:02:49 das Exp $
+ * RCS: @(#) $Id: tkMacOSXMenus.c,v 1.12 2006/05/12 18:17:48 das Exp $
  */
 
 #include "tkMacOSXInt.h"
@@ -192,7 +193,16 @@ TkMacOSXInitMenus(
     if (TkMacOSXUseMenuID(kHMHelpMenuID) != TCL_OK) {
 	Tcl_Panic("Help menu ID %s is already in use!", kHMHelpMenuID);
     }
-    
+
+    /*
+     * Workarund a Carbon bug with kHICommandPreferences: the first call to
+     * IsMenuKeyEvent returns false for the preferences menu item key shorcut
+     * event (even if the corresponding menu item is dynamically enabled by a
+     * kEventCommandUpdateStatus handler), unless the kHICommandPreferences
+     * menu item has previously been enabled manually. [Bug 1481503]
+     */
+    EnableMenuCommand(NULL, kHICommandPreferences);
+
     DrawMenuBar();
     return;
 }
@@ -304,3 +314,8 @@ SourceDialog()
         Tcl_BackgroundError(gInterp);
     }           
 }
+/*
+ * Local Variables:
+ * coding: macintosh;
+ * End:
+ */
