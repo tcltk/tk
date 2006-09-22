373f1922f5cb50d54a325a34060549133ccc7e6b
andreask@activestate.com <andreas_kupries>
2006-09-22 19:01:49 +0000

    * generic/tkConsole.c: TIP #268 update regarding registered package 	* generic/tkMain.c:    version, now using full patchlevel instead of 	* generic/tkWindow.c:  major.minor 	* library/tk.tcl: 	* unix/configure: 	* unix/Makefile.in: 	* unix/tcl.m4: 	* win/configure: 	* win/Makefile.in: 	* win/makefile.vc: 	* win/rules.vc: 	* win/tcl.m4:

--- generic/tkMain.c
+++ generic/tkMain.c
@@ -13,7 +13,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMain.c,v 1.15.2.3 2006/06/09 15:41:29 dgp Exp $
+ * RCS: @(#) $Id: tkMain.c,v 1.15.2.4 2006/09/22 19:01:50 andreas_kupries Exp $
  */
 
 #include <ctype.h>
@@ -117,7 +117,7 @@ Tk_MainEx(argc, argv, appInitProc, interp)
      * really only an issue when Tk is loaded dynamically.
      */
 
-    if (Tcl_InitStubs(interp, TCL_VERSION, 1) == NULL) {
+    if (Tcl_InitStubs(interp, TCL_PATCH_LEVEL, 1) == NULL) {
 	abort();
     }
 
