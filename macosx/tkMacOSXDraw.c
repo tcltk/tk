63d22e7e2679eb691c14232408d686e51801b1dc
das <das>
2006-10-16 16:25:38 +0000

    whitespace

--- macosx/tkMacOSXDraw.c
+++ macosx/tkMacOSXDraw.c
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMacOSXDraw.c,v 1.17 2006/10/16 15:35:28 das Exp $
+ * RCS: @(#) $Id: tkMacOSXDraw.c,v 1.18 2006/10/16 16:25:38 das Exp $
  */
 
 #include "tkMacOSXInt.h"
@@ -649,7 +649,7 @@ XDrawLines(
 	CGContextStrokePath(outContext);
 	TkMacOSXReleaseCGContext(macWin, destPort, &outContext);
     } else {
-	int o = - (lw / 2);
+	int o = -lw/2;
 
 	TkMacOSXSetUpGraphicsPort(gc, destPort);
 	ShowPen();
@@ -725,7 +725,7 @@ void XDrawSegments(
 	}
 	TkMacOSXReleaseCGContext(macWin, destPort, &outContext);
     } else {
-	int o = - (lw / 2);
+	int o = -lw/2;
 
 	TkMacOSXSetUpGraphicsPort(gc, destPort);
 	ShowPen();
@@ -1357,7 +1357,7 @@ XFillArc(
     } else {
 	Rect theRect;
 	short start, extent;
-	int o = - (lw / 2);
+	int o = -lw/2;
 	PolyHandle polygon;
 	double sin1, cos1, sin2, cos2, angle;
 	double boxWidth, boxHeight;
@@ -1499,7 +1499,7 @@ XFillArcs(
     } else {
 	Rect theRect;
 	short start, extent;
-	int o = - (lw / 2);
+	int o = -lw/2;
 	PolyHandle polygon;
 	double sin1, cos1, sin2, cos2, angle;
 	double boxWidth, boxHeight;
