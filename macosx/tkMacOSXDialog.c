04dc2f3545a8f9058967db86e645eab02de111da
das <das>
2007-07-25 05:24:54 +0000

    * macosx/tkMacOSXDialog.c (NavServicesGetFile): reset interp result on 	nav dialog cancel. [Bug 1743786]

--- macosx/tkMacOSXDialog.c
+++ macosx/tkMacOSXDialog.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMacOSXDialog.c,v 1.4.2.16 2007/06/29 03:22:01 das Exp $
+ * RCS: @(#) $Id: tkMacOSXDialog.c,v 1.4.2.17 2007/07/25 05:25:01 das Exp $
  */
 
 #include "tkMacOSXPrivate.h"
@@ -944,6 +944,7 @@ NavServicesGetFile(
 	Tcl_SetObjResult(interp, theResult);
 	result = TCL_OK;
     } else if (err == userCanceledErr) {
+	Tcl_ResetResult(interp);
 	result = TCL_OK;
     }
 
