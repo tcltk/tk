f9805f9bd3995c8d222e1dc10728043c70e147e7
donal.k.fellows@manchester.ac.uk <dkf>
2007-08-18 15:39:03 +0000

    Update to reflect the fact that this is the 8.5 test suite

--- tests/all.tcl
+++ tests/all.tcl
@@ -8,15 +8,15 @@
 #
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
-# 
-# RCS: @(#) $Id: all.tcl,v 1.9 2005/02/14 09:21:13 dkf Exp $
+#
+# RCS: @(#) $Id: all.tcl,v 1.10 2007/08/18 15:39:03 dkf Exp $
 
-package require Tcl 8.4
-package require tcltest 2.1
+package require Tcl 8.5
+package require tcltest 2.2
 package require Tk ;# This is the Tk test suite; fail early if no Tk!
+tcltest::configure {*}$argv
 tcltest::configure -testdir [file join [pwd] [file dirname [info script]]]
 tcltest::configure -loadfile \
 	[file join [tcltest::testsDirectory] constraints.tcl]
 tcltest::configure -singleproc 1
-eval tcltest::configure $argv
 tcltest::runAllTests
