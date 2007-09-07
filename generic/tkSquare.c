f2187488ed22d99a593b23ae96865ea90c0b46f4
dgp@users.sourceforge.net <dgp>
2007-09-07 01:50:44 +0000

    Disable __NO_OLD_CONFIG configuration enabled by latest commit

--- generic/tkSquare.c
+++ generic/tkSquare.c
@@ -12,10 +12,12 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkSquare.c,v 1.8 2007/09/07 00:34:53 dgp Exp $
+ * RCS: @(#) $Id: tkSquare.c,v 1.9 2007/09/07 01:50:44 dgp Exp $
  */
 
+#if 0
 #define __NO_OLD_CONFIG
+#endif
 #include "tkInt.h"
 
 /*
