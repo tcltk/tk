b905459d905f0a6649ba77398fba528c7597d44c
dgp <dgp@users.sourceforge.net>
2007-09-07 01:51:28 +0000

    merge update from HEAD

--- generic/tkSquare.c
+++ generic/tkSquare.c
@@ -12,10 +12,12 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkSquare.c,v 1.7.2.1 2007/09/07 01:25:36 dgp Exp $
+ * RCS: @(#) $Id: tkSquare.c,v 1.7.2.2 2007/09/07 01:51:28 dgp Exp $
  */
 
+#if 0
 #define __NO_OLD_CONFIG
+#endif
 #include "tkInt.h"
 
 /*
