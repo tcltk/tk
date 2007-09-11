a0f56a02b3c56a1c5544ce9cd47ceee121392271
dgp <dgp@users.sourceforge.net>
2007-09-11 17:46:39 +0000

    * generic/tkConsole.c:  Revised calls to Tcl_InitStubs() and         * generic/tkMain.c:     [package require Tcl] so that Tk Says What         * generic/tkWindow.c:   It Means using the new facilties of [package]         * library/tk.tcl:       in Tcl 8.5 about what version(s) of Tcl it         * unix/Makefile.in:     is willing to work with.  [Bug 1578344].         * win/Makefile.in:         * win/makefile.vc:

--- library/tk.tcl
+++ library/tk.tcl
@@ -3,7 +3,7 @@
 # Initialization script normally executed in the interpreter for each
 # Tk-based application.  Arranges class bindings for widgets.
 #
-# RCS: @(#) $Id: tk.tcl,v 1.63 2007/05/18 19:21:59 dgp Exp $
+# RCS: @(#) $Id: tk.tcl,v 1.64 2007/09/11 17:46:41 dgp Exp $
 #
 # Copyright (c) 1992-1994 The Regents of the University of California.
 # Copyright (c) 1994-1996 Sun Microsystems, Inc.
@@ -12,8 +12,11 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 
-# Insist on running with compatible versions of Tcl and Tk.
-package require -exact Tcl 8.5a7
+package require Tcl 8.5	;# Guard against [source] in an 8.4- interp
+			;# before using 8.5 [package] features.
+# Insist on running with compatible version of Tcl
+package require Tcl 8.5a7-8.6
+# Verify that we have Tk binary and script components from the same release
 package require -exact Tk  8.5a7
 
 # Create a ::tk namespace
