0835552e692f21bd3b940a8de5bc70fb1d399a4e
patthoyts <patthoyts@users.sourceforge.net>
2007-09-18 10:39:50 +0000

    Made ttk/all.tcl be the same as tk's all.tcl and make use of file normalize

--- tests/all.tcl
+++ tests/all.tcl
@@ -9,13 +9,13 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
-# RCS: @(#) $Id: all.tcl,v 1.11 2007/08/21 20:30:27 patthoyts Exp $
+# RCS: @(#) $Id: all.tcl,v 1.12 2007/09/18 10:39:52 patthoyts Exp $
 
 package require Tcl 8.5
 package require tcltest 2.2
 package require Tk ;# This is the Tk test suite; fail early if no Tk!
 tcltest::configure {*}$argv
-tcltest::configure -testdir [file join [pwd] [file dirname [info script]]]
+tcltest::configure -testdir [file normalize [file dirname [info script]]]
 tcltest::configure -loadfile \
 	[file join [tcltest::testsDirectory] constraints.tcl]
 tcltest::configure -singleproc 1
