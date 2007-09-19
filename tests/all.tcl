866c5d22436853398b078226064f655c6dd69eea
dgp <dgp@users.sourceforge.net>
2007-09-19 17:28:21 +0000

    merge updates from HEAD

--- tests/all.tcl
+++ tests/all.tcl
@@ -9,13 +9,13 @@
 # See the file "license.terms" for information on usage and redistribution
 # of this file, and for a DISCLAIMER OF ALL WARRANTIES.
 #
-# RCS: @(#) $Id: all.tcl,v 1.9.2.1 2007/09/04 17:44:23 dgp Exp $
+# RCS: @(#) $Id: all.tcl,v 1.9.2.2 2007/09/19 17:28:25 dgp Exp $
 
 package require Tcl 8.5
 package require tcltest 2.2
 package require Tk ;# This is the Tk test suite; fail early if no Tk!
 tcltest::configure {*}$argv
-tcltest::configure -testdir [file join [pwd] [file dirname [info script]]]
+tcltest::configure -testdir [file normalize [file dirname [info script]]]
 tcltest::configure -loadfile \
 	[file join [tcltest::testsDirectory] constraints.tcl]
 tcltest::configure -singleproc 1
