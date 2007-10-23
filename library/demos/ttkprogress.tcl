c150191e81c5cd2d652dcb45fd2ab669032657c3
das <das>
2007-10-23 06:31:16 +0000

    * library/demos/combo.tcl:	Aqua GOOBE. 	* library/demos/toolbar.tcl: 	* library/demos/tree.tcl: 	* library/demos/ttknote.tcl: 	* library/demos/ttkprogress.tcl: 	* library/demos/widget:

--- library/demos/ttkprogress.tcl
+++ library/demos/ttkprogress.tcl
@@ -2,7 +2,7 @@
 #
 # This demonstration script creates several progress bar widgets.
 #
-# RCS: @(#) $Id: ttkprogress.tcl,v 1.1 2007/10/22 14:21:16 dkf Exp $
+# RCS: @(#) $Id: ttkprogress.tcl,v 1.2 2007/10/23 06:31:16 das Exp $
 
 if {![info exists widgetDemo]} {
     error "This script should be run from the \"widget\" demo."
@@ -18,8 +18,16 @@ wm title $w "Progress Bar Demonstration"
 wm iconname $w "ttkprogress"
 positionWindow $w
 
-label $w.msg -font $font -wraplength 4i -justify left -text "Below are two progress bars. The top one is a \u201Cdeterminate\u201D progress bar, which is used for showing how far through a defined task the program has got. The bottom one is an \u201Cindeterminate\u201D progress bar, which is used to show that the program is busy but does not know how long for. Both are run here in self-animated mode, which can be turned on and off using the buttons underneath."
-grid $w.msg - -sticky ew
+ttk::label $w.msg -font $font -wraplength 4i -justify left -text "Below are two progress bars. The top one is a \u201Cdeterminate\u201D progress bar, which is used for showing how far through a defined task the program has got. The bottom one is an \u201Cindeterminate\u201D progress bar, which is used to show that the program is busy but does not know how long for. Both are run here in self-animated mode, which can be turned on and off using the buttons underneath."
+pack $w.msg -side top -fill x
+
+## See Code / Dismiss buttons
+set btns [addSeeDismiss $w.buttons $w]
+pack $btns -side bottom -fill x
+
+ttk::frame $w.f
+pack $w.f -fill both -expand 1
+set w $w.f
 
 proc doBars {op args} {
     foreach w $args {
@@ -38,9 +46,4 @@ grid $w.p2 - -pady 5 -padx 10
 grid $w.start $w.stop -padx 10 -pady 5
 grid configure $w.start -sticky e
 grid configure $w.stop -sticky w
-
-## See Code / Dismiss buttons
-set btns [addSeeDismiss $w.buttons $w]
-grid $btns - - -sticky ews
-grid columnconfigure $w 2 -weight 1
-grid rowconfigure $w $btns -weight 1
+grid columnconfigure $w all -weight 1
