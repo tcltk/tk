3cb42e29bc4c5c1b9afd7d54df9e1ea1613d64bd
dgp <dgp@users.sourceforge.net>
2007-10-24 12:59:29 +0000

    merge updates from HEAD

--- library/demos/ttkmenu.tcl
+++ library/demos/ttkmenu.tcl
@@ -3,7 +3,7 @@
 # This demonstration script creates a toplevel window containing several Ttk
 # menubutton widgets.
 #
-# RCS: @(#) $Id: ttkmenu.tcl,v 1.1.2.2 2007/10/19 14:35:33 dgp Exp $
+# RCS: @(#) $Id: ttkmenu.tcl,v 1.1.2.3 2007/10/24 12:59:33 dgp Exp $
 
 if {![info exists widgetDemo]} {
     error "This script should be run from the \"widget\" demo."
@@ -47,6 +47,7 @@ foreach theme [ttk::themes] {
 }
 
 pack [ttk::frame $w.f] -fill x
+pack [ttk::frame $w.f1] -fill both -expand yes
 lower $w.f
 
 grid anchor $w.f center
