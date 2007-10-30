5a55e631df3cb0ec397a08d209ef3495c7b76510
hobbs <hobbs>
2007-10-30 18:52:06 +0000

    * library/entry.tcl: don't error with Clear event. [Bug 1509288]

--- library/entry.tcl
+++ library/entry.tcl
@@ -3,7 +3,7 @@
 # This file defines the default bindings for Tk entry widgets and provides
 # procedures that help in implementing those bindings.
 #
-# RCS: @(#) $Id: entry.tcl,v 1.24 2005/07/25 09:06:00 dkf Exp $
+# RCS: @(#) $Id: entry.tcl,v 1.25 2007/10/30 18:52:06 hobbs Exp $
 #
 # Copyright (c) 1992-1994 The Regents of the University of California.
 # Copyright (c) 1994-1997 Sun Microsystems, Inc.
@@ -60,7 +60,8 @@ bind Entry <<Paste>> {
     }
 }
 bind Entry <<Clear>> {
-    %W delete sel.first sel.last
+    # ignore if there is no selection
+    catch { %W delete sel.first sel.last }
 }
 bind Entry <<PasteSelection>> {
     if {$tk_strictMotif || ![info exists tk::Priv(mouseMoved)]
