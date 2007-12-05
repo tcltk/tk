56eab6d60509309df89b7213d50493c7f6627b99
hobbs <hobbs>
2007-12-05 19:07:59 +0000

    * win/tkWinButton.c, win/tkWinDialog.c: use SetWindowLongPtr and 	* win/tkWinScrlbr.c, win/tkWinWm.c:     GetWindowLongPtr only. 	* win/ttkWinMonitor.c:

--- win/tkWinButton.c
+++ win/tkWinButton.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkWinButton.c,v 1.31 2007/01/11 15:35:40 dkf Exp $
+ * RCS: @(#) $Id: tkWinButton.c,v 1.32 2007/12/05 19:08:00 hobbs Exp $
  */
 
 #define OEMRESOURCE
@@ -274,13 +274,8 @@ CreateProc(
 	    parent, NULL, Tk_GetHINSTANCE(), NULL);
     SetWindowPos(butPtr->hwnd, HWND_TOP, 0, 0, 0, 0,
 		    SWP_NOACTIVATE | SWP_NOMOVE | SWP_NOSIZE);
-#ifdef _WIN64
     butPtr->oldProc = (WNDPROC)SetWindowLongPtr(butPtr->hwnd, GWLP_WNDPROC,
 	    (LONG_PTR) ButtonProc);
-#else
-    butPtr->oldProc = (WNDPROC)SetWindowLong(butPtr->hwnd, GWL_WNDPROC,
-	    (DWORD) ButtonProc);
-#endif
 
     window = Tk_AttachHWND(tkwin, butPtr->hwnd);
     return window;
@@ -310,11 +305,7 @@ TkpDestroyButton(
     HWND hwnd = winButPtr->hwnd;
 
     if (hwnd) {
-#ifdef _WIN64
 	SetWindowLongPtr(hwnd, GWLP_WNDPROC, (LONG_PTR) winButPtr->oldProc);
-#else
-	SetWindowLong(hwnd, GWL_WNDPROC, (DWORD) winButPtr->oldProc);
-#endif
     }
 }
 
