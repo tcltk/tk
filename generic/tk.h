6d9755c06e1280e8c302839f21ee2a0b7b144b54
dgp <dgp@users.sourceforge.net>
2008-01-02 19:07:16 +0000

    * generic/tk.h:         Bump version number to 8.5.1b1 to distinguish         * library/tk.tcl:       CVS development snapshots from the 8.5.0 and         * unix/configure.in:    8.5.1 releases.         * unix/tk.spec:         * win/configure.in:
            * unix/configure:       autoconf (2.59)
            * win/configure:

--- generic/tk.h
+++ generic/tk.h
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tk.h,v 1.106 2007/12/13 15:24:13 dgp Exp $
+ * RCS: @(#) $Id: tk.h,v 1.107 2008/01/02 19:07:18 dgp Exp $
  */
 
 #ifndef _TK
@@ -53,10 +53,10 @@ extern "C" {
 #define TK_MAJOR_VERSION	8
 #define TK_MINOR_VERSION	5
 #define TK_RELEASE_LEVEL	TCL_FINAL_RELEASE
-#define TK_RELEASE_SERIAL	0
+#define TK_RELEASE_SERIAL	1
 
 #define TK_VERSION		"8.5"
-#define TK_PATCH_LEVEL		"8.5.0"
+#define TK_PATCH_LEVEL		"8.5.1b1"
 
 /*
  * A special definition used to allow this header file to be included from
--- library/tk.tcl
+++ library/tk.tcl
@@ -3,7 +3,7 @@
 # Initialization script normally executed in the interpreter for each
 # Tk-based application.  Arranges class bindings for widgets.
 #
-# RCS: @(#) $Id: tk.tcl,v 1.70 2007/12/13 15:26:27 dgp Exp $
+# RCS: @(#) $Id: tk.tcl,v 1.71 2008/01/02 19:07:18 dgp Exp $
 #
 # Copyright (c) 1992-1994 The Regents of the University of California.
 # Copyright (c) 1994-1996 Sun Microsystems, Inc.
@@ -17,7 +17,7 @@ package require Tcl 8.5	;# Guard against [source] in an 8.4- interp
 # Insist on running with compatible version of Tcl
 package require Tcl 8.5.0-8.6
 # Verify that we have Tk binary and script components from the same release
-package require -exact Tk  8.5.0
+package require -exact Tk  8.5.1b1
 
 # Create a ::tk namespace
 namespace eval ::tk {
