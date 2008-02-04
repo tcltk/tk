fea5d9f43f0fdae3a32abe768c845ed4efe625cb
dgp <dgp@users.sourceforge.net>
2008-02-04 17:32:39 +0000

    *** 8.5.1 TAGGED FOR RELEASE ***
            * generic/tk.h:         Bump to 8.5.1 for release.
            * library/tk.tcl:
            * unix/configure.in:
            * unix/tk.spec:
            * win/configure.in:
    
            * unix/configure:       autoconf-2.59
            * win/configure:

--- generic/tk.h
+++ generic/tk.h
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tk.h,v 1.107 2008/01/02 19:07:18 dgp Exp $
+ * RCS: @(#) $Id: tk.h,v 1.108 2008/02/04 17:32:39 dgp Exp $
  */
 
 #ifndef _TK
@@ -56,7 +56,7 @@ extern "C" {
 #define TK_RELEASE_SERIAL	1
 
 #define TK_VERSION		"8.5"
-#define TK_PATCH_LEVEL		"8.5.1b1"
+#define TK_PATCH_LEVEL		"8.5.1"
 
 /*
  * A special definition used to allow this header file to be included from
