28d9ad3f1e7d999b52639e26e74b4e6bfacff411
dgp@users.sourceforge.net <dgp>
2008-04-11 19:00:45 +0000

    * generic/tk.h:         Bump version number to 8.5.3b1 to distinguish         * library/tk.tcl:       CVS development snapshots from the 8.5.2 and         * unix/configure.in:    8.5.3 releases.         * unix/tk.spec:         * win/configure.in:
            * unix/configure:       autoconf (2.59)
            * win/configure:

--- generic/tk.h
+++ generic/tk.h
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tk.h,v 1.109 2008/03/28 18:13:39 dgp Exp $
+ * RCS: @(#) $Id: tk.h,v 1.109.2.1 2008/04/11 19:00:47 dgp Exp $
  */
 
 #ifndef _TK
@@ -53,10 +53,10 @@ extern "C" {
 #define TK_MAJOR_VERSION	8
 #define TK_MINOR_VERSION	5
 #define TK_RELEASE_LEVEL	TCL_FINAL_RELEASE
-#define TK_RELEASE_SERIAL	2
+#define TK_RELEASE_SERIAL	3
 
 #define TK_VERSION		"8.5"
-#define TK_PATCH_LEVEL		"8.5.2"
+#define TK_PATCH_LEVEL		"8.5.3b1"
 
 /*
  * A special definition used to allow this header file to be included from
