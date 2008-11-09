be295fda6d6cb3d7d89199d7dc4f808ebf27491f
nijtmans <nijtmans>
2008-11-09 20:51:28 +0000

    make all Tk_CustomOption tables const and remove unnecessary type cast

--- generic/tkCanvBmap.c
+++ generic/tkCanvBmap.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkCanvBmap.c,v 1.15 2008/04/27 22:38:55 dkf Exp $
+ * RCS: @(#) $Id: tkCanvBmap.c,v 1.16 2008/11/09 20:51:28 nijtmans Exp $
  */
 
 #include <stdio.h>
@@ -43,13 +43,11 @@ typedef struct BitmapItem  {
  * Information used for parsing configuration specs:
  */
 
-static Tk_CustomOption stateOption = {
-    (Tk_OptionParseProc *) TkStateParseProc,
-    TkStatePrintProc, (ClientData) 2
+static const Tk_CustomOption stateOption = {
+    TkStateParseProc, TkStatePrintProc, (ClientData) 2
 };
-static Tk_CustomOption tagsOption = {
-    (Tk_OptionParseProc *) Tk_CanvasTagsParseProc,
-    Tk_CanvasTagsPrintProc, (ClientData) NULL
+static const Tk_CustomOption tagsOption = {
+    Tk_CanvasTagsParseProc, Tk_CanvasTagsPrintProc, (ClientData) NULL
 };
 
 static Tk_ConfigSpec configSpecs[] = {
