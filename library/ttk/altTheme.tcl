216f96d49a15f71cc5fd40aed613bcfb4f0f002a
patthoyts <patthoyts@users.sourceforge.net>
2008-11-29 00:43:48 +0000

    Use a styled frame around the popdown listbox so we can adjust the border for each theme as needed.

--- library/ttk/altTheme.tcl
+++ library/ttk/altTheme.tcl
@@ -1,5 +1,5 @@
 #
-# $Id: altTheme.tcl,v 1.8 2008/11/01 15:34:24 patthoyts Exp $
+# $Id: altTheme.tcl,v 1.9 2008/11/29 00:43:48 patthoyts Exp $
 #
 # Ttk widget set: Alternate theme
 #
@@ -59,6 +59,8 @@ namespace eval ttk::theme::alt {
 	ttk::style configure TCombobox -padding 1
 	ttk::style map TCombobox -fieldbackground \
 		[list readonly $colors(-frame) disabled $colors(-frame)]
+	ttk::style configure ComboboxPopdownFrame \
+	    -relief solid -borderwidth 1
 
 	ttk::style configure TSpinbox -arrowsize 10 -padding {2 0 10 0}
 	ttk::style map TSpinbox -fieldbackground \
