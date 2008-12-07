2fda008258648b130dc7884a0be2da3b9ad0f38b
jenglish <jenglish@flightlab.com>
2008-12-07 21:24:11 +0000

    ttk::spinbox: Add cross-platform MouseWheel bindings. Factored out [ttk::bindMouseWheel] procedure.

--- library/ttk/spinbox.tcl
+++ library/ttk/spinbox.tcl
@@ -1,5 +1,5 @@
 #
-# $Id: spinbox.tcl,v 1.2 2008/12/07 18:42:55 jenglish Exp $
+# $Id: spinbox.tcl,v 1.3 2008/12/07 21:24:12 jenglish Exp $
 #
 # ttk::spinbox bindings
 #
@@ -19,22 +19,20 @@ bind TSpinbox <ButtonRelease-1> 	{ ttk::spinbox::Release %W }
 bind TSpinbox <Double-Button-1> 	{ ttk::spinbox::DoubleClick %W %x %y }
 bind TSpinbox <Triple-Button-1> 	{} ;# disable TEntry triple-click
 
-bind TSpinbox <KeyPress-Up>		{ ttk::spinbox::Spin %W +1 }
-bind TSpinbox <KeyPress-Down> 		{ ttk::spinbox::Spin %W -1 }
+bind TSpinbox <KeyPress-Up>		{ event generate %W <<Increment>> }
+bind TSpinbox <KeyPress-Down> 		{ event generate %W <<Decrement>> }
 
 bind TSpinbox <<Increment>>		{ ttk::spinbox::Spin %W +1 }
 bind TSpinbox <<Decrement>> 		{ ttk::spinbox::Spin %W -1 }
 
-
-# @@@ x-plat
-bind TSpinbox <MouseWheel> { ttk::spinbox::Spin %W [expr {%D/-120}] }
+ttk::bindMouseWheel TSpinbox 		[list ttk::spinbox::MouseWheel %W]
 
 ## Motion --
 #	Sets cursor.
 #
 proc ttk::spinbox::Motion {w x y} {
     if {   [$w identify $x $y] eq "textarea"
-        && [$w instate {!readonly !disabled}] 
+        && [$w instate {!readonly !disabled}]
     } {
 	ttk::setCursor $w text
     } else {
@@ -53,7 +51,7 @@ proc ttk::spinbox::Press {w x y} {
         *uparrow 	{ ttk::Repeatedly event generate $w <<Increment>> }
 	*leftarrow	-
         *downarrow	{ ttk::Repeatedly event generate $w <<Decrement>> }
-	*spinbutton { 
+	*spinbutton {
 	    if {$y * 2 >= [winfo height $w]} {
 	    	set event <<Decrement>>
 	    } else {
@@ -64,7 +62,7 @@ proc ttk::spinbox::Press {w x y} {
     }
 }
 
-## DoubleClick -- 
+## DoubleClick --
 #	Select all if over the text area; otherwise same as Press.
 #
 proc ttk::spinbox::DoubleClick {w x y} {
@@ -80,23 +78,47 @@ proc ttk::spinbox::Release {w} {
     ttk::CancelRepeat
 }
 
+## MouseWheel --
+#	Mousewheel callback.  Turn these into <<Increment>> (-1, up)
+# 	or <<Decrement> (+1, down) events.
+#
+proc ttk::spinbox::MouseWheel {w dir} {
+    if {$dir < 0} {
+	event generate $w <<Increment>>
+    } else {
+	event generate $w <<Decrement>>
+    }
+}
+
+## SelectAll --
+#	Select widget contents.
+#
 proc ttk::spinbox::SelectAll {w} {
     $w selection range 0 end
     $w icursor end
 }
 
+## Limit --
+#	Limit $v to lie between $min and $max
+#
 proc ttk::spinbox::Limit {v min max} {
     if {$v < $min} { return $min }
     if {$v > $max} { return $max }
     return $v
 }
 
+## Wrap --
+#	Adjust $v to lie between $min and $max, wrapping if out of bounds.
+#
 proc ttk::spinbox::Wrap {v min max} {
     if {$v < $min} { return $max }
     if {$v > $max} { return $min }
     return $v
 }
 
+## Adjust --
+#	Limit or wrap spinbox value depending on -wrap.
+#
 proc ttk::spinbox::Adjust {w v min max} {
     if {[$w cget -wrap]} {
 	return [Wrap $v $min $max]
@@ -105,6 +127,12 @@ proc ttk::spinbox::Adjust {w v min max} {
     }
 }
 
+## Spin --
+#	Handle <<Increment>> and <<Decrement>> events.
+#	If -values is specified, cycle through the list.
+#	Otherwise cycle through numeric range based on
+#	-from, -to, and -increment.
+#
 proc ttk::spinbox::Spin {w dir} {
     set nvalues [llength [set values [$w cget -values]]]
     set value [$w get]
@@ -124,13 +152,16 @@ proc ttk::spinbox::Spin {w dir} {
     uplevel #0 [$w cget -command]
 }
 
+## FormatValue --
+#	Reformat numeric value based on -format.
+#
 proc ttk::spinbox::FormatValue {w val} {
     set fmt [$w cget -format]
     if {$fmt eq ""} {
 	# Try to guess a suitable -format based on -increment.
 	set delta [expr {abs([$w cget -increment])}]
         if {0 < $delta && $delta < 1} {
-	    # NB: This guesses wrong if -increment has more than 1 
+	    # NB: This guesses wrong if -increment has more than 1
 	    # significant digit itself, e.g., -increment 0.25
 	    set nsd [expr {int(ceil(-log10($delta)))}]
 	    set fmt "%.${nsd}f"
