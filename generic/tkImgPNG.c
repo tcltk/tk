474bd9cda9c65a64819dd2992b1eb7cf3de2ca24
dkf <donal.k.fellows@manchester.ac.uk>
2008-12-28 22:13:30 +0000

    Applied Michael Kirkham's fix to make all PngSuite images work.

--- generic/tkImgPNG.c
+++ generic/tkImgPNG.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkImgPNG.c,v 1.1 2008/12/28 13:08:39 dkf Exp $
+ * RCS: @(#) $Id: tkImgPNG.c,v 1.2 2008/12/28 22:13:31 dkf Exp $
  */
 
 #include "tkInt.h"
@@ -2054,14 +2054,13 @@ ReadIDAT(
 	    }
 
 	    inputObj = Tcl_NewObj();
-	    Tcl_SetByteArrayLength(inputObj, PNG_BLOCK_SZ);
 	    Tcl_IncrRefCount(inputObj);
+	    inputPtr = Tcl_SetByteArrayLength(inputObj, blockSz);
 
 	    /*
 	     * Read the next bit of IDAT chunk data, up to read buffer size.
 	     */
 
-	    inputPtr = Tcl_GetByteArrayFromObj(inputObj, NULL);
 	    if (ReadData(interp, pngPtr, inputPtr, blockSz,
 		    &crc) == TCL_ERROR) {
 		Tcl_DecrRefCount(inputObj);
