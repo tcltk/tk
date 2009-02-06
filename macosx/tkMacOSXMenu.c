e29554b6257faae41b36f4ceb4bb97d3d61ed7a4
das <das>
2009-02-06 08:12:51 +0000

    whitespace

--- macosx/tkMacOSXMenu.c
+++ macosx/tkMacOSXMenu.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkMacOSXMenu.c,v 1.45.2.1 2008/12/07 16:44:49 das Exp $
+ * RCS: @(#) $Id: tkMacOSXMenu.c,v 1.45.2.2 2009/02/06 08:12:51 das Exp $
  */
 
 #include "tkMacOSXPrivate.h"
@@ -447,7 +447,7 @@ TkMacOSXUseMenuID(
  */
 
 int
- TkMacOSXGetNewMenuID(
+TkMacOSXGetNewMenuID(
     Tcl_Interp *interp,		/* Used for error reporting */
     TkMenu *menuPtr,		/* The menu we are working with */
     int cascade,		/* 0 if we are working with a normal menu;
