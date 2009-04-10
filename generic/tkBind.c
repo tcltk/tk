6dbb47ffe6bce7aa46c0447ceb739246ca93c80b
das <das>
2009-04-10 15:54:07 +0000

    * generic/tkBind.c (HandleEventGenerate): ensure window exists before 	generating an event for it (fixes testsuite crash).

--- generic/tkBind.c
+++ generic/tkBind.c
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkBind.c,v 1.52 2009/01/09 07:03:31 nijtmans Exp $
+ * RCS: @(#) $Id: tkBind.c,v 1.53 2009/04/10 15:54:07 das Exp $
  */
 
 #include "tkInt.h"
@@ -3325,6 +3325,7 @@ HandleEventGenerate(
     event.xany.serial = NextRequest(Tk_Display(tkwin));
     event.xany.send_event = False;
     if (windowName[0]) {
+	Tk_MakeWindowExist(tkwin);
 	event.xany.window = Tk_WindowId(tkwin);
     } else {
 	event.xany.window =
