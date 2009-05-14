06e383936a84f6aa562302eed4eee286abf36130
patthoyts@users.sourceforge.net <patthoyts>
2009-05-14 00:53:04 +0000

    Backported support for the Vista theme. This requires the vsapi element engine, the hover state and the theme script definition.

--- generic/ttk/ttkTheme.h
+++ generic/ttk/ttkTheme.h
@@ -1,4 +1,4 @@
-/* $Id: ttkTheme.h,v 1.13 2007/12/13 15:26:26 dgp Exp $
+/* $Id: ttkTheme.h,v 1.13.2.1 2009/05/14 00:53:04 patthoyts Exp $
  * Copyright (c) 2003 Joe English.  Freely redistributable.
  *
  * Declarations for Tk theme engine.
@@ -48,7 +48,7 @@ typedef unsigned int Ttk_State;
 #define TTK_STATE_ALTERNATE	(1<<6)
 #define TTK_STATE_INVALID	(1<<7)
 #define TTK_STATE_READONLY 	(1<<8)
-#define TTK_STATE_USER7 	(1<<9)
+#define TTK_STATE_HOVER		(1<<9)
 #define TTK_STATE_USER6		(1<<10)
 #define TTK_STATE_USER5		(1<<11)
 #define TTK_STATE_USER4		(1<<12)
