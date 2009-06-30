4038849e8f35ee9b8e310f5c816bdd3795724c9c
das <das>
2009-06-30 14:21:44 +0000

    s/Tcl_PanicEx/TclPanic/; improve clang assert Tcl_Panic macrology

--- generic/tkInt.h
+++ generic/tkInt.h
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: $Id: tkInt.h,v 1.104 2009/06/30 00:56:29 das Exp $
+ * RCS: $Id: tkInt.h,v 1.105 2009/06/30 14:21:44 das Exp $
  */
 
 #ifndef _TKINT
@@ -988,9 +988,9 @@ MODULE_SCOPE Tcl_HashTable	tkPredefBitmapTable;
 #if defined(PURIFY) && defined(__clang__) && !defined(CLANG_ASSERT)
 #include <assert.h>
 #define CLANG_ASSERT(x) assert(x)
-#define Tcl_PanicEx Tcl_Panic
+#define TclPanic Tcl_Panic
 #undef Tcl_Panic
-#define Tcl_Panic(f, ...) Tcl_PanicEx(f,##__VA_ARGS__); CLANG_ASSERT(0)
+#define Tcl_Panic(f, ...) do { TclPanic(f,##__VA_ARGS__); CLANG_ASSERT(0); } while(0)
 #elif !defined(CLANG_ASSERT)
 #define CLANG_ASSERT(x)
 #endif
