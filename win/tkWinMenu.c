785e788bee72800805bf2e664205150b07ceff58
hobbs <hobbs>
2009-09-14 23:41:42 +0000

    * generic/tkMenuDraw.c (TkPostSubmenu): Fix reposting of submenu in 	* win/tkWinMenu.c (TkWinHandleMenuEvent): torn off Windows menu. 	[Bug 873613] 	(DrawMenuEntryArrow): Draw Win menu arrow after being torn 	off. [Bug 873608]

--- win/tkWinMenu.c
+++ win/tkWinMenu.c
@@ -10,7 +10,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkWinMenu.c,v 1.68 2009/08/02 21:40:17 nijtmans Exp $
+ * RCS: @(#) $Id: tkWinMenu.c,v 1.69 2009/09/14 23:41:42 hobbs Exp $
  */
 
 #define OEMRESOURCE
@@ -49,6 +49,10 @@
 
 #define MENU_SYSTEM_MENU		MENU_PLATFORM_FLAG1
 #define MENU_RECONFIGURE_PENDING	MENU_PLATFORM_FLAG2
+
+#ifndef WM_UNINITMENUPOPUP
+#define WM_UNINITMENUPOPUP              0x0125
+#endif
 
 static int indicatorDimensions[2];
 				/* The dimensions of the indicator space in a
@@ -992,6 +996,19 @@ TkWinHandleMenuEvent(
 	    Tcl_GetThreadData(&dataKey, sizeof(ThreadSpecificData));
 
     switch (*pMessage) {
+    case WM_UNINITMENUPOPUP:
+	hashEntryPtr = Tcl_FindHashEntry(&tsdPtr->winMenuTable,
+		(char *) *pwParam);
+	if (hashEntryPtr != NULL) {
+	    menuPtr = (TkMenu *) Tcl_GetHashValue(hashEntryPtr);
+	    if ((menuPtr->menuRefPtr != NULL)
+		    && (menuPtr->menuRefPtr->parentEntryPtr != NULL)) {
+		TkPostSubmenu(menuPtr->interp,
+			menuPtr->menuRefPtr->parentEntryPtr->menuPtr, NULL);
+	    }
+	}
+	break;
+
     case WM_INITMENU:
 	TkMenuInit();
 	hashEntryPtr = Tcl_FindHashEntry(&tsdPtr->winMenuTable,
@@ -1776,8 +1793,7 @@ DrawMenuEntryArrow(
     COLORREF oldBgColor;
     RECT rect;
 
-    if (!drawArrow || (mePtr->type != CASCADE_ENTRY)
-	    || (mePtr->state != ENTRY_DISABLED)) {
+    if (!drawArrow || (mePtr->type != CASCADE_ENTRY)) {
 	return;
     }
 
@@ -1796,7 +1812,8 @@ DrawMenuEntryArrow(
 	gc->background = activeBgColor->pixel;
     }
 
-    gc->foreground = GetSysColor(COLOR_GRAYTEXT);
+    gc->foreground = GetSysColor((mePtr->state == ENTRY_DISABLED) ?
+	    COLOR_GRAYTEXT : COLOR_MENUTEXT);
 
     rect.top = y + GetSystemMetrics(SM_CYBORDER);
     rect.bottom = y + height - GetSystemMetrics(SM_CYBORDER);
