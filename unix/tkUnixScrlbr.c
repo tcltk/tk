070a5a07deef06d25679dc33b304843a2fe2b1f3
donal.k.fellows@manchester.ac.uk <dkf>
2009-10-07 14:31:02 +0000

    [Patch 2088597]: Stop scrollbars from getting too small at the end.

--- unix/tkUnixScrlbr.c
+++ unix/tkUnixScrlbr.c
@@ -9,7 +9,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkUnixScrlbr.c,v 1.6 2007/12/13 15:28:51 dgp Exp $
+ * RCS: @(#) $Id: tkUnixScrlbr.c,v 1.6.2.1 2009/10/07 14:31:02 dkf Exp $
  */
 
 #include "tkInt.h"
@@ -303,8 +303,8 @@ TkpComputeScrollbarGeometry(
      * grabbed with the mouse).
      */
 
-    if (scrollPtr->sliderFirst > (fieldLength - 2*scrollPtr->borderWidth)) {
-	scrollPtr->sliderFirst = fieldLength - 2*scrollPtr->borderWidth;
+    if (scrollPtr->sliderFirst > (fieldLength - MIN_SLIDER_LENGTH)) {
+	scrollPtr->sliderFirst = fieldLength - MIN_SLIDER_LENGTH;
     }
     if (scrollPtr->sliderFirst < 0) {
 	scrollPtr->sliderFirst = 0;
