aa4c765febd5c742dbc658574c6a06d4e94fdc59
dkf <donal.k.fellows@manchester.ac.uk>
2010-01-01 23:03:42 +0000

    [Bug 1924761]: Consolidate calls to XFilterEvent to make input methods work.

--- generic/tkInt.h
+++ generic/tkInt.h
@@ -11,7 +11,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: $Id: tkInt.h,v 1.82.2.4 2009/06/27 19:53:16 nijtmans Exp $
+ * RCS: $Id: tkInt.h,v 1.82.2.5 2010/01/01 23:03:42 dkf Exp $
  */
 
 #ifndef _TKINT
@@ -313,8 +313,9 @@ typedef struct TkDisplay {
 				 * multiple applications on the display have
 				 * the focus, this will refer to the innermost
 				 * window in the innermost application. This
-				 * information isn't used under Unix or
-				 * Windows, but it's needed on the Mac. */
+				 * information isn't used on Windows, but it's
+				 * needed on the Mac, and also on X11 when XIM
+				 * processing is being done. */
 
     /*
      * Information used by tkGC.c only:
