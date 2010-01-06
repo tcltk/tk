313edfc2baa203cfd4cdeaa8de63d3ec5a7b54e7
nijtmans <nijtmans>
2010-01-06 23:16:20 +0000

    Trivial CYGWIN fixes (backported from trunk)

--- generic/default.h
+++ generic/default.h
@@ -10,14 +10,14 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: default.h,v 1.5 2004/03/17 18:15:42 das Exp $
+ * RCS: @(#) $Id: default.h,v 1.5.4.1 2010/01/06 23:16:20 nijtmans Exp $
  */
 
 #ifndef _DEFAULT
 #define _DEFAULT
 
 #if defined(__WIN32__) || defined(_WIN32) || \
-    defined(__CYGWIN__) || defined(__MINGW32__)
+    defined(__MINGW32__)
 #   include "tkWinDefault.h"
 #else
 #   if defined(MAC_OSX_TK)
