82c15f347e7d14ff5d99b6c3a013b2a15562ef26
patthoyts <patthoyts@users.sourceforge.net>
2010-01-09 00:54:06 +0000

    TIP 360: Remove special handling of the .help menu on X11.

--- library/obsolete.tcl
+++ library/obsolete.tcl
@@ -3,7 +3,7 @@
 # This file contains obsolete procedures that people really shouldn't
 # be using anymore, but which are kept around for backward compatibility.
 #
-# RCS: @(#) $Id: obsolete.tcl,v 1.4.2.1 2010/01/09 00:51:38 patthoyts Exp $
+# RCS: @(#) $Id: obsolete.tcl,v 1.4.2.2 2010/01/09 00:54:06 patthoyts Exp $
 #
 # Copyright (c) 1994 The Regents of the University of California.
 # Copyright (c) 1994 Sun Microsystems, Inc.
@@ -118,6 +118,7 @@ proc ::tk::classic::restore_menu {args} {
 	option add *Menu.activeBorderWidth	2 $prio; # 1
 	option add *Menu.borderWidth		2 $prio; # 1
         option add *Menu.clickToFocus		true $prio
+        option add *Menu.useMotifHelp		true $prio
     }
     if {[tk windowingsystem] ne "aqua"} {
 	option add *Menu.font		"TkDefaultFont" $prio; # "TkMenuFont"
