c0af073d229f34932ad5353fb873cd08c18f0ad1
dgp@users.sourceforge.net <dgp>
2010-05-03 16:30:14 +0000

    * generic/tk.h:         Bump patchlevel to 8.6b1.2 to distinguish         * library/tk.tcl:       CVS snapshots from earlier snapshots as well         * unix/configure.in:    as the 8.6b1 and 8.6b2 releases.         * win/configure.in:
            * unix/configure:       autoconf-2.59
            * win/configure:

--- library/tk.tcl
+++ library/tk.tcl
@@ -3,7 +3,7 @@
 # Initialization script normally executed in the interpreter for each Tk-based
 # application.  Arranges class bindings for widgets.
 #
-# RCS: @(#) $Id: tk.tcl,v 1.90 2010/01/06 18:37:37 dkf Exp $
+# RCS: @(#) $Id: tk.tcl,v 1.91 2010/05/03 16:30:15 dgp Exp $
 #
 # Copyright (c) 1992-1994 The Regents of the University of California.
 # Copyright (c) 1994-1996 Sun Microsystems, Inc.
@@ -15,7 +15,7 @@
 # Insist on running with compatible version of Tcl
 package require Tcl 8.6
 # Verify that we have Tk binary and script components from the same release
-package require -exact Tk  8.6b1.1
+package require -exact Tk  8.6b1.2
 
 # Create a ::tk namespace
 namespace eval ::tk {
