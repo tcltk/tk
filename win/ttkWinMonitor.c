75076ec1c581221a115e64416447c246c5b8096d
hobbs <hobbs>
2010-08-26 02:06:08 +0000

    Major backport of 8.6 Ttk for 8.5.9.  Most changes were only being         committed to head (8.6), although they could apply for 8.5 as well.         This re-sync makes future work easier to maintain and adds some         useful work for 8.5 users. Notable changes:         - Lots of code cleanup         - Some bug fixes never backported         - Addition of ttk::spinbox         - minor color changes         - Improved Vista/7 styling         - Move to tile version 0.8.6 (pseudo-package)         - ABI and API compatible (even $w identify)         - minor new features (extended $w identify)

--- win/ttkWinMonitor.c
+++ win/ttkWinMonitor.c
@@ -1,4 +1,4 @@
-/* $Id: ttkWinMonitor.c,v 1.16 2007/12/13 15:28:56 dgp Exp $
+/* $Id: ttkWinMonitor.c,v 1.16.2.1 2010/08/26 02:06:10 hobbs Exp $
  */
 
 #ifdef _MSC_VER
@@ -75,8 +75,8 @@ CreateThemeMonitorWindow(HINSTANCE hinst, Tcl_Interp *interp)
 {
     WNDCLASSEX wc;
     HWND       hwnd = NULL;
-    CHAR       title[32] = "TtkMonitorWindow";
-    CHAR       name[32] = "TtkMonitorClass";
+    TCHAR      title[32] = TEXT("TtkMonitorWindow");
+    TCHAR      name[32] = TEXT("TtkMonitorClass");
     
     wc.cbSize        = sizeof(WNDCLASSEX);
     wc.style         = CS_HREDRAW | CS_VREDRAW;
