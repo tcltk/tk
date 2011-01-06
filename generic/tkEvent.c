0073223922e3f2ce1c15c501de3c84196b5972da
stwo <stwo>
2011-01-06 05:58:15 +0000

    Cast some NULLs to (void *) in order to quash "missing sentinel in function call" compiler warnings.

--- generic/tkEvent.c
+++ generic/tkEvent.c
@@ -12,7 +12,7 @@
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * RCS: @(#) $Id: tkEvent.c,v 1.43 2010/02/13 13:47:49 nijtmans Exp $
+ * RCS: @(#) $Id: tkEvent.c,v 1.44 2011/01/06 05:58:15 stwo Exp $
  */
 
 #include "tkInt.h"
@@ -339,7 +339,7 @@ CreateXIC(
 	preedit_attlist = XVaCreateNestedList(0,
 		XNSpotLocation, &spot,
 		XNFontSet, dispPtr->inputXfs,
-		NULL);
+		(void *) NULL);
     }
 
     winPtr->inputContext = XCreateIC(dispPtr->inputMethod,
@@ -347,7 +347,7 @@ CreateXIC(
 	    XNClientWindow, winPtr->window,
 	    XNFocusWindow, winPtr->window,
 	    preedit_attname, preedit_attlist,
-	    NULL);
+	    (void *) NULL);
 
     if (preedit_attlist) {
 	XFree(preedit_attlist);
@@ -362,7 +362,7 @@ CreateXIC(
     /*
      * Adjust the window's event mask if the IM requires it.
      */
-    XGetICValues(winPtr->inputContext, XNFilterEvents, &im_event_mask, NULL);
+    XGetICValues(winPtr->inputContext, XNFilterEvents, &im_event_mask, (void *) NULL);
     if ((winPtr->atts.event_mask & im_event_mask) != im_event_mask) {
 	winPtr->atts.event_mask |= im_event_mask;
 	XSelectInput(winPtr->display, winPtr->window, winPtr->atts.event_mask);
