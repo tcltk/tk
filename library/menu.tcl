77c7c2a98a42947869d30688637b3946ba735720
patthoyts <patthoyts@users.sourceforge.net>
2011-04-30 22:28:14 +0000

    [Bug 2949774]: cascade menus should popdown ofter loosing the pointer.
    When the pointer moves to another entry from a cascade entry the sub-menu
    should popdown. This is how other menus on X11 work today. This effect will
    not be used if the user has configured ClickToFocus to maintain the previous
    Tk menu effects.
    
    (backported from trunk)
    
    Signed-off-by: Pat Thoyts <patthoyts@users.sourceforge.net>

--- library/menu.tcl
+++ library/menu.tcl
@@ -408,6 +408,8 @@ proc ::tk::MenuUnpost menu {
 
     after cancel [array get Priv menuActivatedTimer]
     unset -nocomplain Priv(menuActivated)
+    after cancel [array get Priv menuDeactivatedTimer]
+    unset -nocomplain Priv(menuDeactivated)
 
     catch {
 	if {$mb ne ""} {
@@ -563,13 +565,18 @@ proc ::tk::MenuMotion {menu x y state} {
         set index [$menu index @$x,$y]
         if {[info exists Priv(menuActivated)] \
                 && $index ne "none" \
-                && $index ne $activeindex \
-                && [$menu type $index] eq "cascade"} {
+                && $index ne $activeindex} {
             set mode [option get $menu clickToFocus ClickToFocus]
             if {$mode eq "" || ([string is boolean $mode] && !$mode)} {
-                set delay [expr {[$menu cget -type] eq "menubar"? 0 : 50}]
-                set Priv(menuActivatedTimer) \
-                    [after $delay [list $menu postcascade active]]
+                set delay [expr {[$menu cget -type] eq "menubar" ? 0 : 50}]
+                if {[$menu type $activeindex] eq "cascade"} {
+                    set Priv(menuDeactivatedTimer) \
+                        [after $delay [list $menu postcascade none]]
+                }
+                if {[$menu type $index] eq "cascade"} {
+                    set Priv(menuActivatedTimer) \
+                        [after $delay [list $menu postcascade active]]
+                }
             }
         }
     }
