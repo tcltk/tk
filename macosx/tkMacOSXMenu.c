1f0ffdd4d06989c359d53164ea7fb406af33d329
Kevin Walzer <kw@codebykevin.com>
2012-01-29 17:05:02 +0000

    Add copyright info

--- macosx/tkMacOSXMenu.c
+++ macosx/tkMacOSXMenu.c
@@ -6,6 +6,7 @@
  * Copyright (c) 1996-1997 by Sun Microsystems, Inc.
  * Copyright 2001-2009, Apple Inc.
  * Copyright (c) 2005-2009 Daniel A. Steffen <das@users.sourceforge.net>
+ * Copyright (c) 2012 Adrian Robert.
  *
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
