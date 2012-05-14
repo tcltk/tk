7a042bc5f554d7d18f45c12c28a622ca192320a8
jan.nijtmans <nijtmans@users.sourceforge.net>
2012-05-14 23:06:39 +0000

    give tkWinWm.c access to SM_XVIRTUALSCREEN and friends

--- win/tkWin.h
+++ win/tkWin.h
@@ -22,10 +22,10 @@
  */
 
 #ifndef WINVER
-#define WINVER 0x0410
+#define WINVER 0x0500
 #endif
-#ifndef _WIN32_WINDOWS
-#define _WIN32_WINDOWS 0x0410
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0500
 #endif
 
 #ifndef _TK
