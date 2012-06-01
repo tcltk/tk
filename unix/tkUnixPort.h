6382e7ec7834e0f919f4e6082f6a97ad31ed9126
jan.nijtmans <nijtmans@users.sourceforge.net>
2012-06-01 07:29:44 +0000

    unbreak Linux build

--- unix/tkUnixPort.h
+++ unix/tkUnixPort.h
@@ -126,6 +126,15 @@
 #   define WPARAM void *
 #   define LPARAM void *
 #   define LRESULT void *
+#else
+/*
+ * The TkPutImage macro strips off the color table information, which isn't
+ * needed for X.
+ */
+
+#define TkPutImage(colors, ncolors, display, pixels, gc, image, srcx, srcy, destx, desty, width, height) \
+	XPutImage(display, pixels, gc, image, srcx, srcy, destx, \
+	desty, width, height);
 #endif
 
 /*
