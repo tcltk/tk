66836e90a816969f6437bfdabcef12b2d330d74d
jan.nijtmans <nijtmans@users.sourceforge.net>
2012-06-05 11:11:21 +0000

    Generate same pkgIndex.tcl file for win32 and cygwin, one that is equally useable for both

--- generic/tkIntXlibDecls.h
+++ generic/tkIntXlibDecls.h
@@ -32,7 +32,7 @@
  * Exported function declarations:
  */
 
-#if defined(__WIN32__) || defined(__CYGWIN__) /* WIN */
+#if defined(__WIN32__) /* WIN */
 #ifndef XSetDashes_TCL_DECLARED
 #define XSetDashes_TCL_DECLARED
 /* 0 */
@@ -1192,7 +1192,7 @@ typedef struct TkIntXlibStubs {
     int magic;
     struct TkIntXlibStubHooks *hooks;
 
-#if defined(__WIN32__) || defined(__CYGWIN__) /* WIN */
+#if defined(__WIN32__) /* WIN */
     int (*xSetDashes) (Display *display, GC gc, int dash_offset, _Xconst char *dash_list, int n); /* 0 */
     XModifierKeymap * (*xGetModifierMapping) (Display *d); /* 1 */
     XImage * (*xCreateImage) (Display *d, Visual *v, unsigned int ui1, int i1, int i2, char *cp, unsigned int ui2, unsigned int ui3, int i3, int i4); /* 2 */
@@ -1411,7 +1411,7 @@ extern TkIntXlibStubs *tkIntXlibStubsPtr;
  * Inline function declarations:
  */
 
-#if defined(__WIN32__) || defined(__CYGWIN__) /* WIN */
+#if defined(__WIN32__) /* WIN */
 #ifndef XSetDashes
 #define XSetDashes \
 	(tkIntXlibStubsPtr->xSetDashes) /* 0 */
