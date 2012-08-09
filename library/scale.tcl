f22cfaf023d523622c66214d5de1a16b32fbc69c
jan.nijtmans <nijtmans@users.sourceforge.net>
2012-08-09 14:59:12 +0000

    8 new virtual events (doc not updated yet)

--- library/scale.tcl
+++ library/scale.tcl
@@ -71,10 +71,10 @@ if {[tk windowingsystem] eq "win32"} {
 bind Scale <Control-1> {
     tk::ScaleControlPress %W %x %y
 }
-bind Scale <Up> {
+bind Scale <<PrevLine>> {
     tk::ScaleIncrement %W up little noRepeat
 }
-bind Scale <Down> {
+bind Scale <<NextLine>> {
     tk::ScaleIncrement %W down little noRepeat
 }
 bind Scale <<PrevChar>> {
@@ -83,10 +83,10 @@ bind Scale <<PrevChar>> {
 bind Scale <<NextChar>> {
     tk::ScaleIncrement %W down little noRepeat
 }
-bind Scale <Control-Up> {
+bind Scale <<PrevPara>> {
     tk::ScaleIncrement %W up big noRepeat
 }
-bind Scale <Control-Down> {
+bind Scale <<NextPara>> {
     tk::ScaleIncrement %W down big noRepeat
 }
 bind Scale <<PrevWord>> {
--- library/spinbox.tcl
+++ library/spinbox.tcl
@@ -120,10 +120,10 @@ bind Spinbox <Control-1> {
     %W icursor @%x
 }
 
-bind Spinbox <Up> {
+bind Spinbox <<PrevLine>> {
     %W invoke buttonup
 }
-bind Spinbox <Down> {
+bind Spinbox <<NextLine>> {
     %W invoke buttondown
 }
 
@@ -231,19 +231,9 @@ if {[tk windowingsystem] ne "win32"} {
 
 # Additional emacs-like bindings:
 
-bind Spinbox <Control-b> {
-    if {!$tk_strictMotif} {
-	::tk::EntrySetCursor %W [expr {[%W index insert] - 1}]
-    }
-}
 bind Spinbox <Control-d> {
     if {!$tk_strictMotif} {
 	%W delete insert
-    }
-}
-bind Spinbox <Control-f> {
-    if {!$tk_strictMotif} {
-	::tk::EntrySetCursor %W [expr {[%W index insert] + 1}]
     }
 }
 bind Spinbox <Control-h> {
