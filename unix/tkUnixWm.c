28580658e41781e622b4072b6adc559f4a3a5c14
dgp <dgp@users.sourceforge.net>
2012-08-25 01:52:21 +0000

    3554026 3561016 Better fix from Emiliano Gavilan.

--- unix/tkUnixWm.c
+++ unix/tkUnixWm.c
@@ -6274,14 +6274,14 @@ TkSetTransientFor(tkwin, parent)
     if (parent == None) {
 	parent = Tk_Parent(tkwin);
 	while (!Tk_IsTopLevel(parent))
-	    parent = Tk_Parent(tkwin);
+	    parent = Tk_Parent(parent);
     }
     /*
      * Prevent crash due to incomplete initialization, or other problems.
      * [Bugs 3554026, 3561016]
      */
     if (((TkWindow *)parent)->wmInfoPtr->wrapperPtr == NULL) {
-	return;
+	CreateWrapper(((TkWindow *)parent)->wmInfoPtr);
     }
     XSetTransientForHint(Tk_Display(tkwin),
 	((TkWindow *)tkwin)->wmInfoPtr->wrapperPtr->window,
