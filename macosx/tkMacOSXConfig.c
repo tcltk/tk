5ecd06409ec1cc93719a9b8189f3ce570d2343e5
nijtmans@users.sourceforge.net <jan.nijtmans>
2012-10-23 08:52:05 +0000

    remove RCS keywords, use mode: objc consistantly

--- macosx/tkMacOSXConfig.c
+++ macosx/tkMacOSXConfig.c
@@ -9,8 +9,6 @@
  *
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
- *
- * RCS: @(#) $Id$
  */
 
 #include "tkInt.h"
@@ -38,8 +36,8 @@
 Tcl_Obj *
 TkpGetSystemDefault(
     Tk_Window tkwin,			/* A window to use. */
-    CONST char *dbName,			/* The option database name. */
-    CONST char *className)		/* The name of the option class. */
+    const char *dbName,			/* The option database name. */
+    const char *className)		/* The name of the option class. */
 {
     return NULL;
 }
