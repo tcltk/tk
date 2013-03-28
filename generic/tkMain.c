f678a58da5f3a3ba660833161cf5c59893f6ae6f
nijtmans@users.sourceforge.net <jan.nijtmans>
2013-03-28 07:36:44 +0000

    Easier solution, with proper protection and configure warnings.

--- generic/tkMain.c
+++ generic/tkMain.c
@@ -135,7 +135,7 @@ Tk_MainEx(
      * Ensure that we are getting a compatible version of Tcl.
      */
 
-    if (Tcl_InitStubs(interp, "8.5.0", 0) == NULL) {
+    if (Tcl_InitStubs(interp, TCL_VERSION ".0", 0) == NULL) {
 	if (Tcl_InitStubs(interp, "8.1", 0) == NULL) {
 	    abort();
 	} else {
