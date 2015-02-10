44859ed40417dd3fbfc5be5ed3035feadf796318
dgp <dgp@users.sourceforge.net>
2015-02-10 20:21:27 +0000

    Restore build of backported scrollbar work.

--- macosx/tkMacOSXScrlbr.c
+++ macosx/tkMacOSXScrlbr.c
@@ -43,7 +43,7 @@ typedef struct MacScrollbar {
  * variable is declared at this scope.
  */
 
-const Tk_ClassProcs tkpScrollbarProcs = {
+Tk_ClassProcs tkpScrollbarProcs = {
     sizeof(Tk_ClassProcs),	/* size */
     NULL,					/* worldChangedProc */
     NULL,					/* createProc */
