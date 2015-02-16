b307cb001af410e26d360f9f9b9658449e267853
Kevin Walzer <kw@codebykevin.com>
2015-02-16 20:19:42 +0000

    Major fix for HITheme button metrics; thanks to Marc Culler for patch.

--- macosx/ttkMacOSXTheme.c
+++ macosx/ttkMacOSXTheme.c
@@ -298,8 +298,9 @@ static void TabElementSize(
     void *clientData, void *elementRecord, Tk_Window tkwin,
     int *widthPtr, int *heightPtr, Ttk_Padding *paddingPtr)
 {
-    *heightPtr = kThemeMetricLargeTabHeight;
+    *heightPtr = GetThemeMetric(kThemeMetricLargeTabHeight, heightPtr);
     *paddingPtr = Ttk_MakePadding(0, 0, 0, 2);
+
 }
 
 static void TabElementDraw(
