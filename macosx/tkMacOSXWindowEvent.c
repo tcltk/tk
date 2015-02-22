9aa3522100580e6189a9711f9b34b7e1c85846db
Kevin Walzer <kw@codebykevin.com>
2015-02-22 18:03:22 +0000

    Fix for CGRect/NSRect confusion

--- macosx/tkMacOSXWindowEvent.c
+++ macosx/tkMacOSXWindowEvent.c
@@ -849,7 +849,7 @@ ExposeRestrictProc(
 
 - (void)viewDidEndLiveResize
 {
-    NSRect bounds = NSRectFromCGRect([self bounds]);
+    HIRect bounds = NSRectToCGRect([self bounds]);
     HIShapeRef shape = HIShapeCreateWithRect(&bounds);
     [self generateExposeEvents: shape];
 
