bb265da18662c40e9ed5c44f300e1ba32f9d8a92
Kevin Walzer <kw@codebykevin.com>
2015-03-21 01:30:55 +0000

    Improvement of memory management, removal of zombie windows from Tk-Cocoa; thanks to Marc Culler for patch

--- macosx/tkMacOSXMenu.c
+++ macosx/tkMacOSXMenu.c
@@ -261,6 +261,7 @@ static int	ModifierCharWidth(Tk_Font tkfont);
 	    /*Add time for errors to fire if necessary. This is sub-optimal but avoids issues with Tcl/Cocoa event loop integration.*/
 	    Tcl_Sleep(100);
 
+	    NSAutoreleasePool *pool = [NSAutoreleasePool new];
 	    Tcl_Preserve(interp);
 	    Tcl_Preserve(menuPtr);
 
@@ -273,6 +274,7 @@ static int	ModifierCharWidth(Tk_Font tkfont);
 	    }
 	    Tcl_Release(menuPtr);
 	    Tcl_Release(interp);
+	    [pool drain];
 	}
     }
 }
