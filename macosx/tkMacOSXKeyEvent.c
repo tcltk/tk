da7cf69fbc6411664a2cdd0ea218aec618d55aab
jan.nijtmans <nijtmans@users.sourceforge.net>
2015-03-23 10:44:09 +0000

    Remove some unnecessary end-of-line spacing

--- macosx/tkMacOSXKeyEvent.c
+++ macosx/tkMacOSXKeyEvent.c
@@ -7,7 +7,7 @@
  * Copyright 2001-2009, Apple Inc.
  * Copyright (c) 2006-2009 Daniel A. Steffen <das@users.sourceforge.net>
  * Copyright (c) 2012 Adrian Robert.
- * Copyright 2015 Marc Culler. 
+ * Copyright 2015 Marc Culler.
  *
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
