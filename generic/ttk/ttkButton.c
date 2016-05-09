0db53b33a757eaa0ef068fc338edd977a99fe354
jan.nijtmans <nijtmans@users.sourceforge.net>
2016-05-09 09:35:14 +0000

    Fix [fa8de779361f5ec2]: ttk::checkbutton handle empty variable graceful. A test-case would be nice.

--- generic/ttk/ttkButton.c
+++ generic/ttk/ttkButton.c
@@ -480,12 +480,13 @@ static int
 CheckbuttonConfigure(Tcl_Interp *interp, void *recordPtr, int mask)
 {
     Checkbutton *checkPtr = recordPtr;
-    Ttk_TraceHandle *vt = Ttk_TraceVariable(
-	interp, checkPtr->checkbutton.variableObj,
-	CheckbuttonVariableChanged, checkPtr);
+    Tcl_Obj *varName = checkPtr->checkbutton.variableObj;
+    Ttk_TraceHandle *vt = 0;
 
-    if (!vt) {
-	return TCL_ERROR;
+    if (varName != NULL && *Tcl_GetString(varName) != '\0') {
+	vt = Ttk_TraceVariable(interp, varName,
+		CheckbuttonVariableChanged, checkPtr);
+	if (!vt) return TCL_ERROR;
     }
 
     if (BaseConfigure(interp, recordPtr, mask) != TCL_OK){
@@ -493,7 +494,9 @@ CheckbuttonConfigure(Tcl_Interp *interp, void *recordPtr, int mask)
 	return TCL_ERROR;
     }
 
-    Ttk_UntraceVariable(checkPtr->checkbutton.variableTrace);
+    if (checkPtr->checkbutton.variableTrace) {
+	Ttk_UntraceVariable(checkPtr->checkbutton.variableTrace);
+    }
     checkPtr->checkbutton.variableTrace = vt;
 
     return TCL_OK;
