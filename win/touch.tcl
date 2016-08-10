701f5007742d03b731f17302912696f8a44d6830
pspjuth <peter.spjuth@gmail.com>
2016-08-10 22:12:06 +0000

    Touch work

--- win/touch.tcl
+++ win/touch.tcl
@@ -4,25 +4,24 @@ proc rndCol {} {
     return [lindex $lst $i]
 }
 
-proc Touch {d x y X Y} {
+proc Touch1 {d x y X Y} {
     set id [dict get $d id]
-    set flags [dict get $d flags]
 
-    set move [expr {!!($flags & 1)}]
-    set down [expr {!!($flags & 2)}]
-    set up   [expr {!!($flags & 4)}]
-    set primary [expr {!!($flags & 16)}]
+    set move [dict exists $d move]
+    set down [dict exists $d down]
+    set up   [dict exists $d up]
+    set primary [dict exists $d primary]
 
     if {![info exists ::t($id,id)]} {
-	set ::t($id,id) [.c create oval $x $y $x $y]
+	set ::t($id,id) [.c1 create oval $x $y $x $y]
 	if {$primary} {
-	    .c itemconfigure $::t($id,id) -fill red
+	    .c1 itemconfigure $::t($id,id) -fill red
 	} else {
-	    .c itemconfigure $::t($id,id) -fill [rndCol]
+	    .c1 itemconfigure $::t($id,id) -fill [rndCol]
 	}
     }
     if {$up} {
-	.c delete $::t($id,id)
+	.c1 delete $::t($id,id)
 	array unset ::t $id,*
 	return
     }
@@ -31,15 +30,28 @@ proc Touch {d x y X Y} {
     if {$move && $x == $::t($id,x) && $y == $::t($id,y)} {
 	return
     }
-    set r 50
+    set r [expr {[winfo screenwidth .] / 50}]
     set ::t($id,x) $x
     set ::t($id,y) $y
-    .c coords $::t($id,id) [expr {$x - $r}] [expr {$y - $r}] [expr {$x + $r}] [expr {$y + $r}]
+    .c1 coords $::t($id,id) [expr {$x - $r}] [expr {$y - $r}] \
+	[expr {$x + $r}] [expr {$y + $r}]
+}
 
+proc Touch2 {d x y X Y} {
+    set id [dict get $d id]
+    # Gesture events do not do much yet
+    .c2 delete gesture
+    .c2 create text [expr {$::size / 2}] [expr {$::size / 2}] -text $d -tags gesture
+    
 }
 
 #console show
-canvas .c -width 800 -height 800
-wm touch .c
-pack .c -fill both -expand 1
-bind .c <Touch> "Touch %d %x %y %X %Y"
+set ::size [expr {[winfo screenwidth .] / 4}]
+canvas .c1 -width $::size -height $::size -bd 3 -relief solid
+canvas .c2 -width $::size -height $::size -bd 3 -relief solid
+.c1 create text [expr {$::size * 0.5}] [expr {$::size *0.25}] -text Touch
+.c2 create text [expr {$::size * 0.5}] [expr {$::size *0.25}] -text Gesture
+wm touch .c1
+pack .c1 .c2 -side left -fill both -expand 1
+bind .c1 <Touch> "Touch1 %d %x %y %X %Y"
+bind .c2 <Touch> "Touch2 %d %x %y %X %Y"
