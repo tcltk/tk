230bcecbc9ab84ca793fb98a6160b480737e71f8
fvogel <fvogelnew1@free.fr>
2017-02-09 19:28:56 +0000

    Group multiple [style map TCombobox] statements into a single instruction

--- library/ttk/defaults.tcl
+++ library/ttk/defaults.tcl
@@ -74,11 +74,11 @@ namespace eval ttk::theme::default {
 	    -relief sunken -fieldbackground white -padding 1
 	ttk::style map TEntry -fieldbackground \
 	    [list readonly $colors(-frame) disabled $colors(-frame)]
-        ttk::style map TCombobox -arrowcolor [list disabled $colors(-disabledfg)]
 
 	ttk::style configure TCombobox -arrowsize 12 -padding 1
 	ttk::style map TCombobox -fieldbackground \
-	    [list readonly $colors(-frame) disabled $colors(-frame)]
+	    [list readonly $colors(-frame) disabled $colors(-frame)] \
+	    -arrowcolor [list disabled $colors(-disabledfg)]
 
 	ttk::style configure TSpinbox -arrowsize 10 -padding {2 0 10 0}
 	ttk::style map TSpinbox -fieldbackground \
