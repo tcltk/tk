4bc9f682f7f5bc6c184a8c8e92f9298b3ecdc982
jan.nijtmans <nijtmans@users.sourceforge.net>
2017-02-13 09:57:07 +0000

    Make tk.h work unchanged with Tcl 9.0 (novem), account for possible missing TCL_STORAGE_CLASS definition. More internal use of size_t in stead of int.

--- generic/tkGC.c
+++ generic/tkGC.c
@@ -23,7 +23,7 @@
 typedef struct {
     GC gc;			/* Graphics context. */
     Display *display;		/* Display to which gc belongs. */
-    int refCount;		/* Number of active uses of gc. */
+    size_t refCount;		/* Number of active uses of gc. */
     Tcl_HashEntry *valueHashPtr;/* Entry in valueTable (needed when deleting
 				 * this structure). */
 } TkGC;
@@ -312,8 +312,7 @@ Tk_FreeGC(
 	Tcl_Panic("Tk_FreeGC received unknown gc argument");
     }
     gcPtr = Tcl_GetHashValue(idHashPtr);
-    gcPtr->refCount--;
-    if (gcPtr->refCount == 0) {
+    if (gcPtr->refCount-- <= 1) {
 	XFreeGC(gcPtr->display, gcPtr->gc);
 	Tcl_DeleteHashEntry(gcPtr->valueHashPtr);
 	Tcl_DeleteHashEntry(idHashPtr);
