67918e86e9508f7f35d632b4eb998b2bf4e83584
fvogel <fvogelnew1@free.fr>
2017-02-21 22:53:52 +0000

    Silence compiler warning

--- generic/tkBitField.c
+++ generic/tkBitField.c
@@ -127,7 +127,7 @@ LsbIndex(uint32_t x)
 	 0,  1, 28,  2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17,  4, 8,
 	31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18,  6, 11,  5, 10, 9
     };
-    return MultiplyDeBruijnBitPosition[((uint32_t) ((x & -x)*0x077cb531)) >> 27];
+    return MultiplyDeBruijnBitPosition[((uint32_t) ((x & -((int32_t) x))*0x077cb531)) >> 27];
 }
 #  else
 /* The "classical" method, but about 20% slower than the DeBruijn method on my system. */
