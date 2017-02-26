22015767f017c8ac205163fe067414cda87d7070
fvogel <fvogelnew1@free.fr>
2017-02-26 20:38:30 +0000

    Added a copy of stdint.h (named tkWinStdInt.h) copied from https://github.com/mattn/gntp-send/blob/master/include/msinttypes/stdint.h and made it be used whenever stdint.h is not provided by the compiler. This should only happen on Windows, with MSVC strictly before version 1600 (i.e. strictly earlier than Visual Studio 2010)

--- generic/tkQTree.h
+++ generic/tkQTree.h
@@ -14,7 +14,16 @@
 
 #include "tkInt.h" /* required for inline support */
 #include "tkBool.h"
-#include <stdint.h>
+
+#if HAVE_STDINT_H
+# include <stdint.h>
+#elif defined(_MSC_VER) && _MSC_VER < 1600
+/* work around for the support of ancient compilers */
+# include "tkWinStdInt.h"
+#else
+/* this is not expected with compilers from this century, except MSVC (handled above) */
+# error "C99 support is required - can't find stdint.h"
+#endif
 
 
 /* =========================================================================
