d53bc92a5ebf083973c593b1140bc40097ab7cd4
fvogel <fvogelnew1@free.fr>
2018-03-06 07:13:53 +0000

    Moderate impact of the comment added in the previous commit.

--- unix/tkUnixRFont.c
+++ unix/tkUnixRFont.c
@@ -1059,15 +1059,19 @@ TkDrawAngledChars(
 		    x <= maxCoord - metrics.width &&
 		    y <= maxCoord - metrics.height) {
 
-			/*
-			 * NOTE:
-			 * The whole algorithm has a design problem, the choice of
-			 * NUM_SPEC is arbitrary, and so the inter-glyph spacing will
-			 * look arbitrary. This algorithm has to draw the whole string
-			 * at once (or whole blocks with same font), this requires a
-			 * dynamic 'glyphs' array. In case of overflow the array has to
-			 * be divided until the maximal string will fit. (GC)
-			 */
+		    /*
+		     * NOTE:
+		     * The whole algorithm has a design problem, the choice of
+		     * NUM_SPEC is arbitrary, and so the inter-glyph spacing could
+		     * look arbitrary. This algorithm has to draw the whole string
+		     * at once (or whole blocks with same font), this requires a
+		     * dynamic 'glyphs' array. In case of overflow the array has to
+		     * be divided until the maximal string will fit. (GC)
+                     * Given the resolution of current displays though, this should
+                     * not be a huge issue since NUM_SPEC is 1024 and thus able to
+                     * cover about 6000 pixels for a 6 pixel wide font (which is
+                     * a very small barely readable font)
+		     */
 
 		    XftDrawGlyphs(fontPtr->ftDraw, xftcolor, currentFtFont,
 			    originX, originY, glyphs, nglyph);
