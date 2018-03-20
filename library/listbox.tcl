3fc2f5bbd33e8b511de20b9be3d91b549a5cd0c6
fvogel <fvogelnew1@free.fr>
2018-03-20 21:11:31 +0000

    Add missing documentation about items of the tk::Priv array

--- library/listbox.tcl
+++ library/listbox.tcl
@@ -14,11 +14,17 @@
 # tk::Priv elements used in this file:
 #
 # afterId -		Token returned by "after" for autoscanning.
-# listboxPrev -		The last element to be selected or deselected
+# listboxPrev -	The last element to be selected or deselected
 #			during a selection operation.
 # listboxSelection -	All of the items that were selected before the
 #			current selection operation (such as a mouse
 #			drag) started;  used to cancel an operation.
+# wheel_acc_h_%W -   accumulated deltas from the <MouseWheel> event, in
+#                    horizontal direction for window %W
+# wheel_acc_v_%W -   accumulated deltas from the <MouseWheel> event, in
+#                    vertical direction for window %W
+# x -                x mouse position
+# y -                y mouse position
 #--------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------
