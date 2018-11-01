896536552eaacae34a8c48a2c208e9e42617e14f
dkf <donal.k.fellows@manchester.ac.uk>
2018-11-01 11:39:50 +0000

    Some cleaning up

--- generic/rbc/rbcMain.c
+++ generic/rbc/rbcMain.c
@@ -1,8 +1,7 @@
 /*
- * rbcInt.c --
+ * rbcMain.c --
  *
- *      This file constructs the basic functionality of the
- *      rbc commands.
+ *      This file constructs the basic functionality of the rbc commands.
  *
  * Copyright (c) 2001 BLT was created by George Howlett.
  * Copyright (c) 2009 RBC was created by Samuel Green, Nicholas Hudson, Stanton Sievers, Jarrod Stormo
@@ -13,7 +12,7 @@
  */
 
 #include "rbcInt.h"
-
+
 /*
  * -----------------------------------------------------------------------
  *
@@ -30,41 +29,46 @@
  *
  * ------------------------------------------------------------------------
  */
+
 MODULE_SCOPE int
 Rbc_Init(
-    Tcl_Interp * interp)
+    Tcl_Interp *interp)
 {                               /* Base interpreter to return results to. */
-    Tcl_Namespace  *nsPtr;
+    Tcl_Namespace *nsPtr = Tcl_CreateNamespace(interp, "::rbc", NULL, NULL);
 
-    nsPtr = Tcl_CreateNamespace(interp, "::rbc", NULL, NULL);
     if (nsPtr == NULL) {
         return TCL_ERROR;
     }
 
+    /*
+     * Export our public API.
+     */
+
     if (Tcl_Export(interp, nsPtr, "vector", 0) != TCL_OK) {
         return TCL_ERROR;
     }
-
     if (Tcl_Export(interp, nsPtr, "graph", 0) != TCL_OK) {
         return TCL_ERROR;
     }
-
     if (Tcl_Export(interp, nsPtr, "stripchart", 0) != TCL_OK) {
         return TCL_ERROR;
     }
-
     if (Tcl_Export(interp, nsPtr, "barchart", 0) != TCL_OK) {
         return TCL_ERROR;
     }
 
+    /*
+     * Create the implementations of our API.
+     */
+
     RbcVectorInit(interp);
     RbcGraphInit(interp);
 
-    if (Tcl_PkgProvide(interp, "rbc", RBC_VERSION) == TCL_ERROR) {
-        return TCL_ERROR;
-    }
+    /*
+     * Finalize the provision of the package.
+     */
 
-    return TCL_OK;
+    return Tcl_PkgProvide(interp, "rbc", RBC_VERSION);
 }
-
+
 /* vim: set ts=4 sw=4 sts=4 ff=unix et : */
