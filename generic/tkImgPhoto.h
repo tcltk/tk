0f7b8d2063fe93f66637210cc6ffac0b99ff00d8
oehhar <harald.oehlmann@elmicron.de>
2018-12-11 10:35:53 +0000

    Corrected indents and formatting

--- generic/tkImgPhoto.h
+++ generic/tkImgPhoto.h
@@ -166,7 +166,7 @@ struct PhotoMaster {
     Tcl_Obj *format;		/* User-specified format of data in image file
 				 * or string value. */
     Tcl_Obj *metadata;		/* User-specified metadata dict or read from
-                                 * image file */
+				 * image file */
     unsigned char *pix32;	/* Local storage for 32-bit image. */
     int ditherX, ditherY;	/* Location of first incorrectly dithered
 				 * pixel in image. */
