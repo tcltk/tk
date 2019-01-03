9c36893ae64c2a094660762e984f25820336e221
jan.nijtmans <nijtmans@users.sourceforge.net>
2019-01-03 21:14:00 +0000

    Remove 3 lines of code in tkTextDisp.c which had become obsolete but were accidentally left in the file.

--- generic/tkTextDisp.c
+++ generic/tkTextDisp.c
@@ -3391,10 +3391,6 @@ TkTextUpdateLineMetrics(
 	GetYView(textPtr->interp, textPtr, 1);
     }
     if (fullUpdateRequested) {
-        TextDInfo *dInfoPtr = textPtr->dInfoPtr;
-
-        dInfoPtr->lastMetricUpdateLine = lineNum;
-        dInfoPtr->currentMetricUpdateLine = lineNum;
         GenerateWidgetViewSyncEvent(textPtr, 1);
     }
     return lineNum;
