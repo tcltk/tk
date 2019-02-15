6a05da239a35f663bc414406a673aff0ac54f166
jan.nijtmans <nijtmans@users.sourceforge.net>
2019-02-15 20:05:17 +0000

    Tcl_BackgroundError -> Tcl_BackgroundException. Mark deprecated macro's as such (remove then with -DTCL_NO_DEPRECATED). Change field in Tk_ClassProcs to size_t, when compiling against Tcl 9 headers

--- generic/tkText.c
+++ generic/tkText.c
@@ -5622,7 +5622,7 @@ TkTextRunAfterSyncCmd(
     code = Tcl_EvalObjEx(textPtr->interp, textPtr->afterSyncCmd, TCL_EVAL_GLOBAL);
     if (code == TCL_ERROR) {
 	Tcl_AddErrorInfo(textPtr->interp, "\n    (text sync)");
-	Tcl_BackgroundError(textPtr->interp);
+	Tcl_BackgroundException(textPtr->interp, TCL_ERROR);
     }
     Tcl_Release((ClientData) textPtr->interp);
     Tcl_DecrRefCount(textPtr->afterSyncCmd);
