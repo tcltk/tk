8148777ca719f069d48cff9dd62cba65abaef5f0
dgp <dgp@users.sourceforge.net>
2019-06-17 19:36:26 +0000

    Release candidate branch for Tk 8.7a3.

--- generic/tk.h
+++ generic/tk.h
@@ -68,10 +68,10 @@ extern "C" {
 #define TK_MAJOR_VERSION	8
 #define TK_MINOR_VERSION	7
 #define TK_RELEASE_LEVEL	TCL_ALPHA_RELEASE
-#define TK_RELEASE_SERIAL	2
+#define TK_RELEASE_SERIAL	3
 
 #define TK_VERSION		"8.7"
-#define TK_PATCH_LEVEL		"8.7a2"
+#define TK_PATCH_LEVEL		"8.7a3"
 
 /*
  * A special definition used to allow this header file to be included from
