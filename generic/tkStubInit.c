5b0ab3da0e490d51ca930773cc40292bdc101a00
jan.nijtmans <nijtmans@users.sourceforge.net>
2019-09-26 08:56:18 +0000

    Better fix for [2026405]: winfo id bug in cygwin build. Actually, formatting of %p is libc-specific, now that we have TCL_Z_MODIFIER we can make it platform-independant for all cases.

--- generic/tkStubInit.c
+++ generic/tkStubInit.c
@@ -116,7 +116,7 @@ TkpPrintWindowId(
 				 * the hex representation of a pointer. */
     Window window)		/* Window to be printed into buffer. */
 {
-    sprintf(buf, "%#08lx", (unsigned long) (window));
+    sprintf(buf, "0x%" TCL_Z_MODIFIER "x", (size_t)window);
 }
 
 int
