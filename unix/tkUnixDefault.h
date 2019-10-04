52c2b26db4d625bd600120dda9108e4eac8e44fb
jan.nijtmans <nijtmans@users.sourceforge.net>
2019-10-04 09:59:36 +0000

    Too quick conclusion: all lines and text were gone :-). Reverting most of previous commit

--- unix/tkUnixDefault.h
+++ unix/tkUnixDefault.h
@@ -511,6 +511,7 @@
  * Defaults for canvas text:
  */
 
+#define DEF_CANVTEXT_FILL		BLACK
 #define DEF_CANVTEXT_FONT		"TkDefaultFont"
 
 /*
@@ -520,6 +521,7 @@
 
 #define DEF_CANVBMAP_FG			BLACK
 #define DEF_CANVITEM_OUTLINE		BLACK
+#define DEF_CANVLINE_FILL		DEF_CANVITEM_OUTLINE
 
 /*
  * Defaults for toplevels (most of the defaults for frames also apply
