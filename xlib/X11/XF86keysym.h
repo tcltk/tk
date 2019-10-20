0624b567b683fc3efc75983d2bc0223c860d6c5d
jan.nijtmans <nijtmans@users.sourceforge.net>
2019-10-20 18:30:32 +0000

    Add a few defines from latest X11-R6

--- xlib/X11/XF86keysym.h
+++ xlib/X11/XF86keysym.h
@@ -23,11 +23,12 @@
 #define XF86XK_ModeLock		0x1008FF01	/* Mode Switch Lock */
 
 /* Backlight controls. */
-#define XF86XK_MonBrightnessUp   0x1008FF02  /* Monitor/panel brightness */
-#define XF86XK_MonBrightnessDown 0x1008FF03  /* Monitor/panel brightness */
-#define XF86XK_KbdLightOnOff     0x1008FF04  /* Keyboards may be lit     */
-#define XF86XK_KbdBrightnessUp   0x1008FF05  /* Keyboards may be lit     */
-#define XF86XK_KbdBrightnessDown 0x1008FF06  /* Keyboards may be lit     */
+#define XF86XK_MonBrightnessUp    0x1008FF02  /* Monitor/panel brightness */
+#define XF86XK_MonBrightnessDown  0x1008FF03  /* Monitor/panel brightness */
+#define XF86XK_KbdLightOnOff      0x1008FF04  /* Keyboards may be lit     */
+#define XF86XK_KbdBrightnessUp    0x1008FF05  /* Keyboards may be lit     */
+#define XF86XK_KbdBrightnessDown  0x1008FF06  /* Keyboards may be lit     */
+#define XF86XK_MonBrightnessCycle 0x1008FF07  /* Monitor/panel brightness */
 
 /*
  * Keys found on some "Internet" keyboards.
@@ -117,12 +118,12 @@
 #define XF86XK_Explorer		0x1008FF5D   /* Launch file explorer        */
 #define XF86XK_Game		0x1008FF5E   /* Launch game                 */
 #define XF86XK_Go		0x1008FF5F   /* Go to URL                   */
-#define XF86XK_iTouch		0x1008FF60   /* Logitch iTouch- don't use   */
+#define XF86XK_iTouch		0x1008FF60   /* Logitech iTouch- don't use  */
 #define XF86XK_LogOff		0x1008FF61   /* Log off system              */
 #define XF86XK_Market		0x1008FF62   /* ??                          */
 #define XF86XK_Meeting		0x1008FF63   /* enter meeting in calendar   */
-#define XF86XK_MenuKB		0x1008FF65   /* distingush keyboard from PB */
-#define XF86XK_MenuPB		0x1008FF66   /* distinuish PB from keyboard */
+#define XF86XK_MenuKB		0x1008FF65   /* distinguish keyboard from PB */
+#define XF86XK_MenuPB		0x1008FF66   /* distinguish PB from keyboard */
 #define XF86XK_MySites		0x1008FF67   /* Favourites                  */
 #define XF86XK_New		0x1008FF68   /* New (folder, document...    */
 #define XF86XK_News		0x1008FF69   /* News                        */
@@ -196,6 +197,15 @@
 #define XF86XK_TouchpadOff	0x1008FFB1   /* The touchpad got switched off */
 
 #define XF86XK_AudioMicMute	0x1008FFB2   /* Mute the Mic from the system */
+
+#define XF86XK_Keyboard		0x1008FFB3   /* User defined keyboard related action */
+
+#define XF86XK_WWAN		0x1008FFB4   /* Toggle WWAN (LTE, UMTS, etc.) radio */
+#define XF86XK_RFKill		0x1008FFB5   /* Toggle radios on/off */
+
+#define XF86XK_AudioPreset	0x1008FFB6   /* Select equalizer preset, e.g. theatre-mode */
+
+#define XF86XK_RotationLockToggle 0x1008FFB7 /* Toggle screen rotation lock on/off */
 
 /* Keys for special action keys (hot keys) */
 /* Virtual terminals on some operating systems */
