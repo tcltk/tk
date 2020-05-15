623aceed37d5ea18f8f176002f5b3f9fa6968025
culler <culler>
2020-05-15 15:04:42 +0000

    Make sure unix includes its platform-specific header in files that use TkKeyEvent.

--- generic/tkEvent.c
+++ generic/tkEvent.c
@@ -15,6 +15,16 @@
 
 #include "tkInt.h"
 
+/* 
+ * Include platform-specific TkKeyEvent
+ */
+
+#if defined(_WIN32)
+#include "tkWinInt.h"
+#elif !defined(MAC_OSX_TK)
+#include "tkUnixInt.h"
+#endif
+
 /*
  * There's a potential problem if a handler is deleted while it's current
  * (i.e. its function is executing), since Tk_HandleEvent will need to read
