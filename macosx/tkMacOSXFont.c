9f01c54f9b828f4d3b4f6f345811fb626c5eb979
jan.nijtmans <nijtmans@users.sourceforge.net>
2020-05-20 10:10:40 +0000

    Fix TkUtfAtIndex(), still was not 100% correct. Simplify TKNSString::DString, since Tcl_UniCharToUtf() is already capable of surrogate handling with a proper Tcl version.

--- macosx/tkMacOSXFont.c
+++ macosx/tkMacOSXFont.c
@@ -133,7 +133,6 @@ static void		DrawCharsInContext(Display *display, Drawable drawable,
 	_string = [[NSString alloc] initWithString:aString];
 	self.UTF8String = _string.UTF8String;
     }
-    printf("Initialized with string %s\n", self.UTF8String);
     return self;
 }
 
@@ -166,32 +165,16 @@ static void		DrawCharsInContext(Display *display, Drawable drawable,
 	 * The DString has not been initialized. Construct it from
 	 * our string's unicode characters.
 	 */
-
-	char buffer[2*TCL_UTF_MAX];
-	unsigned int index, length, ch;
+	char *p;
+	int index;
 
 	Tcl_DStringInit(&_ds);
-#if TCL_UTF_MAX == 3
+	Tcl_DStringSetLength(&_ds, 3 * [_string length]);
+	p = Tcl_DStringValue(&_ds);
 	for (index = 0; index < [_string length]; index++) {
-	    UniChar uni = [_string characterAtIndex: index];
-
-	    if (CFStringIsSurrogateHighCharacter(uni)) {
-		UniChar low = [_string characterAtIndex: ++index];
-		ch = CFStringGetLongCharacterForSurrogatePair(uni, low);
-	    } else {
-		ch = uni;
-	    }
-	    length = TkUniCharToUtf(ch, buffer);
-	    Tcl_DStringAppend(&_ds, buffer, length);
+	    p += Tcl_UniCharToUtf([_string characterAtIndex: index], p);
 	}
-#else
-	for (index = 0; index < [_string length]; index++) {
-	    ch = (int) [_string characterAtIndex: index];
-	    length = Tcl_UniCharToUtf(ch, buffer);
-	    Tcl_DStringAppend(&_ds, buffer, length);
-	}
-
-#endif
+	Tcl_DStringSetLength(&_ds, p - Tcl_DStringValue(&_ds));
     }
     return _ds;
 }
