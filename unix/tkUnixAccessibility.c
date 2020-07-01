cb5c14e6a91b549301804712a2020e6fe384453a
Kevin Walzer <kw@codebykevin.com>
2020-07-01 01:55:10 +0000

    Map states

--- unix/tkUnixAccessibility.c
+++ unix/tkUnixAccessibility.c
@@ -6,6 +6,7 @@
  *      the standard accessibility library for X11 systems. 
  *
  * Copyright (c) 2006, Marcus von Appen
+ * Copyright (c) 2009, Allen B. Taylor
  * Copyright (c) 2020 Kevin Walzer/WordTech Communications LLC.
  *
  * See the file "license.terms" for information on usage and redistribution of
@@ -15,7 +16,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <AtkObject.h>
 
 /*
  * Get main Tcl interpreter. 
@@ -230,83 +230,87 @@ static AtkRole ATKRole_Register(Tk_Window tkwin, char *name) {
 
   switch (name) {
     
-  case "TK_ROLE_CELL":
+  case "TK_ACCESSIBLE_ROLE_CELL":
     role = ATK_ROLE_CELL;
     break;
 
-  case "TK_ROLE_CHECK_BOX":
+  case "TK_ACCESSIBLE_ROLE_CHECK_BOX":
     role = ATK_ROLE_CHECK_BOX;
     break;
 
-  case "TK_ROLE_CHECK_MENU_ITEM":
+  case "TK_ACCESSIBLE_ROLE_CHECK_MENU_ITEM":
     role = ATK_ROLE_CHECK_MENU_ITEM;
     break;
 
-  case "TK_ROLE_COMBOBOX":
+  case "TK_ACCESSIBLE_ROLE_COMBOBOX":
     role = ATK_ROLE_COMBOBOX;
     break;
 
-  case "TK_ROLE_ENTRY":
+  case "TK_ACCESSIBLE_ROLE_ENTRY":
     role = "ATK_ROLE_ENTRY";
     break;
 
-  case "TK_ROLE_GROUPING":
+   case "TK_ACCESSIBLE_ROLE_PASSWORD_TEXT":
+    role = ATK_ROLE_PASSWORD_TEXT;
+    break;
+
+  case "TK_ACCESSIBLE_ROLE_GROUPING":
     role = ATK_ROLE_GROUPING;
     break;
 
-  case "TK_ROLE_LABEL":
+  case "TK_ACCESSIBLE_ROLE_LABEL":
     role = ATK_ROLE_LABEL;
     break;
 
-  case  "TK_ROLE_MENU":
+  case  "TK_ACCESSIBLE_ROLE_MENU":
     role = ATK_ROLE_MENU;
     break;
 
-  case "TK_ROLE_MENUBAR":
+  case "TK_ACCESSIBLE_ROLE_MENUBAR":
     role = ATK_ROLE_MENUBAR;
     break;
     
-  case "TK_ROLE_MENUITEM":
+  case "TK_ACCESSIBLE_ROLE_MENUITEM":
     role = ATK_ROLE_MENUITEM;
     break;
     
-  case "TK_ROLE_OUTLINE":
+  case "TK_ACCESSIBLE_ROLE_OUTLINE":
     role = ATK_ROLE_OUTLINE;
     break;
 
-  case "TK_ROLE_OUTLINEITEM":
+  case "TK_ACCESSIBLE_ROLE_OUTLINEITEM":
     role = ATK_ROLE_OUTLINEITEM;
     break;
 
-  case "TK_ROLE_PAGE_TAB":
+  case "TK_ACCESSIBLE_ROLE_PAGE_TAB":
     role = ATK_ROLE_PAGE_TAB;
     break;
 
-  case "TK_ROLE_PAGETABLIST":
+  case "TK_ACCESSIBLE_ROLE_PAGETABLIST":
     role = ATK_ROLE_PAGETABLIST;
     break;
 
-  case "TK_ROLE_PANE":
+  case "TK_ACCESSIBLE_ROLE_PANE":
     role = ATK_ROLE_PANE;
     break;
 
-  case "TK_ROLE_PUSH_BUTTON":
+  case "TK_ACCESSIBLE_ROLE_PUSH_BUTTON":
     role = ATK_ROLE_PUSH_BUTTON;
     break;
 
-  case "TK_ROLE_RADIOBUTTON":
+  case "TK_ACCESSIBLE_ROLE_RADIOBUTTON":
     role = ATK_ROLE_RADIOBUTTON;
     break;
 
-  case "TK_ROLE_ROWHEADER":
+  case "TK_ACCESSIBLE_ROLE_ROWHEADER":
     role = ATK_ROLE_ROWHEADER;
     break;
 
-  case "TK_ROLE_SLIDER":
+  case "TK_ACCESSIBLE_ROLE_SLIDER":
     role = ATK_ROLE_SLIDER;
     break;
 
-  case "TK_ROLE_SPINBUTTON":
+  case "TK_ACCESSIBLE_ROLE_SPINBUTTON":
     role = ATK_ROLE_SPINBUTTON;
     break;
 
@@ -318,6 +322,82 @@ static AtkRole ATKRole_Register(Tk_Window tkwin, char *name) {
   return role;
 
 }
+
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * ATKState_Register --
+ *
+ *  Maps the Tk Accessible state to an Atk state.
+ *
+ * Results:
+ *  Maps the abstract Tk Accessible to a platform-specific Atk state.
+ *
+ *
+ * Side effects:
+ *	None.
+ *
+ *----------------------------------------------------------------------
+ */
+
+
+
+static AtkState ATKState_Register(char *name) {
+
+  
+  AtkRole state = NULL;
+
+  switch (name) {
+
+  case "TK_ACCESSIBLE_STATE_CHECKED":
+    state = ATK_STATE_CHECKED;
+    break;
+
+  case "TK_ACCESSIBLE_STATE_ENABLED":
+    state = ATK_STATE_ENABLED;
+    break;
+
+  case "TK_ACCESSIBLE_STATE_FOCUSABLE":
+    state = ATK_STATE_FOCUSABLE;
+    break;
+
+  case "TK_ACCESSIBLE_STATE_HORIZONTAL":
+    state = ATK_STATE_HORIZONTAL;
+    break;
+
+  case "TK_ACCESSIBLE_STATE_VERTICAL":
+    state = ATK_STATE_VERTICAL;
+    break;
+
+  case "TK_ACCESSIBLE_STATE_MULTISELECTABLE":
+    state = ATK_STATE_MULTISELECTABLE;
+    break;
+
+  case "TK_ACCESSIBLE_STATE_RESIZABLE":
+    state = ATK_STATE_RESIZABLE;
+    break;
+
+  case "TK_ACCESSIBLE_STATE_SELECTED":
+    state = ATK_STATE_SELECTED;
+    break;
+
+  case "TK_ACCESSIBLE_STATE_EXPANDABLE":
+    state = ATK_STATE_EXPANDABLE;
+    break;
+
+  case "TK_ACCESSIBLE_STATE_SENSITIVE":
+    state = ATK_STATE_SENSITIVE;
+    break;
+
+  default:
+    break;
+  }
+
+  return state;
+}
+
+
 
 /*
  *----------------------------------------------------------------------
