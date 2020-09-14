075429fc2cc7afc986bc3839feacf935c89ccbc7
jan.nijtmans <nijtmans@users.sourceforge.net>
2020-09-14 07:36:16 +0000

    Use consistant $idx < 0 in stead of $idx != -1 for checking index meaning 'not found'

--- library/spinbox.tcl
+++ library/spinbox.tcl
@@ -483,10 +483,10 @@ proc ::tk::spinbox::MouseSelect {w x {cursor {}}} {
 	word {
 	    if {$cur < [$w index anchor]} {
 		set before [tcl_wordBreakBefore [$w get] $cur]
-		set after [tcl_wordBreakAfter [$w get] [expr {$anchor-1}]]
+		set after [tcl_wordBreakAfter [$w get] $anchor-1]
 	    } else {
 		set before [tcl_wordBreakBefore [$w get] $anchor]
-		set after [tcl_wordBreakAfter [$w get] [expr {$cur - 1}]]
+		set after [tcl_wordBreakAfter [$w get] $cur-1]
 	    }
 	    if {$before < 0} {
 		set before 0
