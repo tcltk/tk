3345094d8be02dd135abbc1ff562f72d4a946ce6
marc_culler <marc.culler@gmail.com>
2020-10-13 17:53:52 +0000

    Handle the case where the app exits because the last main window was closed.

--- unix/tkAppInit.c
+++ unix/tkAppInit.c
@@ -101,15 +101,8 @@ main(
 void sampleExitHandler(ClientData clientdata) {
     Tcl_Interp *interp = (Tcl_Interp *)clientdata;
 
-    /*
-     * Don't do anything if the interpreter has already been destroyed
-     * because the mainloop ended.
-     */
-
-    if (Tk_GetNumMainWindows() > 0) {
-	Tcl_EvalEx(interp, "tk_messageBox -type ok -message \"Really quit?\"", -1,
+    Tcl_EvalEx(interp, "tk_messageBox -type ok -message \"Really quit?\"", -1,
 		   TCL_EVAL_GLOBAL);
-    }
 }
 
 int
