3e45975eb8e68b96df149d258bf416d279f66a1e
Kevin Walzer <kw@codebykevin.com>
2020-11-11 00:08:39 +0000

    Attempt to return error on systray command on Windows

--- library/systray.tcl
+++ library/systray.tcl
@@ -339,6 +339,12 @@ proc ::tk::sysnotify {title message} {
     return
 }
 
+if {[tk windowingsystem] eq "win32"} {
+	proc systray {} {
+	error "invalid command name \"systray\""
+	}
+}
+
 #Add these commands to the tk command ensemble: tk systray, tk sysnotify
 #Thanks to Christian Gollwitzer for the guidance here
 set map [namespace ensemble configure tk -map]
