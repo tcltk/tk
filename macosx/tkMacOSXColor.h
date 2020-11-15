be107509e367e0c13e57f1f49755c5aeb88c50bd
marc_culler <marc.culler@gmail.com>
2020-11-15 17:14:50 +0000

    Fix several issues with the selected text background color.

--- macosx/tkMacOSXColor.h
+++ macosx/tkMacOSXColor.h
@@ -249,6 +249,7 @@ static SystemColorDatum systemColorData[] = {
 {"LinkColor",			    semantic, 0, "blueColor", 0, NULL },
 {"PlaceholderTextColor",	    semantic, 0, "grayColor", 0, NULL },
 {"SeparatorColor",		    semantic, 0, "grayColor", 0, NULL },
+{"UnemphasizedSelectedTextBackgroundColor", semantic, 0, "grayColor", 0, NULL },
 {NULL,				    0, 0, NULL, 0, NULL }
 };
 
