be5e079937b3911aa69421b2d9a83c7b97c5d8d3
jan.nijtmans <nijtmans@users.sourceforge.net>
2021-01-08 14:36:27 +0000

    Slightly better overflow-check, without doing too many re-calculations of the same multiply

--- generic/tkImgSVGnano.c
+++ generic/tkImgSVGnano.c
@@ -592,14 +592,15 @@ RasterizeSVG(
 	goto cleanAST;
     }
 
-    /* Tk Ticket [822330269b] Check potential int overflow in following ckalloc*/
-    if ( w * h < 0 || w * h > INT_MAX / 4) {
+    /* Tk Ticket [822330269b] Check potential int overflow in following ckalloc */
+    unsigned long long wh = (unsigned long long)w * (unsigned long long)h;
+    if ( wh > INT_MAX / 4) {
 	Tcl_SetObjResult(interp, Tcl_NewStringObj("image size overflow", -1));
 	Tcl_SetErrorCode(interp, "TK", "IMAGE", "SVG", "IMAGE_SIZE_OVERFLOW", NULL);
 	goto cleanRAST;
     }
-    
-    imgData = (unsigned char *)attemptckalloc(w * h *4);
+
+    imgData = (unsigned char *)attemptckalloc(wh * 4);
     if (imgData == NULL) {
 	Tcl_SetObjResult(interp, Tcl_NewStringObj("cannot alloc image buffer", -1));
 	Tcl_SetErrorCode(interp, "TK", "IMAGE", "SVG", "OUT_OF_MEMORY", NULL);
