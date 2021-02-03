a863d3b9c66311d89d572a0bafe3f300af22c9a6
marc_culler <marc.culler@gmail.com>
2021-02-03 18:59:40 +0000

    Actually, the @available directive was available in XCode 10.9 (for macOS 10.14)

--- macosx/tkMacOSXSysTray.c
+++ macosx/tkMacOSXSysTray.c
@@ -437,7 +437,7 @@ typedef TkStatusItem** StatusItemInfo;
      */
 
     DEBUG_LOG("willPresentNotification\n");
-#if MAC_OS_X_VERSION_MAX_ALLOWED >= 110000
+#if MAC_OS_X_VERSION_MAX_ALLOWED >= 101400
     if (@available(macOS 11.0, *)) {
 	completionHandler(ALERT_OPTION);
     }
