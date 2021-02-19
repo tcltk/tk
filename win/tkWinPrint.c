ec34c118302b8a78173de13de5897bebfb560880
Kevin Walzer <kw@codebykevin.com>
2021-02-19 02:21:41 +0000

    Some progress with text printing on Windows

--- win/tkWinPrint.c
+++ win/tkWinPrint.c
@@ -229,159 +229,219 @@ done:
  */
 
 static int WinTextPrint(
-    TCL_UNUSED(void * ),
-    Tcl_Interp * interp,
-    int objc,
-    Tcl_Obj *
-    const * objv) {
-
-    Tcl_Channel chan;
-    Tcl_Obj * printbuffer;
+			TCL_UNUSED(void * ),
+			Tcl_Interp * interp,
+			int objc,
+			Tcl_Obj *
+			const * objv) {
     PRINTDLG pd;
     HDC hDC;
     TEXTMETRIC tm;
-    HANDLE printhandle;
     size_t lineCur;
     int countlines;
+    int yChar;
+    int  lines_per_page, total_pages, chars_per_line;
+    int result;
+    int page;
+    DOCINFO di;
+    HFONT hFont = NULL;
+    char * data;
+    const char * tmptxt;
+    LPCTSTR printbuffer;
+    LONG bufferlen;
+    int output;
     int dpi_x, dpi_y, margin_left, margin_right, margin_top, margin_bottom;
     int printarea_horz, printarea_vert, phys_height, phys_width;
     int digital_margin_left, digital_margin_top, digital_margin_right, digital_margin_bottom;
     int left_adjust_margin, top_adjust_margin, right_adjust_margin, bottom_adjust_margin;
     int page_height, page_width;
-    int yChar;
-    int page, lines_per_page, total_pages, header_height;
-    int result;
-
-    DOCINFO di;
-    HFONT hFont;
-    char * cline;
-    int clen;
-    const char * mode;
 
     result = TCL_OK;
 
     if (objc != 2) {
-        Tcl_WrongNumArgs(interp, 1, objv, "text");
-        result = TCL_ERROR;
-        return result;
+	Tcl_WrongNumArgs(interp, 1, objv, "text");
+	result = TCL_ERROR;
+	return result;
     }
 
-    mode = "r";
-
-    /* Initialize print dialog. */
+    /* 
+     *Initialize print dialog. 
+     */
     ZeroMemory( &pd, sizeof(pd));
     pd.lStructSize = sizeof(pd);
     pd.hwndOwner = GetDesktopWindow();
-    pd.Flags = PD_RETURNDC | PD_NOPAGENUMS;
+    pd.Flags = PD_RETURNDC | PD_NOPAGENUMS | PD_ALLPAGES | PD_USEDEVMODECOPIESANDCOLLATE;
 
     if (PrintDlg( &pd) == TRUE) {
-        hDC = pd.hDC;
-        if (hDC == NULL) {
-            Tcl_AppendResult(interp, "can't allocate printer DC", NULL);
-            return TCL_ERROR;
-        }
+	hDC = pd.hDC;
+	if (hDC == NULL) {
+	    Tcl_AppendResult(interp, "can't allocate printer DC", NULL);
+	    return TCL_ERROR;
+	}
 
-        ZeroMemory( &di, sizeof(di));
-        di.cbSize = sizeof(di);
-        di.lpszDocName = "Tk Output";
+	ZeroMemory( &di, sizeof(di));
+	di.cbSize = sizeof(di);
+	di.lpszDocName = "Tk Output";
 
-        /* Read file for printing and count number of lines in file.*/
-        chan = Tcl_FSOpenFileChannel(interp, objv[1], mode, 0);
-        if (chan == NULL) {
-            Tcl_AppendResult(interp, "unable to open channel to file", NULL);
-            result = TCL_ERROR;
-            return result;
-        }
-        printbuffer = Tcl_NewObj();
-        Tcl_IncrRefCount(printbuffer);
-        Tcl_ReadChars(chan, printbuffer, -1, 1);
-        cline = Tcl_GetStringFromObj(printbuffer, &clen);
-        countlines = 0;
-        for (lineCur = 0; lineCur < strlen(cline); lineCur++) {
-            if (cline[lineCur] == '\n') {
-                countlines++;
-            }
-        }
+	data = Tcl_GetString(objv[1]);
+	countlines = 0;
+	for (lineCur = 0; lineCur < strlen(data); lineCur++) {
+	    if (data[lineCur] == '\n') {
+		countlines++;
+	    }
+	}
 
-        Tcl_Close(interp, chan);
-        Tcl_DecrRefCount(printbuffer);
-
-        /* Get printer resolution. */
-        dpi_x = GetDeviceCaps(hDC, LOGPIXELSX);
-        dpi_y = GetDeviceCaps(hDC, LOGPIXELSY);
-
-        /* Compute physical area and margins. */
-        margin_left = GetDeviceCaps(hDC, PHYSICALOFFSETX);
-        margin_top = GetDeviceCaps(hDC, PHYSICALOFFSETY);
-        printarea_horz = GetDeviceCaps(hDC, HORZRES);
-        printarea_vert = GetDeviceCaps(hDC, VERTRES);
-        phys_width = GetDeviceCaps(hDC, PHYSICALWIDTH);
-        phys_height = GetDeviceCaps(hDC, PHYSICALHEIGHT);
-        margin_right = phys_width - printarea_horz - margin_left;
-        margin_bottom = phys_height - printarea_vert - margin_top;
-
-        /* Convert margins into pixel values the printer understands. */
-        digital_margin_left = MulDiv(margin_left, dpi_x, 1000);
-        digital_margin_top = MulDiv(margin_top, dpi_y, 1000);
-        digital_margin_right = MulDiv(margin_right, dpi_x, 1000);
-        digital_margin_bottom = MulDiv(margin_bottom, dpi_y, 1000);
-
-        /* Compute adjusted printer margins in pixels. */
-        left_adjust_margin = digital_margin_left - margin_left;
-        top_adjust_margin = digital_margin_top - margin_top;
-        right_adjust_margin = digital_margin_right - margin_right;
-        bottom_adjust_margin = digital_margin_bottom - margin_bottom;
-
-        /* Finally, here is our print area. */
-        page_width = printarea_horz - (left_adjust_margin + right_adjust_margin);
-        page_height = printarea_vert - (top_adjust_margin + bottom_adjust_margin);
-
-        hFont = (HFONT) GetStockObject(ANSI_FIXED_FONT);
-
-        /* Set up the current device context. */
-        SetMapMode(hDC, MM_TEXT);
-        SelectObject(hDC, hFont);
-
-        /* Work out the character dimensions for the current font. */
+	/* 
+	 * Work out the character dimensions for the current font. 
+	 */
         GetTextMetrics(hDC, &tm);
-        yChar = tm.tmHeight;
+        yChar = tm.tmHeight + tm.tmExternalLeading;
 
-        /* Work out how much data can be printed onto each page. */
-        header_height = 0;
-        lines_per_page = (page_height - header_height / yChar);
+	/* 
+	 * Work out how much data can be printed onto each page. 
+	 */
+	chars_per_line = GetDeviceCaps (hDC, HORZRES) / tm.tmAveCharWidth ;
+        lines_per_page = GetDeviceCaps (hDC, VERTRES) / yChar ;
         total_pages = (countlines + lines_per_page - 1) / lines_per_page;
+	
+	/* 
+	 * Convert input text into a format Windows can use for printing. 
+	 */
+	tmptxt = data;
+	printbuffer = (LPCTSTR) tmptxt;
+	bufferlen = lstrlen(printbuffer);
 
-        if (StartDoc(hDC, &di) > 0) {
-	    if(StartPage(pd.hDC) < 0) {
-               Tcl_AppendResult(interp, "unable to start page", NULL);
-            result = TCL_ERROR;
-            return result;
-                }
-          
-	    SetViewportOrgEx(hDC, left_adjust_margin, top_adjust_margin, NULL);
-            for (page = 0; page < total_pages; page++) {
-                printhandle = SelectObject(hDC, hFont);
-                RECT r = {
-                    0,
-                    0,
-                    page_width,
-                    page_height
-                };
-                DrawText(hDC, cline, -1, &r, DT_NOPREFIX | DT_WORDBREAK);
-            }
+	/* 
+	 * Get printer resolution. 
+	 */
+	dpi_x = GetDeviceCaps(hDC, LOGPIXELSX);
+	dpi_y = GetDeviceCaps(hDC, LOGPIXELSY);
 
-            EndPage(hDC);
-            EndDoc(hDC);
-            DeleteDC(hDC);
+	/* 
+	 * Compute physical area and margins. 
+	 */
+	margin_left = GetDeviceCaps(hDC, PHYSICALOFFSETX);
+	margin_top = GetDeviceCaps(hDC, PHYSICALOFFSETY);
+	printarea_horz = GetDeviceCaps(hDC, HORZRES);
+	printarea_vert = GetDeviceCaps(hDC, VERTRES);
+	phys_width = GetDeviceCaps(hDC, PHYSICALWIDTH);
+	phys_height = GetDeviceCaps(hDC, PHYSICALHEIGHT);
+	margin_right = phys_width - printarea_horz - margin_left;
+	margin_bottom = phys_height - printarea_vert - margin_top;
 
-            result = TCL_OK;
-            return result;
-        }
-        result = TCL_OK;
-        return result;
+	/* 
+	 * Convert margins into pixel values the printer understands. 
+	 */
+	digital_margin_left = MulDiv(margin_left, dpi_x, 1000);
+	digital_margin_top = MulDiv(margin_top, dpi_y, 1000);
+	digital_margin_right = MulDiv(margin_right, dpi_x, 1000);
+	digital_margin_bottom = MulDiv(margin_bottom, dpi_y, 1000);
+
+	/* 
+	 *Compute adjusted printer margins in pixels. 
+	 */
+	left_adjust_margin = digital_margin_left - margin_left;
+	top_adjust_margin = digital_margin_top - margin_top;
+	right_adjust_margin = digital_margin_right - margin_right;
+	bottom_adjust_margin = digital_margin_bottom - margin_bottom;
+
+	/* 
+	 *Finally, here is our print area.
+	 */
+	page_width = printarea_horz - (left_adjust_margin + right_adjust_margin);
+	page_height = printarea_vert - (top_adjust_margin + bottom_adjust_margin);
+
+	/*
+	  Set font and define variables for printing.
+	*/
+	hFont = (HFONT) GetStockObject(ANSI_FIXED_FONT);
+
+	LONG printlen = bufferlen;
+	int text_done = 0;
+	LPCTSTR begin_text;
+	int testheight = 0;
+
+	/* 
+	 * Start printing. 
+	 */
+	output = StartDoc(hDC, &di);
+ 	if (output = 0) {
+	    Tcl_AppendResult(interp, "unable to start document", NULL);
+	    return TCL_ERROR;		
+	}
+
+	RECT r, testrect;
+	r.left = 100;
+	r.top = 100;
+	r.right = (page_width - 100);
+	r.bottom = (page_height - 100);
+		
+	begin_text = printbuffer;
+
+	/* 
+	 * Loop through the text until it is all printed. We are
+         * drawing to a dummy rect with the DT_CALCRECT flag set to
+	 * calculate the full area of the text buffer; see
+	 * https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-drawtext.
+	 */
+	for (page = pd.nMinPage; (output >= 0) && (text_done < printlen); page++)
+	    {
+		int textrange_low = 0;
+		int textrange_high = printlen - text_done;
+		int textcount = textrange_high;
+
+		output = StartPage(hDC);
+		if (output = 0) {
+		    Tcl_AppendResult(interp, "unable to start page", NULL);
+		    return TCL_ERROR;
+		    break;
+		}
+
+		SetMapMode(hDC, MM_TEXT);
+	        SelectObject(hDC, hFont);
+		
+		testrect = r;
+		while (textrange_low < textrange_high) {
+		    testrect.right = r.right;
+		    testheight = DrawText(hDC, begin_text, textcount, &testrect, DT_CALCRECT|DT_WORDBREAK|DT_NOCLIP|DT_EXPANDTABS|DT_NOPREFIX);
+		    if (testheight < r.bottom)
+			textrange_low = textcount;
+		    if (testheight > r.bottom)
+			textrange_high = textcount;
+		    if (textrange_low == textrange_high - 1)
+			textrange_low = textrange_high;
+		    if (textrange_low < textrange_high)
+		        textcount = textrange_low + (textrange_high - textrange_low)/2;
+		    break;
+		}
+		output = DrawText(hDC, begin_text, textcount, &r, DT_WORDBREAK|DT_NOCLIP|DT_EXPANDTABS|DT_NOPREFIX);
+		if (output = 0) {
+		    Tcl_AppendResult(interp, "unable to draw text", NULL);
+		    return TCL_ERROR;	
+		}
+	        /*
+		 * Recalculate each of these values to draw on the next page 
+		 * until the buffer is empty, then end that page.
+		 */
+		begin_text += textcount;
+		text_done += textcount;
+
+		output = EndPage(hDC);
+		if (output = 0) {
+		    Tcl_AppendResult(interp, "unable to end page", NULL);
+		    return TCL_ERROR;	
+		}
+	    }
+	EndDoc(hDC);
+	DeleteDC(hDC);
+
+	result = TCL_OK;
+	return result;
     }
     return result;
 }
+
+
 
 /*
  * ----------------------------------------------------------------------
