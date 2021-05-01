28e53b9ee9eb17acc3c3f076af4e4089203b698b
Kevin Walzer <kw@codebykevin.com>
2021-05-01 17:45:57 +0000

    Remove page setup - print customizing can be done at the select printer dialog

--- win/tkWinPrint.c
+++ win/tkWinPrint.c
@@ -34,9 +34,6 @@ PDEVMODE returnedDevmode;
 PDEVMODE localDevmode;
 static int PrintSelectPrinter( ClientData clientData,Tcl_Interp *interp,
 			       int objc,Tcl_Obj *const objv[]);
-static int PrintPageSetup( ClientData clientData, Tcl_Interp *interp,
-			   int objc,Tcl_Obj *const objv[]);
-BOOL CALLBACK PaintHook(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
 int Winprint_Init(Tcl_Interp * interp);
 
 /*----------------------------------------------------------------------
@@ -142,137 +139,6 @@ static int PrintSelectPrinter(ClientData clientData,
     return TCL_OK;
 }
 
-
-/*
- * --------------------------------------------------------------------------
- *
- * PrintPageSetup--
- *
- *     Show the page setup dialogue box.
- *
- * Results:
- *      Returns the complete page setup.
- *
- * -------------------------------------------------------------------------
- */
-
-static int PrintPageSetup( ClientData clientData,
-			   Tcl_Interp *interp,
-			   int objc,
-			   Tcl_Obj *const objv[])
-{
-
-    (void) clientData;
-    (void) objc;
-    (void) objv;
-    
-    /* Initialize PAGESETUPDLG. */
-    ZeroMemory(&psd, sizeof(psd));
-    psd.lStructSize = sizeof(psd);
-    psd.hwndOwner = GetDesktopWindow();
-    returnedDevmode = NULL;
-    localDevmode = NULL;
-
-
-    psd.Flags =  PSD_ENABLEPAGEPAINTHOOK | PSD_MARGINS;
-
-    /*Set default margins.*/
-    psd.rtMargin.top = 1000;
-    psd.rtMargin.left = 1250;
-    psd.rtMargin.right = 1250;
-    psd.rtMargin.bottom = 1000;
-
-    /*Callback for displaying print preview.*/
-    psd.lpfnPagePaintHook = (LPPAGEPAINTHOOK)PaintHook;
-
-    if (PageSetupDlg(&psd)==TRUE)
-	{
-	    /* Copy print attributes to local structure. */ 
-	    returnedDevmode = (PDEVMODE)GlobalLock(psd.hDevMode);	
-	    localDevmode = (LPDEVMODE)HeapAlloc(
-						GetProcessHeap(), 
-						HEAP_ZERO_MEMORY | HEAP_GENERATE_EXCEPTIONS, 
-						returnedDevmode->dmSize);
-                        
-	    if (localDevmode !=NULL) 
-		{
-		    memcpy(
-			   (LPVOID)localDevmode,
-			   (LPVOID)returnedDevmode, 
-			   returnedDevmode->dmSize);
-		    /* Get values from user-set and built-in properties. */
-		    localPrinterName = (char*) localDevmode->dmDeviceName;
-		    dpi_y = localDevmode->dmYResolution;
-		    dpi_x =  localDevmode->dmPrintQuality;
-		    paper_height = (int) localDevmode->dmPaperLength;
-		    paper_width = (int) localDevmode->dmPaperWidth; 
-		}
-	    else
-		{
-		    localDevmode = NULL;
-		}
-	    if (psd.hDevMode !=NULL) 
-		{
-		    GlobalFree(psd.hDevMode);
-		}
-	    return TCL_OK;
-  
-	} else {
-	Tcl_AppendResult(interp, "can't display page setup dialog", NULL);
-	return TCL_ERROR;
-    }
-}
-
-/*
- * --------------------------------------------------------------------------
- *
- * PaintHook--
- *
- *     Callback for displaying page margins/print preview.
- *
- * Results:
- *      Returns visual thumbnail of page margins.
- *
- * -------------------------------------------------------------------------
- */
-
-
-BOOL CALLBACK PaintHook(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam) 
-{ 
-    LPRECT lprc; 
-    COLORREF crMargRect; 
-    HDC hdc, hdcOld; 
- 
-    switch (uMsg) 
-    { 
-        /* Draw the margin rectangle.  */
-        case WM_PSD_MARGINRECT: 
-            hdc = (HDC) wParam; 
-            lprc = (LPRECT) lParam; 
- 
-            /* Get the system highlight color. */ 
-            crMargRect = GetSysColor(COLOR_HIGHLIGHT); 
- 
-            /* 
-	     * Create a dash-dot pen of the system highlight color and  
-             * select it into the DC of the sample page. 
-	     */ 
-            hdcOld = SelectObject(hdc, CreatePen(PS_DASHDOT, .5, crMargRect)); 
- 
-            /* Draw the margin rectangle.  */
-            Rectangle(hdc, lprc->left, lprc->top, lprc->right, lprc->bottom); 
- 
-            /* Restore the previous pen to the DC.  */
-            SelectObject(hdc, hdcOld); 
-            return TRUE; 
- 
-        default: 
-            return FALSE; 
-    } 
-    return TRUE; 
-}
-
-
 /*
  * ----------------------------------------------------------------------
  *
@@ -291,8 +157,7 @@ int Winprint_Init(Tcl_Interp * interp)
 {
 
     Tcl_CreateObjCommand(interp, "::tk::print::_selectprinter", PrintSelectPrinter, NULL, NULL);
-    Tcl_CreateObjCommand(interp, "::tk::print::_pagesetup", PrintPageSetup,
-			 NULL, NULL);
+ 
     return TCL_OK;
 }
 
