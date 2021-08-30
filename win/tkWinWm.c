479cdaab5d24db58c825dd3bfb3af7bc3973209f
Kevin Walzer <kw@codebykevin.com>
2021-08-30 20:57:11 +0000

    Fix argument parsing in test suite

--- win/tkWinWm.c
+++ win/tkWinWm.c
@@ -4295,7 +4295,12 @@ WmIconphotoCmd(
     unsigned size;
     (void)tkwin;
 
-    if ((objc == 3) && (base_icon == NULL)) {
+	if (strcmp(Tcl_GetString(objv[1]), "iconphoto") != 0) {
+		Tcl_SetObjResult(interp, Tcl_NewStringObj("Argument should be \"iconphoto\"",  -1));
+		return TCL_ERROR;
+	}
+		
+    if ((objc == 3) && (strcmp(Tcl_GetString(objv[1]), "iconphoto") == 0) && base_icon == NULL) {
 	Tcl_SetObjResult(interp, Tcl_NewStringObj("", -1));
 	return TCL_OK;
     }
