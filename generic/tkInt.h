196e74aaa957571020f1edb360ef67328bfd1fa6
fvogelnew1 <42586262+fvogelnew1@users.noreply.github.com>
2021-12-12 16:14:37 +0100

    Undo previous commit - not the cause for the crash

--- generic/tkInt.h
+++ generic/tkInt.h
@@ -373,9 +373,9 @@ typedef struct TkDisplay {
      * and ttkMacOSXTheme.c:
      */
 
-#define TkGetContainer(tkwin) (tkwin ? (Tk_TopWinHierarchy((TkWindow *)tkwin) ? NULL : \
+#define TkGetContainer(tkwin) (Tk_TopWinHierarchy((TkWindow *)tkwin) ? NULL : \
 	(((TkWindow *)tkwin)->maintainerPtr != NULL ? \
-	 ((TkWindow *)tkwin)->maintainerPtr : ((TkWindow *)tkwin)->parentPtr)) : NULL)
+	 ((TkWindow *)tkwin)->maintainerPtr : ((TkWindow *)tkwin)->parentPtr))
 
     /*
      * Information used by tkGet.c only:
