5cd5c03bfa8190a10d9af36f29fcacb0525b0c27
jan.nijtmans <nijtmans@users.sourceforge.net>
2021-12-14 16:55:12 +0000

    TIP #577 follow-up (Enhanced index values for Tk). Now that an index may be "", reflect this in error-messages as well

--- generic/tkConfig.c
+++ generic/tkConfig.c
@@ -641,7 +641,7 @@ DoObjConfig(
 	if (TkGetIntForIndex(valuePtr, TCL_INDEX_END, 0, &newIndex) != TCL_OK) {
 	    if (interp) {
 		Tcl_AppendResult(interp, "bad index \"", Tcl_GetString(valuePtr),
-			"\": must be integer?[+-]integer? or end?[+-]integer?", NULL);
+			"\": must be integer?[+-]integer?, end?[+-]integer?, or \"\"", NULL);
 	    }
 	    return TCL_ERROR;
 	}
