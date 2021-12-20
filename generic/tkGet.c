2b0dbb3ef8f3b69d557135033a1a2833879aaa39
jan.nijtmans <nijtmans@users.sourceforge.net>
2021-12-20 14:27:34 +0000

    Update documentation

--- generic/tkGet.c
+++ generic/tkGet.c
@@ -472,6 +472,7 @@ Tk_NameOfJustify(
     case TK_JUSTIFY_LEFT: return "left";
     case TK_JUSTIFY_RIGHT: return "right";
     case TK_JUSTIFY_CENTER: return "center";
+    case TK_JUSTIFY_NULL: return "";
     default: break;
     }
     return "unknown justification style";
