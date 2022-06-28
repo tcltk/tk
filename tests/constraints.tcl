8381d4ddd20ee1d41cc43baac59862c4b47e8173
fvogel <fvogelnew1@free.fr>
2022-06-28 21:52:34 +0000

    Further ruggedize unixEmbed-11.2 because it still failed once at GitHub Actions in branch unstable_tests on macOS with clang (mem, --enable-aqua). Even with [645a3198] the time out is hit (1 second in total), after which [winfo exists .embed.b] still is O. Try to update in more steps when creating toplevels or packs windows.

--- tests/constraints.tcl
+++ tests/constraints.tcl
@@ -268,7 +268,7 @@ namespace eval tk {
 	    }
 	    if {$count > 19} {
 		puts "waitForUnmap $w: timeout! mapping state is [winfo ismapped $w]"
-		puts "                 winfo geometry is [winfo geometry $w]"
+		puts "  winfo geometry is [winfo geometry $w]"
 	    }
 	}
 
