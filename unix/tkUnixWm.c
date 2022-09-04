6101fdd02a15270df0d15bac100c20f8307b2960
jan.nijtmans <nijtmans@users.sourceforge.net>
2022-09-04 20:33:24 +0000

    Fix  wm-1.2 testcase

--- unix/tkUnixWm.c
+++ unix/tkUnixWm.c
@@ -471,6 +471,9 @@ static int		WmStateCmd(Tk_Window tkwin, TkWindow *winPtr,
 static int		WmTitleCmd(Tk_Window tkwin, TkWindow *winPtr,
 			    Tcl_Interp *interp, int objc,
 			    Tcl_Obj *const objv[]);
+static int		WmTouchCmd(Tk_Window tkwin,
+			    TkWindow *winPtr, Tcl_Interp *interp, int objc,
+			    Tcl_Obj *const objv[]);
 static int		WmTransientCmd(Tk_Window tkwin, TkWindow *winPtr,
 			    Tcl_Interp *interp, int objc,
 			    Tcl_Obj *const objv[]);
@@ -1024,7 +1027,7 @@ Tk_WmObjCmd(
 	"iconposition", "iconwindow", "manage", "maxsize",
 	"minsize", "overrideredirect", "positionfrom",
 	"protocol", "resizable", "sizefrom", "stackorder",
-	"state", "title", "transient", "withdraw", NULL };
+	"state", "title", "touch", "transient", "withdraw", NULL };
     enum options {
 	WMOPT_ASPECT, WMOPT_ATTRIBUTES, WMOPT_CLIENT, WMOPT_COLORMAPWINDOWS,
 	WMOPT_COMMAND, WMOPT_DEICONIFY, WMOPT_FOCUSMODEL, WMOPT_FORGET,
@@ -1034,7 +1037,7 @@ Tk_WmObjCmd(
 	WMOPT_ICONPOSITION, WMOPT_ICONWINDOW, WMOPT_MANAGE, WMOPT_MAXSIZE,
 	WMOPT_MINSIZE, WMOPT_OVERRIDEREDIRECT, WMOPT_POSITIONFROM,
 	WMOPT_PROTOCOL, WMOPT_RESIZABLE, WMOPT_SIZEFROM, WMOPT_STACKORDER,
-	WMOPT_STATE, WMOPT_TITLE, WMOPT_TRANSIENT, WMOPT_WITHDRAW };
+	WMOPT_STATE, WMOPT_TITLE, WMOPT_TOUCH, WMOPT_TRANSIENT, WMOPT_WITHDRAW };
     int index;
     const char *argv1;
     TkWindow *winPtr;
@@ -1085,8 +1088,8 @@ Tk_WmObjCmd(
 	return TCL_ERROR;
     }
     winPtr = (TkWindow *) targetWin;
-    if (!Tk_IsTopLevel(winPtr) &&
-	    (index != WMOPT_MANAGE) && (index != WMOPT_FORGET)) {
+    if (!Tk_IsTopLevel(winPtr) && (index != WMOPT_MANAGE)
+	    && (index != WMOPT_TOUCH) && (index != WMOPT_FORGET)) {
 	Tcl_SetObjResult(interp, Tcl_ObjPrintf(
 		"window \"%s\" isn't a top-level window", winPtr->pathName));
 	Tcl_SetErrorCode(interp, "TK", "LOOKUP", "TOPLEVEL", winPtr->pathName,
@@ -1157,6 +1160,8 @@ Tk_WmObjCmd(
 	return WmStateCmd(tkwin, winPtr, interp, objc, objv);
     case WMOPT_TITLE:
 	return WmTitleCmd(tkwin, winPtr, interp, objc, objv);
+    case WMOPT_TOUCH:
+	return WmTouchCmd(tkwin, winPtr, interp, objc, objv);
     case WMOPT_TRANSIENT:
 	return WmTransientCmd(tkwin, winPtr, interp, objc, objv);
     case WMOPT_WITHDRAW:
@@ -3547,6 +3552,39 @@ WmTitleCmd(
     return TCL_OK;
 }
 
+/*
+ *----------------------------------------------------------------------
+ *
+ * WmTouchCmd --
+ *
+ *	This function is invoked to process the "wm touch" Tcl command. See
+ *	the user documentation for details on what it does.
+ *
+ * Results:
+ *	A standard Tcl result.
+ *
+ * Side effects:
+ *	See the user documentation.
+ *
+ *----------------------------------------------------------------------
+ */
+
+static int
+WmTouchCmd(
+    TCL_UNUSED(Tk_Window),		/* Main window of the application. */
+    TkWindow *winPtr,		/* Toplevel to work with */
+    Tcl_Interp *interp,		/* Current interpreter. */
+    int objc,			/* Number of arguments. */
+    Tcl_Obj *const objv[])	/* Argument objects. */
+{
+    (void)winPtr;
+    (void)interp;
+    (void)objc;
+    (void)objv;
+    /* TODO: to be implemented yet */
+    return TCL_ERROR;
+}
+
 /*
  *----------------------------------------------------------------------
  *
