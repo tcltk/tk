305b5222e8bdf8962e6700734368ad7f541e09e6
jan.nijtmans <nijtmans@users.sourceforge.net>
2022-09-08 10:17:56 +0000

    Simplify TkpConfigureMenuEntry(), using existing TKNSString initWithTclUtfBytes functionality

--- macosx/tkMacOSXMenu.c
+++ macosx/tkMacOSXMenu.c
@@ -736,14 +736,9 @@ TkpConfigureMenuEntry(
     [menuItem setImage:image];
     if ((!image || mePtr->compound != COMPOUND_NONE) && mePtr->labelPtr &&
 	    mePtr->labelLength) {
-	Tcl_DString ds;
-	Tcl_DStringInit(&ds);
-	Tcl_UtfToUniCharDString(Tcl_GetString(mePtr->labelPtr),
-				mePtr->labelLength, &ds);
-	title = [[NSString alloc]
-		    initWithCharacters:(unichar *)Tcl_DStringValue(&ds)
-				length:Tcl_DStringLength(&ds)>>1];
-	Tcl_DStringFree(&ds);
+	title = [[TKNSString alloc]
+		    initWithTclUtfBytes:Tcl_GetString(mePtr->labelPtr)
+				length:mePtr->labelLength];
 	if ([title hasSuffix:@"..."]) {
 	    title = [NSString stringWithFormat:@"%@%C",
 		    [title substringToIndex:[title length] - 3], 0x2026];
