ba78963fea5311eebbd490cde30b8f973133cbca
jan.nijtmans <nijtmans@users.sourceforge.net>
2022-09-10 21:10:20 +0000

    Fix [006403a25d]: various comment/documentation fixes

--- macosx/tkMacOSXColor.c
+++ macosx/tkMacOSXColor.c
@@ -299,7 +299,7 @@ GetRGBA(
 
 	/*
 	 * Prior to OSX 10.14, getComponents returns black when applied to
-	 * windowBackGroundColor.
+	 * windowBackgroundColor.
 	 */
 
 	if ([NSApp macOSVersion] < 101400) {
