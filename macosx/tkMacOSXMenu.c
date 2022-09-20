9be9bf7971132adece3ee705f5607132c8a236a8
marc_culler <marc.culler@gmail.com>
2022-09-20 22:49:23 +0000

    Fix wrong initial value for static Bool.

--- macosx/tkMacOSXMenu.c
+++ macosx/tkMacOSXMenu.c
@@ -94,7 +94,7 @@ static const struct {
 static int gNoTkMenus = 0;	/* This is used by Tk_MacOSXTurnOffMenus as
 				 * the flag that Tk is not to draw any
 				 * menus. */
-static Bool   inPostMenu = true;
+static Bool   inPostMenu = false;
 static SInt32 menuMarkColumnWidth = 0, menuIconTrailingEdgeMargin = 0;
 static SInt32 menuTextLeadingEdgeMargin = 0, menuTextTrailingEdgeMargin = 0;
 static SInt16 menuItemExtraHeight = 0, menuItemExtraWidth = 0;
