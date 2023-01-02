1084d1a1148fdbfd02aa2617a1bb73de8a9b1047
jan.nijtmans <nijtmans@users.sourceforge.net>
2023-01-02 17:51:05 +0000

    Attempt to make it look better on MacOS, and more ttk adaptations

--- library/ttk/altTheme.tcl
+++ library/ttk/altTheme.tcl
@@ -38,7 +38,7 @@ namespace eval ttk::theme::alt {
         ttk::style map "." -embossed [list disabled 1] ;
 
 	ttk::style configure TButton \
-	    -anchor center -width -11 -padding [ScaleNum 1] \
+	    -anchor center -width -11 -padding 0.75p \
 	    -relief raised -shiftrelief 1 \
 	    -highlightthickness 1 -highlightcolor $colors(-frame)
 	ttk::style map TButton -relief {
@@ -49,9 +49,9 @@ namespace eval ttk::theme::alt {
 	set t [ScaleNum 2]; set r [ScaleNum 4]; set b $t
 	set indMargin [list 0 $t $r $b]
 	ttk::style configure TCheckbutton -indicatorcolor "#ffffff" \
-	    -indicatormargin $indMargin -padding [ScaleNum 2]
+	    -indicatormargin $indMargin -padding 1.5p
 	ttk::style configure TRadiobutton -indicatorcolor "#ffffff" \
-	    -indicatormargin $indMargin -padding [ScaleNum 2]
+	    -indicatormargin $indMargin -padding 1.5p
 	ttk::style map TCheckbutton -indicatorcolor \
 	    [list  pressed $colors(-frame) \
 	           alternate $colors(-altindicator) \
@@ -62,14 +62,14 @@ namespace eval ttk::theme::alt {
 	           disabled $colors(-frame)]
 
 	ttk::style configure TMenubutton \
-	    -width -11 -arrowsize [ScaleNum 5] \
-	    -padding [ScaleNum 3] -relief raised
+	    -width -11 -arrowsize 3.75p \
+	    -padding 2.5p -relief raised
 
 	ttk::style configure TEntry -padding 1
 	ttk::style map TEntry -fieldbackground \
 		[list readonly $colors(-frame) disabled $colors(-frame)]
 
-	ttk::style configure TCombobox -padding 1 -arrowsize [ScaleNum 14]
+	ttk::style configure TCombobox -padding 1 -arrowsize 10.5p
 	ttk::style map TCombobox -fieldbackground \
 		[list readonly $colors(-frame) disabled $colors(-frame)] \
 		-arrowcolor [list disabled $colors(-disabledfg)]
@@ -77,21 +77,20 @@ namespace eval ttk::theme::alt {
 	    -relief solid -borderwidth 1
 
 	set l [ScaleNum 2]; set r [ScaleNum 10]
-	ttk::style configure TSpinbox -arrowsize [ScaleNum 10] \
+	ttk::style configure TSpinbox -arrowsize 7.5p \
 	    -padding [list $l 0 $r 0]
 	ttk::style map TSpinbox -fieldbackground \
 	    [list readonly $colors(-frame) disabled $colors(-frame)] \
 	    -arrowcolor [list disabled $colors(-disabledfg)]
 
-	ttk::style configure Toolbutton -relief flat -padding [ScaleNum 2]
+	ttk::style configure Toolbutton -relief flat -padding 1.5p
 	ttk::style map Toolbutton -relief \
 	    {disabled flat selected sunken pressed sunken active raised}
 	ttk::style map Toolbutton -background \
 	    [list pressed $colors(-darker)  active $colors(-activebg)]
 
-	set scrlbarWidth [ScaleNum 14]
 	ttk::style configure TScrollbar -relief raised \
-	    -arrowsize $scrlbarWidth -width $scrlbarWidth
+	    -arrowsize [ScaleNum 14] -width [ScaleNum 14]
 
 	ttk::style configure TLabelframe -relief groove -borderwidth 2
 
@@ -99,7 +98,7 @@ namespace eval ttk::theme::alt {
 	set margins [list $l $t $r 0]
 	ttk::style configure TNotebook -tabmargins $margins
 	ttk::style configure TNotebook.Tab -background $colors(-darker) \
-	    -padding [list [ScaleNum 4] [ScaleNum 2]]
+	    -padding [list 3p 1.5p]
 	ttk::style map TNotebook.Tab \
 	    -background [list selected $colors(-frame)] \
 	    -expand [list selected $margins]
@@ -126,5 +125,5 @@ namespace eval ttk::theme::alt {
 	    -barsize [ScaleNum 30] -thickness $thickness
     }
 
-    unset l t r b indMargin scrlbarWidth margins thickness
+    unset l t r b indMargin margins thickness
 }
