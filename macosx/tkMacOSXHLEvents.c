ee30ba1fd8af947b6a98aabf00018fa0a1ea0709
jan.nijtmans <nijtmans@users.sourceforge.net>
2023-03-03 15:45:52 +0000

    Get rid of TCL_ENCODING_NOCOMPLAIN, since it will (most likely) be removed from Tcl

--- macosx/tkMacOSXHLEvents.c
+++ macosx/tkMacOSXHLEvents.c
@@ -227,8 +227,8 @@ static const char getSdefProc[] = "::tk::mac::GetDynamicSdef";
 	if (fileURL == nil) {
 	    continue;
 	}
-	(void)Tcl_ExternalToUtfDStringEx(utf8, [[fileURL path] UTF8String], -1,
-		TCL_ENCODING_NOCOMPLAIN, &pathName);
+	(void)Tcl_ExternalToUtfDString(utf8, [[fileURL path] UTF8String], -1,
+		&pathName);
 	Tcl_DStringAppendElement(openCommand, Tcl_DStringValue(&pathName));
 	Tcl_DStringFree(&pathName);
     }
