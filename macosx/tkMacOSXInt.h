7982579bbbeea8c8e9afd4b1e1204a3d71fa7d9e
fvogel <fvogelnew1@free.fr>
2023-05-29 13:00:41 +0000

    Fix second part of [0e658c9479]: Remove unused function declarations from tkMacOSXInt.h - TkpRetainRegion() and TkpReleaseRegion()

--- macosx/tkMacOSXInt.h
+++ macosx/tkMacOSXInt.h
@@ -160,8 +160,6 @@ typedef struct TkWindowPrivate MacDrawable;
  */
 
 MODULE_SCOPE void TkMacOSXDefaultStartupScript(void);
-MODULE_SCOPE void TkpRetainRegion(Region r);
-MODULE_SCOPE void TkpReleaseRegion(Region r);
 MODULE_SCOPE Bool TkTestLogDisplay(Drawable drawable);
 
 /*
