37919ac69e235b8ee2502ccdf01ad9ed5e5fcc32
culler <culler>
2024-05-23 16:55:00 +0000

    Clean up.

--- generic/tkGrab.c
+++ generic/tkGrab.c
@@ -1102,7 +1102,6 @@ TkInOutEvents(
 	    }
 	}
     }
-    fflush(stderr);
 }
 
 /*
--- win/tkWinWm.c
+++ win/tkWinWm.c
@@ -2750,8 +2750,6 @@ TkWmDeadWindow(
      */
 
     CheckForPointer(winPtr);
-    //TkPointerDeadWindow(winPtr);
-    
     ckfree(wmPtr);
     winPtr->wmInfoPtr = NULL;
 }
