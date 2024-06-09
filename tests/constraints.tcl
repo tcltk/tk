ab8d1af1f460fa855401793104aa03e597820202
fvogel <fvogelnew1@free.fr>
2024-06-09 12:29:09 +0000

    Remove constraints hasEarthPhoto and hasOusterPhoto, the image files used in the tests are distributed in the tests directory so these constraints are always satisfied.

--- tests/constraints.tcl
+++ tests/constraints.tcl
@@ -296,12 +296,6 @@ testConstraint testtext      [llength [info commands testtext]]
 testConstraint testwinevent  [llength [info commands testwinevent]]
 testConstraint testwrapper   [llength [info commands testwrapper]]
 
-# Some graphic files used in some tests
-set earthPhotoFile [file join [file dirname [info script]] earth.gif]
-testConstraint hasEarthPhoto [file exists $earthPhotoFile]
-set ousterPhotoFile [file join [file dirname [info script]] ouster.png]
-testConstraint hasOusterPhoto [file exists $ousterPhotoFile]
-
 # constraints about what sort of fonts are available
 testConstraint fonts 1
 destroy .e
