7d9e69c41f0ee2b446083812e3863b261047e704
Kevin Walzer <kw@codebykevin.com>
2024-11-25 02:21:18 +0000

    Cleanup of attribute procedures to allow binding as an additional command to <Map> event

--- library/accessibility.tcl
+++ library/accessibility.tcl
@@ -20,113 +20,68 @@ namespace eval ::tk::accessible {
 	}
     }
 
-    #check to see if attributes have been applied before binding default values 
-    proc _checkattributes {w} {
-	if {[catch {info exists [::tk::accessible::get_acc_role $w]} msg]} {
-	    return
+    #get text in text widget
+    proc _gettext {w} {
+	set data [$w get sel.first sel.last]
+	if {$data eq ""} {
+	    set data [$w get 1.0 end]
 	}
+	return $data
     }
 
-    bind all <Map> {
-	
-	variable acc_class
-	set acc_class [winfo class %W]
-	
-	switch -- $acc_class {
-	    Button -
-	    TButton {
-		::tk::accessible::_checkattributes %W
-		::tk::accessible::acc_role %W Button
-		::tk::accessible::acc_name %W Button
-		::tk::accessible::acc_description %W  [%W cget -text]
-		::tk::accessible::acc_value %W {}
-		::tk::accessible::acc_state %W  [%W cget -state]
-		::tk::accessible::acc_action %W  {%W invoke}
+    #Set initial accessible attributes and add binding to <Map> event. If the accessibility role is already set,
+    #return because we only want these to fire once.
+    
+    proc _init {w role name description value state action} {
+	if {[catch {::tk::accessible::get_acc_role $w} msg]} {
+	    if {$msg == $role} {
+		return
 	    }
-	    Canvas {}
-	    Dialog {
-		::tk::accessible::_checkattributes %W
-		::tk::accessible::acc_role %W Dialog
-		::tk::accessible::acc_name %W [wm title %W]
-		::tk::accessible::acc_description %W [::tk::accessible::_getdialogtext %W]
-		::tk::accessible::acc_value %W  {}
-		::tk::accessible::acc_state %W  {}
-		::tk::accessible::acc_action %W  {}
-	    }
-	    Entry -
-	    TEntry {
-		::tk::accessible::_checkattributes %W
-		::tk::accessible::acc_role %W Entry
-		::tk::accessible::acc_name %W Entry
-		::tk::accessible::acc_description %W Entry
-		::tk::accessible::acc_value %W  [%W get 0 end]
-		::tk::accessible::acc_state %W  [%W cget -state]
-		::tk::accessible::acc_action %W  {}
-	    }
-	    Listbox {
-		::tk::accessible::_checkattributes %W
-		::tk::accessible::acc_role %W Table
-		::tk::accessible::acc_name %W Table
-		::tk::accessible::acc_description %W Table
-		::tk::accessible::acc_value %W [%W get [%W curselection]]
-		::tk::accessible::acc_state %W  [%W cget -state]
-		::tk::accessible::acc_action %W  {}
-	    }
-	    Menu {
-	        #menus on macOS are native and accessibility-ready
-		if {[tk windowingsystem] ne "aqua"} {
-		    ::tk::accessible::_checkattributes %W
-		    ::tk::accessible::acc_role %W Menu
-		    ::tk::accessible::acc_name %W [%W entrycget active -label]
-		    ::tk::accessible::acc_description %W [%W entrycget active\
-							      -label]
-		    ::tk::accessible::acc_value %W {}
-		    ::tk::accessible::acc_state %W  [%W cget -state]
-		    ::tk::accessible::acc_action %W  {%W invoke}
-		}	
-	    }
-	    
-	    Scale 
-	    {
-		::tk::accessible::_checkattributes %W
-		::tk::accessible::acc_role %W Scale
-		::tk::accessible::acc_name %W Scale
-		::tk::accessible::acc_description %W Scale
-		::tk::accessible::acc_value %W [%W get]
-		::tk::accessible::acc_state %W  [%W cget -state]
-		::tk::accessible::acc_action %W  {%W set}
-	    }
-	    TScale {
-		::tk::accessible::_checkattributes %W
-		::tk::accessible::acc_role %W Scale
-		::tk::accessible::acc_name %W Scale
-		::tk::accessible::acc_description %W Scale
-		::tk::accessible::acc_value %W [%W get]
-		::tk::accessible::acc_state %W  [%W state]
-		::tk::accessible::acc_action %W  {%W set}
-	    }
-	    Scrollbar  {
-		::tk::accessible::_checkattributes %W
-		::tk::accessible::acc_role %W Scrollbar
-		::tk::accessible::acc_name %W Scrollbar
-		::tk::accessible::acc_description %W Scrollbar
-		::tk::accessible::acc_value %W [%W get]
-		::tk::accessible::acc_state %W  [%W cget -state]
-		::tk::accessible::acc_action %W  {%W cget -command}
-	    }
-	    TScrollbar  {
-		::tk::accessible::_checkattributes %W
-		::tk::accessible::acc_role %W Scrollbar
-		::tk::accessible::acc_name %W Scrollbar
-		::tk::accessible::acc_description %W Scrollbar
-		::tk::accessible::acc_value %W [%W get]
-		::tk::accessible::acc_state %W  [%W state]
-		::tk::accessible::acc_action %W  {%W cget -command}
-	    }
-	    
 	}
+	::tk::accessible::acc_role $w $role
+	::tk::accessible::acc_name $w $name
+	::tk::accessible::acc_description $w $description
+	::tk::accessible::acc_value $w $value  
+	::tk::accessible::acc_state $w $state
+	::tk::accessible::acc_action $w $action
     }
 
+    #Button bindings
+    bind Button <Map> {+::tk::accessible::_init %W Button Button [%W cget -text] {} [%W cget -state] {%W invoke}}
+    #Canvas bindings
+    bind Canvas <Map> {+::tk::accessible::_init %W Canvas Canvas Canvas {} {} {}}
+
+    #Dialog bindings
+    bind Dialog <Map> {+::tk::accessible::_init %W Dialog [wm title %W] [::tk::accessible::_getdialogtext %W] {} {} {}}
+
+    #Entry/TEntry bindings
+    bind Entry <Map> {+::tk::accessible::_init %W Entry Entry Entry [%W get 0 end] [%W cget -state] {} }
+    bind TEntry <Map> {+::tk::accessible::_init %W Entry Entry Entry [%W get 0 end] [%W cget -state] {} }
+
+    #Listbox bindings
+    bind Listbox <Map> {+::tk::accessible::_init %W Table Table Table [%W get [%W curselection]] [%W cget -state] {}}
+
+    #Radiobutton bindings
+    bind Radiobutton <Map> {+::tk::accessible::_init %W Radiobutton Radiobutton Radiobutton [set [%W cget -variable]] [%W cget -state] {}}
+
+    #Scale/TScale bindings
+    bind Scale <Map> {+::tk::accessible::_init %W Scale Scale Scale [%W get] [%W cget -state] {%W set}}
+    bind TScale <Map> {+::tk::accessible::_init %W Scale Scale Scale [%W get] [%W state] {%W set}}
+
+    #Menu bindings - macOS menus are native and already accessible-enabled
+    if {[tk windowingsystem] ne "aqua"} {
+	bind Menu <Map> {+::tk::accessible::_init %W Menu [%W entrycget active -label]  [%W entrycget active -label] {} [%W cget -state] {%W invoke}}
+    }
+
+    #Scrollbar/TScrollbar bindings
+    bind Scrollbar <Map> {+::tk::accessible::_init %W Scrollbar Scrollbar Scrollbar [%W get] [%W cget -state] {%W cget -command}}
+    bind TScrollbar <Map> {+::tk::accessible::_init %W Scrollbar Scrollbar Scrollbar [%W get] [%W cget -state] {%W cget -command}}
+
+    #Text bindings
+    bind Text <Map> {+::tk::accessible::_init %W Text Text Text [::tk::accessible::_gettext %W] [%W cget -state] {}}
+
+
+    #Export the main commands.
     namespace export acc_role acc_name acc_description acc_value acc_state acc_action get_acc_role get_acc_name get_acc_description get_acc_value get_acc_state get_acc_action
     namespace ensemble create
 }
