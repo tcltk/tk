706f1daf030af3c3e6646033447ccf965dd4dc3a
erikleunissen <elns@xs4all.nl>
2025-01-10 16:33:14 +0000

    testutils.tcl: rearrange utility procs in ::tk::test

--- tests/testutils.tcl
+++ tests/testutils.tcl
@@ -19,7 +19,91 @@
 namespace eval tk {
     namespace eval test {
 
-	namespace export loadTkCommand
+	proc assert {expr {message ""}} {
+	    if {![uplevel 1 [list expr $expr]]} {
+		error "PANIC: $message ($expr failed)"
+	    }
+	}
+	# controlPointerWarpTiming --
+	#
+	# This proc is intended to ensure that the (mouse) pointer has actually
+	# been moved to its new position after a Tk test issued:
+	#
+	#    [event generate $w $event -warp 1 ...]
+	#
+	# It takes care of the following timing details of pointer warping:
+	#
+	# a. Allow pointer warping to happen if it was scheduled for execution at
+	#    idle time. This happens synchronously if $w refers to the
+	#    whole screen or if the -when option to [event generate] is "now".
+	#
+	# b. Work around a race condition associated with OS notification of
+	#    mouse motion on Windows.
+	#
+	#    When calling [event generate $w $event -warp 1 ...], the following
+	#    sequence occurs:
+	#    - At some point in the processing of this command, either via a
+	#      synchronous execution path, or asynchronously at idle time, Tk calls
+	#      an OS function* to carry out the mouse cursor motion.
+	#    - Tk has previously registered a callback function** with the OS, for
+	#      the OS to call in order to notify Tk when a mouse move is completed.
+	#    - Tk doesn't wait for the callback function to receive the notification
+	#      from the OS, but continues processing. This suits most use cases
+	#      because usually the notification arrives fast enough (within a few tens
+	#      of microseconds). However ...
+	#    - A problem arises if Tk performs some processing, immediately following
+	#      up on [event generate $w $event -warp 1 ...], and that processing
+	#      relies on the mouse pointer having actually moved. If such processing
+	#      happens just before the notification from the OS has been received,
+	#      Tk will be using not yet updated info (e.g. mouse coordinates).
+	#
+	#         Hickup, choke etc ... !
+	#
+	#            *  the function SendInput() of the Win32 API
+	#            ** the callback function is TkWinChildProc()
+	#
+	#    This timing issue can be addressed by putting the Tk process on hold
+	#    (do nothing at all) for a somewhat extended amount of time, while
+	#    letting the OS complete its job in the meantime. This is what is
+	#    accomplished by calling [after ms].
+	#
+	#    ----
+	#    For the history of this issue please refer to Tk ticket [69b48f427e],
+	#    specifically the comment on 2019-10-27 14:24:26.
+	#
+	#
+	# Beware: there are cases, not (yet) exercised by the Tk test suite, where
+	# [controlPointerWarpTiming] doesn't ensure the new position of the pointer.
+	# For example, when issued under Tk8.7+, if the value for the -when option
+	# to [event generate $w] is not "now", and $w refers to a Tk window, i.e. not
+	# the whole screen.
+	#
+	proc controlPointerWarpTiming {{duration 50}} {
+		update idletasks ;# see a. above
+		if {[tk windowingsystem] eq "win32"} {
+			after $duration ;# see b. above
+		}
+	}
+	proc deleteWindows {} {
+	    destroy {*}[winfo children .]
+	    # This update is needed to avoid intermittent failures on macOS in unixEmbed.test
+	    # with the (GitHub Actions) CI runner.
+	    # Reason for the failures is unclear but could have to do with window ids being deleted
+	    # after the destroy command returns. The detailed mechanism of such delayed deletions
+	    # is not understood, but it appears that this update prevents the test failures.
+	    update
+	}
+	proc fixfocus {} {
+	    catch {destroy .focus}
+	    toplevel .focus
+	    wm geometry .focus +0+0
+	    entry .focus.e
+	    .focus.e insert 0 "fixfocus"
+	    pack .focus.e
+	    update
+	    focus -force .focus.e
+	    destroy .focus
+	}
 	proc loadTkCommand {} {
 	    set tklib {}
 	    foreach pair [info loaded {}] {
@@ -30,6 +114,29 @@ namespace eval tk {
 		}
 	    }
 	    return [list load $tklib Tk]
+	}
+	namespace export assert controlPointerWarpTiming deleteWindows fixfocus loadTkCommand
+
+	# On macOS windows are not allowed to overlap the menubar at the top of the
+	# screen or the dock.  So tests which move a window and then check whether it
+	# got moved to the requested location should use a y coordinate larger than the
+	# height of the menubar (normally 23 pixels) and an x coordinate larger than the
+	# width of the dock, if it happens to be on the left.
+	# menubarheight deals with this issue but may not be available from the test
+	# environment, therefore provide a fallback here
+	if {[llength [info procs menubarheight]] == 0} {
+	    if {[tk windowingsystem] ne "aqua"} {
+		# Windows may overlap the menubar
+		proc menubarheight {} {
+		    return 0
+		}
+	    } else {
+		# Windows may not overlap the menubar
+		proc menubarheight {} {
+		    return 30 ;  # arbitrary value known to be larger than the menubar height
+		}
+	    }
+	    namespace export menubarheight
 	}
 
 	namespace eval bg {
@@ -106,7 +213,6 @@ namespace eval tk {
 		return $Data
 	    }
 	}
-
 	proc Export {internal as external} {
 	    uplevel 1 [list namespace import $internal]
 	    uplevel 1 [list rename [namespace tail $internal] $external]
@@ -115,122 +221,6 @@ namespace eval tk {
 	Export bg::setup as setupbg
 	Export bg::cleanup as cleanupbg
 	Export bg::do as dobg
-
-	namespace export deleteWindows
-	proc deleteWindows {} {
-	    destroy {*}[winfo children .]
-	    # This update is needed to avoid intermittent failures on macOS in unixEmbed.test
-	    # with the (GitHub Actions) CI runner.
-	    # Reason for the failures is unclear but could have to do with window ids being deleted
-	    # after the destroy command returns. The detailed mechanism of such delayed deletions
-	    # is not understood, but it appears that this update prevents the test failures.
-	    update
-	}
-
-	namespace export fixfocus
-	proc fixfocus {} {
-	    catch {destroy .focus}
-	    toplevel .focus
-	    wm geometry .focus +0+0
-	    entry .focus.e
-	    .focus.e insert 0 "fixfocus"
-	    pack .focus.e
-	    update
-	    focus -force .focus.e
-	    destroy .focus
-	}
-
-	#
-	#  CONTROL TIMING ASPECTS OF POINTER WARPING
-	#
-	# The proc [controlPointerWarpTiming] is intended to ensure that the (mouse)
-	# pointer has actually been moved to its new position after a Tk test issued:
-	#
-	#    [event generate $w $event -warp 1 ...]
-	#
-	# It takes care of the following timing details of pointer warping:
-	#
-	# a. Allow pointer warping to happen if it was scheduled for execution at
-	#    idle time. This happens synchronously if $w refers to the
-	#    whole screen or if the -when option to [event generate] is "now".
-	#
-	# b. Work around a race condition associated with OS notification of
-	#    mouse motion on Windows.
-	#
-	#    When calling [event generate $w $event -warp 1 ...], the following
-	#    sequence occurs:
-	#    - At some point in the processing of this command, either via a
-	#      synchronous execution path, or asynchronously at idle time, Tk calls
-	#      an OS function* to carry out the mouse cursor motion.
-	#    - Tk has previously registered a callback function** with the OS, for
-	#      the OS to call in order to notify Tk when a mouse move is completed.
-	#    - Tk doesn't wait for the callback function to receive the notification
-	#      from the OS, but continues processing. This suits most use cases
-	#      because usually the notification arrives fast enough (within a few tens
-	#      of microseconds). However ...
-	#    - A problem arises if Tk performs some processing, immediately following
-	#      up on [event generate $w $event -warp 1 ...], and that processing
-	#      relies on the mouse pointer having actually moved. If such processing
-	#      happens just before the notification from the OS has been received,
-	#      Tk will be using not yet updated info (e.g. mouse coordinates).
-	#
-	#         Hickup, choke etc ... !
-	#
-	#            *  the function SendInput() of the Win32 API
-	#            ** the callback function is TkWinChildProc()
-	#
-	#    This timing issue can be addressed by putting the Tk process on hold
-	#    (do nothing at all) for a somewhat extended amount of time, while
-	#    letting the OS complete its job in the meantime. This is what is
-	#    accomplished by calling [after ms].
-	#
-	#    ----
-	#    For the history of this issue please refer to Tk ticket [69b48f427e],
-	#    specifically the comment on 2019-10-27 14:24:26.
-	#
-	#
-	# Beware: there are cases, not (yet) exercised by the Tk test suite, where
-	# [controlPointerWarpTiming] doesn't ensure the new position of the pointer.
-	# For example, when issued under Tk8.7+, if the value for the -when option
-	# to [event generate $w] is not "now", and $w refers to a Tk window, i.e. not
-	# the whole screen.
-	#
-	proc controlPointerWarpTiming {{duration 50}} {
-		update idletasks ;# see a. above
-		if {[tk windowingsystem] eq "win32"} {
-			after $duration ;# see b. above
-		}
-	}
-	namespace export controlPointerWarpTiming
-
-	# On macOS windows are not allowed to overlap the menubar at the top of the
-	# screen or the dock.  So tests which move a window and then check whether it
-	# got moved to the requested location should use a y coordinate larger than the
-	# height of the menubar (normally 23 pixels) and an x coordinate larger than the
-	# width of the dock, if it happens to be on the left.
-	# menubarheight deals with this issue but may not be available from the test
-	# environment, therefore provide a fallback here
-	if {[llength [info procs menubarheight]] == 0} {
-	    if {[tk windowingsystem] ne "aqua"} {
-		# Windows may overlap the menubar
-		proc menubarheight {} {
-		    return 0
-		}
-	    } else {
-		# Windows may not overlap the menubar
-		proc menubarheight {} {
-		    return 30 ;  # arbitrary value known to be larger than the menubar height
-		}
-	    }
-	    namespace export menubarheight
-	}
-
-	proc assert {expr {message ""}} {
-	    if {![uplevel 1 [list expr $expr]]} {
-		error "PANIC: $message ($expr failed)"
-	    }
-	}
-	namespace export assert
     }
 }
 
