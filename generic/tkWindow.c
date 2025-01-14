bd6c452a9ff6d858c03437702fd0e506200772a8
jan.nijtmans <nijtmans@users.sourceforge.net>
2025-01-14 16:04:01 +0000

    Put "no-xft" "build-info" tag in the alphabetical correct place

--- generic/tkWindow.c
+++ generic/tkWindow.c
@@ -1008,6 +1008,9 @@ TkCreateMainWindow(
 #ifndef TCL_CFG_OPTIMIZED
 		".no-optimize"
 #endif
+#if !defined(_WIN32) && !defined(MAC_OSX_TK) && !defined(HAVE_XFT)
+		".no-xft"
+#endif
 #ifdef __OBJC__
 		".objective-c"
 #if defined(__cplusplus)
@@ -1031,9 +1034,6 @@ TkCreateMainWindow(
 #endif
 #if !defined(_WIN32) && !defined(MAC_OSX_TK)
 		".x11"
-#if !defined(HAVE_XFT)
-		".no-xft"
-#endif
 #endif
 		), NULL);
     }
