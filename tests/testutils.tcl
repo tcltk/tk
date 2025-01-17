2841c71527dfcdb205109f9f4fc467a7c0a4cc7a
erikleunissen <elns@xs4all.nl>
2025-01-17 08:31:27 +0000

    testutils.tcl: collect procs setupbg, cleanupbg, dobg and Ready as subcommands of a new ensemble command "childTkProcess", with new names to better reflect their functionality. Also rename the functional area from "bg" to "child".

--- tests/testutils.tcl
+++ tests/testutils.tcl
@@ -202,83 +202,89 @@ namespace import -force tk::test::*
 # these functional areas.
 #
 
-namespace eval ::tk::test::bg {
-    # Manage a background process.
+namespace eval ::tk::test::child {
+    # Manage a child Tk process.
     # Replace with child interp or thread?
     namespace import ::tcltest::interpreter
     namespace import ::tk::test::loadTkCommand
 
-    proc cleanupbg {} {
+    proc childTkProcess {subcmd args} {
 	variable fd
-	# catch in case the background process has closed $fd
-	catch {puts $fd exit}
-	catch {close $fd}
-	set fd ""
-    }
-
-    proc dobg {cmd {block 0}} {
-	variable fd
-	variable Data
-	variable Done
-	if {$block} {
-	    fileevent $fd readable {}
-	}
-	puts $fd "[list catch $cmd msg]; update; puts \$msg;\
-		puts **DONE**; flush stdout"
-	flush $fd
-	set Data {}
-	if {$block} {
-	    while {![eof $fd]} {
-		set line [gets $fd]
-		if {$line eq "**DONE**"} {
-		    break
+	switch -- $subcmd {
+	    create {
+		if {[info exists fd] && [string length $fd]} {
+		    childTkProcess exit
 		}
-		append Data $line
+		set fd [open "|[list [interpreter] \
+			-geometry +0+0 -name tktest] $args" r+]
+		puts $fd "puts foo; flush stdout"
+		flush $fd
+		if {[gets $fd data] < 0} {
+		    error "unexpected EOF from \"[interpreter]\""
+		}
+		if {$data ne "foo"} {
+		    error "unexpected output from\
+			    background process: \"$data\""
+		}
+		puts $fd [loadTkCommand]
+		flush $fd
+		fileevent $fd readable [namespace code {childTkProcess read}]
 	    }
-	} else {
-	    set Done 0
-	    vwait [namespace which -variable Done]
+	    eval {
+		variable Data
+		variable Done
+
+		# arguments
+		set script [lindex $args 0]
+		set block 0
+		if {[llength $args] == 2} {
+		    set block [lindex $args 1]
+		}
+
+		if {$block} {
+		    fileevent $fd readable {}
+		}
+		puts $fd "[list catch $script msg]; update; puts \$msg;\
+			puts **DONE**; flush stdout"
+		flush $fd
+		set Data {}
+		if {$block} {
+		    while {![eof $fd]} {
+			set line [gets $fd]
+			if {$line eq "**DONE**"} {
+			    break
+			}
+			append Data $line
+		    }
+		} else {
+		    set Done 0
+		    vwait [namespace which -variable Done]
+		}
+		return $Data
+	    }
+	    exit {
+		# catch in case the background process has closed $fd
+		catch {puts $fd exit}
+		catch {close $fd}
+		set fd ""
+	    }
+	    read {
+		variable Data
+		variable Done
+		set x [gets $fd]
+		if {[eof $fd]} {
+		    fileevent $fd readable {}
+		    set Done 1
+		} elseif {$x eq "**DONE**"} {
+		    set Done 1
+		} else {
+		    append Data $x
+		}
+	    }
 	}
-	return $Data
     }
 
-    proc Ready {} {
-	variable fd
-	variable Data
-	variable Done
-	set x [gets $fd]
-	if {[eof $fd]} {
-	    fileevent $fd readable {}
-	    set Done 1
-	} elseif {$x eq "**DONE**"} {
-	    set Done 1
-	} else {
-	    append Data $x
-	}
-    }
-
-    proc setupbg args {
-	variable fd
-	if {[info exists fd] && [string length $fd]} {
-	    cleanupbg
-	}
-	set fd [open "|[list [interpreter] \
-		-geometry +0+0 -name tktest] $args" r+]
-	puts $fd "puts foo; flush stdout"
-	flush $fd
-	if {[gets $fd data] < 0} {
-	    error "unexpected EOF from \"[interpreter]\""
-	}
-	if {$data ne "foo"} {
-	    error "unexpected output from\
-		    background process: \"$data\""
-	}
-	puts $fd [loadTkCommand]
-	flush $fd
-	fileevent $fd readable [namespace code Ready]
-    }
-
-    namespace export cleanupbg dobg setupbg
+    namespace export *
 }
 
 namespace eval ::tk::test::button {
@@ -726,6 +732,7 @@ namespace eval ::tk::test::send {
 	if {[llength $options] ni {0 2}} {
 	    return -code error "invalid #args"
 	}
+
 	foreach {key value} $options {
 	    if {$key ne "-class"} {
 		return -code error "invalid option \"$key\""
