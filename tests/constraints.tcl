a55aac73849bafe74e0e412a053794aad2702a47
erikleunissen <elns@xs4all.nl>
2025-03-21 12:55:40 +0000

    "XQuarz" -> "XQuartz"

--- tests/constraints.tcl
+++ tests/constraints.tcl
@@ -43,7 +43,7 @@ childTkProcess exit
 testutils forget child
 
 testConstraint failsOnUbuntu [expr {![info exists ::env(CI)] || ![string match Linux $::tcl_platform(os)]}]
-testConstraint failsOnXQuarz [expr {$tcl_platform(os) ne "Darwin" || [tk windowingsystem] ne "x11" }]
+testConstraint failsOnXQuartz [expr {$tcl_platform(os) ne "Darwin" || [tk windowingsystem] ne "x11" }]
 
 #
 # FONTS
