00db4e75e73bd9b14a116c200d95003c7e32654b
fvogel <fvogelnew1@free.fr>
2025-04-26 11:45:08 +0000

    Complementary fix by Emiliano Gavilan for [d2eac285d9]: tk print command fails with canvas widget

--- library/print.tcl
+++ library/print.tcl
@@ -279,8 +279,8 @@ namespace eval ::tk::print {
 		set sc [$wid cget -scrollregion]
 		# if there is no scrollregion, use width and height.
 		if {$sc eq ""} {
-		    set window_x [$wid cget -width]
-		    set window_y [$wid cget -height]
+		    set window_x [winfo pixels $wid [$wid cget -width]]
+		    set window_y [winfo pixels $wid [$wid cget -height]]
 		} else {
 		    set window_x [lindex $sc 2]
 		    set window_y [lindex $sc 3]
