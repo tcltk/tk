c92c35761b413290a62667917383895b08973344
Kevin Walzer <kw@codebykevin.com>
2025-05-13 19:58:45 +0000

    Additional handling for all different types of MSAA objects; still working on aligning accessibility and Tk focus

--- win/tkWinAccessibility.h
+++ win/tkWinAccessibility.h
@@ -11,13 +11,17 @@
 DEFINE_GUID(IID_IAccessible, 0x618736e0, 0x3c3d, 0x11cf, 0x81, 0xc, 0x0, 0xaa, 0x0, 0x38, 0x9b, 0x71);
 
 /* Data declarations used in this file. */
-typedef struct {
+typedef struct TkWinAccessible {
   IAccessibleVtbl *lpVtbl;
   Tk_Window win; 
   Tk_Window toplevel;
   Tcl_Interp *interp;
   HWND hwnd;
   char *pathName;
+  IAccessible **children;
+  int numChildren;
+  Tk_Window focusedChildWin;
+  int focusedIndex; 
   LONG refCount;
 } TkWinAccessible;
 
