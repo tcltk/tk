05d5e34b797600db8fc1dc1d213cfa58c0bfc64f
Kevin Walzer <kw@codebykevin.com>
2025-05-27 11:35:56 +0000

    Remove tkWinAccessibility.h - code refactoring make it unnecessary

--- win/tkWinAccessibility.h
+++
@@ -1,5 +0,0 @@
-/* Functions necessary for more than one file. */
-typedef struct TkRootAccessible TkRootAccessible;
-TkRootAccessible *GetTkAccessibleForWindow(Tk_Window win);
-Tk_Window GetTkWindowForHwnd(HWND hwnd);
-
