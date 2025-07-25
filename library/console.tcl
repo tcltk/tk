9ee905ddaf1cbc0cdcca8fa5952d3caa081541d8
culler <culler>
2025-07-25 03:13:11 +0000

    One less variable.

--- library/console.tcl
+++ library/console.tcl
@@ -159,13 +159,12 @@ proc ::tk::ConsoleInit {} {
     ConsoleBind $con
 
     if {[tk windowingsystem] eq "aqua"} {
-	set stdinFG "systemLinkColor"
+	$con tag configure stdin -foreground systemLinkColor
     } else {
-	set stdinFG "blue"
+	$con tag configure stdin -foreground blue
     }
 
     $con tag configure stderr	-foreground red
-    $con tag configure stdin	-foreground "$stdinFG"
     $con tag configure prompt	-foreground \#8F4433
     $con tag configure proc	-foreground \#008800
     $con tag configure var	-background \#FFC0D0
