fb237161cfcb0ed3f8b905305d4c0aab865a688e
oehhar <harald.oehlmann@elmicron.de>
2025-08-08 14:33:00 +0000

    [7c28f835] Zoom file dialogs if they don't fit on the screen

--- library/choosedir.tcl
+++ library/choosedir.tcl
@@ -91,7 +91,7 @@ proc ::tk::dialog::file::chooseDir:: {args} {
     # so we know how big it wants to be, then center the window in the
     # display (Motif style) and de-iconify it.
 
-    ::tk::PlaceWindow $w widget $data(-parent)
+    ::tk::PlaceWindow $w -zoomnospace widget $data(-parent)
     wm title $w $data(-title)
 
     # Set a grab and claim the focus too.
