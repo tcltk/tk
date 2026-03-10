9cadfd7e1ca86f64ca792633dedcd957180fca22
Kevin Walzer <kw@codebykevin.com>
2026-03-10 03:10:49 +0000

    Moving NanoVG to framebuffer now compiles but segfaults

--- wayland/tkGlfwInit.c
+++ wayland/tkGlfwInit.c
@@ -24,10 +24,10 @@
 #include <GLES2/gl2.h>
 
 
-#include "nanovg.h"
 #ifndef NANOVG_GLES2_IMPLEMENTATION
 #define NANOVG_GLES2_IMPLEMENTATION
 #endif
+
 #include "nanovg_gl.h"
 #include "nanovg_gl_utils.h"
 
