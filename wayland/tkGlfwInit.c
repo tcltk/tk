eb3843355903891d4893429f4eea2fb754a6d552
Kevin Walzer <kw@codebykevin.com>
2026-05-30 17:54:22 +0000

    Add copyright info

--- wayland/tkGlfwInit.c
+++ wayland/tkGlfwInit.c
@@ -10,6 +10,7 @@
  *
  * Copyright (c) 1995-1997 Sun Microsystems, Inc.
  * Copyright (c) 2026  Kevin Walzer
+ * Copyright (c) 2026 Marc Culler
  *
  * See the file "license.terms" for information on usage and redistribution of
  * this file, and for a DISCLAIMER OF ALL WARRANTIES.
