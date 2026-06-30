303106bd8953371fb71214b5f8839601779dcc81
Serhiy Storchaka <storchaka+tcl@gmail.com>
2026-06-30 13:47:05 +0000

    Fix [3cb7c4ac72]: tk inactive returns a negative value on Windows for long inactivity
    On Windows long is 32-bit, so the unsigned 32-bit GetTickCount()-li.dwTime delta
    in Tk_GetUserInactiveTime() was returned as a negative number for inactivity of
    2**31 ms or more.  Clamp to 0x7fffffff.
    
    CPython: https://github.com/python/cpython/issues/151881

--- win/tkWinX.c
+++ win/tkWinX.c
@@ -1932,6 +1932,7 @@ Tk_GetUserInactiveTime(
      TCL_UNUSED(Display *))
 {
     LASTINPUTINFO li;
+    DWORD inactive;
 
     li.cbSize = sizeof(li);
     if (!GetLastInputInfo(&li)) {
@@ -1942,7 +1943,17 @@ Tk_GetUserInactiveTime(
      * Last input info is in milliseconds, since restart time.
      */
 
-    return (GetTickCount()-li.dwTime);
+    inactive = GetTickCount() - li.dwTime;
+
+    /*
+     * "long" is 32-bit on Windows, so clamp to its maximum to avoid returning
+     * a large inactivity interval as a negative value.
+     */
+
+    if (inactive > 0x7fffffff) {
+	return 0x7fffffff;
+    }
+    return (long)inactive;
 }
 
 /*
