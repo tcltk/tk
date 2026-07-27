0cb168d6d1997c27102e6c6b536cbeb7a876029d
Brian <Brian>
2026-07-27 01:44:02 +0000

    Fix for [f476bada5b]: Canvas deleting canvas items leaks memory

--- generic/tkCanvUtil.c
+++ generic/tkCanvUtil.c
@@ -1013,6 +1013,9 @@ Tk_DeleteOutline(
     if (outline->gc != NULL) {
 	Tk_FreeGC(display, outline->gc);
     }
+    if (outline->offsetObj != NULL) {
+	Tcl_DecrRefCount(outline->offsetObj);
+    }
     if ((unsigned) ABS(outline->dash.number) > sizeof(char *)) {
 	Tcl_Free(outline->dash.pattern.pt);
     }
