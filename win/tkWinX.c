30213f6273af27807b7ea2dc83ea74ae42abbc5d
Serhiy Storchaka <storchaka+tcl@gmail.com>
2026-09-22 13:06:09 +0000

    Fix [1954237]: double click is lost if processing a click takes too long
    On Windows the time of a mouse or key event was the time at which the message was processed, not the time at which it was posted, so if a binding script ran longer than the double click time, the events of the second click got too late times and no <Double-1> event was generated. A new TkpGetEventTime() returns GetMessageTime() on Windows and the current time on macOS.

--- win/tkWinX.c
+++ win/tkWinX.c
@@ -1088,7 +1088,7 @@ GenerateXEvent(
     case WM_KEYDOWN:
     case WM_KEYUP: {
 	unsigned int state = GetState(message, wParam, lParam);
-	Time time = TkGetMS();
+	Time time = TkpGetEventTime();
 	POINT clientPoint;
 	union {DWORD msgpos; POINTS point;} root;	/* Note: POINT and POINTS are different */
 
@@ -1646,7 +1646,7 @@ HandleIMEComposition(
 	event.xkey.root = RootWindow(winPtr->display, winPtr->screenNum);
 	event.xkey.subwindow = None;
 	event.xkey.state = TkWinGetModifierState();
-	event.xkey.time = TkGetMS();
+	event.xkey.time = TkpGetEventTime();
 	event.xkey.same_screen = True;
 
 	for (i=0; i<n; ) {
@@ -1781,6 +1781,30 @@ unsigned long
 TkGetMS(void)
 {
     return GetTickCount();
+}
+
+/*
+ *----------------------------------------------------------------------
+ *
+ * TkpGetEventTime --
+ *
+ *	The time of the message which is currently being processed, in the
+ *	same units as TkGetMS(). It is earlier than the current time if the
+ *	message waited in the queue while a script was running. [Bug 1954237]
+ *
+ * Results:
+ *	Number of milliseconds.
+ *
+ * Side effects:
+ *	None.
+ *
+ *----------------------------------------------------------------------
+ */
+
+unsigned long
+TkpGetEventTime(void)
+{
+    return (unsigned long) GetMessageTime();
 }
 
 /*
